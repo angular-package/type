ed2ffdd560eba2256625fbc6d73c0e6f27477a65
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-02 22:03:24 +0100

    refactor(guardString): use isString and add example typescript playground

--- packages/type/src/guard/lib/guard-string.func.ts
+++ packages/type/src/guard/lib/guard-string.func.ts
@@ -1,6 +1,8 @@
+import { isString } from '../../check/lib/is-string.func';
 /**
  * Guard the `value` to be a `string` type.
  * Use `isString()` function for check ONLY.
- * @param value `string` type value to guard.
+ * @param value String type value to guard.
+ * @example https://www.typescriptlang.org/play?jsx=0#code/MYewdgzgLgBAlhAylATnMBzGBeGAKANwEMAbAVwFMAuGIsATwEobjyL4IZo1McA+GFHoAHCiABmMVpRzZcAcm7oM8gNwAoUJFgYyRFABNkPLLkKlKNJZmZSL7BF1TL+HY8vNtGGzeGgxEABUAJQBJADkAcQB9ADUAQQAZAFUAURwYeUSQFAoAW3hhCDI8gH41X20YcOSAWQAhVOC4pLSMgCYAdg0tf3qAeX7E1PjwlpT03HFSCAofXpASCgA6EhAMPAADILCo8bTNgBoYXX0jZ0w8HYiYhInGbxgAeifBFEpKiEWVtY3NmoaTX2qSOJz0hnclwBjWadzSD1Uz1e0xIs0+31W6y2AyGIzGcJBx1OEIuGxxw1GwIRSJgKLRQA
  */
-export const guardString = (value: string): value is string => typeof value === 'string';
+export const guardString = (value: string): value is string => isString(value);
