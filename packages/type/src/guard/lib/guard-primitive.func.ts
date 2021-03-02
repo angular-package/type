58abcff27c2e4a4bc622faf4207ac90addc65aef
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-02 23:09:10 +0100

    refactor(guardPrimitive): use isPrimitive and update example

--- packages/type/src/guard/lib/guard-primitive.func.ts
+++ packages/type/src/guard/lib/guard-primitive.func.ts
@@ -1,9 +1,10 @@
 import { Primitives } from '../../lib/primitives.type';
+import { isPrimitive } from '../../check/lib/is-primitive.func';
 /**
  * Guard the `value` to be a generic `Type` from one of the `Primitives`.
  * Use `isPrimitive()` function for check ONLY.
  * @param value A generic `Type` type value to guard.
  * @param type One of the `Primitives` `'boolean'`, `'bigint'`, `'number'`, `'string'` to check `value`.
- * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBACgTgSwLYOAgbhAzlAvFAcgCMB7EgGwgEMA7AqAH0KIQHMEbh6mCaBXJEQhxuhLMEQ1WBANwAoAMYka4qKz5U4AE3jJUGaPgA8AFXAQAfAAp0VcnwgAuKGcgAaKKEjPdKNJiwASmdbe2gEHFdDC09zEgAzKFCHPFx8Lwh5JRVgKAAhAHkCgBkAUQBBADkAfQA1cuKAVVK8KHi7LEzFZVU8gEkAcT7KkzqG5taATgAGaYB2AEZJyYAmAFYAFjmN6eWFmiye3MrGgFk80oAlMaaW-AWVucOcqABlE0vhgZuJ-AJTkBQfiCYRQCKyboqCgQAB05BIrCsAANCiUKjV6rckR51JodIg-AYjKRobRrKiylUfqUPMQyJRaARAoEZFAAPRszxwByQrDQuEI5H9IYjanYtQabS+fSYYlsDjAcmDYajTHNWksdicJks9mciQ87J8ygCxFIk7nK5inGS-F6fwQIzAoRwawWi7XNU0wjO4Q61kcrmGnr8+Fm96fSrfL3i3FSgkyx3iSSsawRr7U2nJjjSZkB-XciC80OClFFSkY8alWO26UO4n06g0NMfDNejWNxl5vVtDpFo0ls3ClXWiV4utEzUKt1nD2Z5jy7XdwPtcidYsmsPI91WmM28cJ+u+11WdNR+e8AQu-091frgeb0tn6NVmsH+1E7NSGeWz1VrMSDmN4rn2chAA
+ * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBACgTgSwLYOAgbhAzlAvFAcgCMB7EgGwgEMA7AqAH0KIQHMEbh6mCaBXJEQhxuhLMEQ1WBANwAoAMYka4qAizxkqDNHwAeACrgIAPgAU6KuT4QAXFFogANFFCR7mlGkxYAlPctraHUoI0g8E1djEgAzKECbPFx8Nwh5JRVgKFY+KjgAE09tTDwoQ2NzBLtQ4xdUj0QvHT8Aq0SQsN1I9SLvCAs2iDrjX3TlVQAhAHkpgBkAUQBBADkAfQA1RdmAVXnSmKssNMVxrImASQBxc+WDDa3d0oBOAAYXgHYARiengCYAVgALO9AS8fp8aGNMlBltsALITeYAJXuOz2+E+v3eUNUAGUDEibpdUY98AQ4SAoPxBMI1FhZCcVBQIAA6cgkVhmAAG0zmSzWmzRXJcOTyhUaxQgelIzNo5l5CxWJPmLmIZEotAIvlGUAA9LrXHAbIysMy2RzuRdrrdlcLsrkCr0dNK2BxgPKrjc7oLdqqWOxOFqdfrDcaMqbKObOVzYQjkbaRQ7xVo+npqUI4OZY4iUT6VYR08IgzI9QaJGHxmb2dH8YTlsS83bRY6JanxJJWOZa0Tlar2xxpNqSyHyxATVWLTyZoqBQ95k2k07MNL1dQaF2CT2837V5qh6WoAdyEdx5Hq5bPTbG4mxUupf63Vn4Tne8xXYH9yGjyfwxPo9n42ve1b1bZ1C0zMxu3rV9eAEDNiwPb8x1-M9Jyghs5wXECU2dfspCfONcznPsJAHBCv0OMcgA
  */
-export const guardPrimitive = <Type>(value: Type, type: Primitives): value is Type => typeof value === type;
+export const guardPrimitive = <Type>(value: Type, type: Primitives): value is Type => isPrimitive(value, type);
