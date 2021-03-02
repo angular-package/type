f29d64590c3dc42f8a3eee7be6d3378b359ad055
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-02 22:01:30 +0100

    refactor: export all functions

--- packages/type/src/guard/index.ts
+++ packages/type/src/guard/index.ts
@@ -1,4 +1,6 @@
+export { guardFunction } from './lib/guard-function.func';
 export { guardNumber } from './lib/guard-number.func';
+export { guardObjectKey } from './lib/guard-object-key.func';
 export { guardObject } from './lib/guard-object.func';
 export { guardPrimitive } from './lib/guard-primitive.func';
 export { guardString } from './lib/guard-string.func';
