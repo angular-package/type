7c044a858c33e1d579b026e545a44da4a5c625f1
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-02 22:09:25 +0100

    docs(isString): update description

--- packages/type/src/check/lib/is-string.func.ts
+++ packages/type/src/check/lib/is-string.func.ts
@@ -1,7 +1,8 @@
 /**
- * Check is any `value` a `string` type.
+ * Check is any `value` a `'string'` type.
  * Use `guardString()` to type guard `string` `value` also.
- * @param value Any `value` to check it is a `string` type.
+ * @param value Any `value` to check it is a `'string'` type.
  * @returns boolean.
+ * @example https://www.typescriptlang.org/play?jsx=0#code/MYewdgzgLgBAlhAylATnMBzGBeGAKANwEMAbAVwFMAuGIsATwEobjyL4IZo1McA+GFHoAHCiABmMVpRzZcAcm7oM8gNwAodaEixEAFQBKASQByAcQD6ANQCCAGQCqAURwx5dkCgoBbeMIhk3gD8alrg0DAmDgCyAEJOBtb2zq4ATADsGtoRsQDyuXZONiZJji644qQQFBphkCAkFAB0JCAYeAAG+sbmpc4dADQcyDzt3aaWtmWMjKowAPTzgiiUdRANza3tHVFxCX1Og8Ooyni78YlTzjNzizCVJNVrGy1tnXkFRSVXh0MII6cPoVigcbgslg8nkA
  */
 export const isString = (value: any): value is string => typeof value === 'string';
