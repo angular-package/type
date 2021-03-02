f504653232ed53e9a14ab5267cc571c3ed58a602
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-02 02:02:53 +0100

    fix: export Primitives

--- packages/type/src/public-api.ts
+++ packages/type/src/public-api.ts
@@ -3,10 +3,10 @@
  */
 
 // Guard types
-export { guardNumber, guardObject, guardPrimitive, guardString, guardType } from './guard/';
+export { guardNumber, guardObject, guardPrimitive, guardString, guardType } from './guard';
 
 // Check types
-export { isFunction, isNumber, isObject, isPrimitive, isString, isType } from './check/';
+export { isFunction, isNumber, isObject, isPrimitive, isString, isType } from './check';
 
 // Types
-export { Constructor, CycleHook, FunctionType, Partial, Types } from './lib';
+export { Constructor, CycleHook, FunctionType, Partial, Primitives, Types } from './lib';
