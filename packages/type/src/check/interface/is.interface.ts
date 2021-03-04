102e90dbbdc5f6d1edd50fa778f3435f6525eb00
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-04 03:48:45 +0100

    fix(Is): change file name

--- packages/type/src/check/interface/is.interface.ts
+++ packages/type/src/check/interface/is.interface.ts
@@ -5,13 +5,17 @@ import { IsObject } from '../type/is-object.type';
 import { IsPrimitive } from '../type/is-primitive.type';
 import { IsString } from '../type/is-string.type';
 import { IsType } from '../type/is-type.type';
+import { IsUndefined } from '../type/is-undefined.type';
+import { IsBoolean } from '../type/is-boolean.type';
 
 export interface Is {
   array: IsArray;
+  boolean: IsBoolean;
   function: IsFunction;
   number: IsNumber;
   object: IsObject;
   primitive: IsPrimitive;
   string: IsString;
   type: IsType;
+  undefined: IsUndefined;
 }
