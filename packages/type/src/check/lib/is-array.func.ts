6476575847028420f1cb2910a98a6f46de5963b6
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-05 03:45:47 +0100

    refactor(isArray): add IsArray type, generic Type as default any, update description

--- packages/type/src/check/lib/is-array.func.ts
+++ packages/type/src/check/lib/is-array.func.ts
@@ -1,9 +1,9 @@
 import { IsArray } from '../type/is-array.type';
 /**
- * Check is any `value` an `Array` of any type.
- * Use `guardArray()` to type guard array `value` also.
- * @param value Any `value` to check it is an `Array` of any type.
+ * Check if any `value` is an `Array` of a generic type and `Array` instance.
+ * Use the `guardArray()` function to type-guard `array` also.
+ * @param value Any `value` to check if it's an `Array` of a generic type `Type` and `Array` instance.
  * @returns boolean.
- * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBACgTgSwLYOAgbtAvFARge3wBsIBDAOygB88EBzBc4aqcgVyKJfaVwjhYBnYInJ0hIXsRZtyAEwgAzRhDkBuAFABjfOWFQEggIJw4pEFBwAKdKSJsIALigUQASme370Q1BNmQAB5XAD5LMP9zADpDSJAbOwc3KAAyFKgvBwM9YAotCHxFP1NzTW1dfQBlABUAJQBJADkAcQB9ADUjABkAVQBRSygAci78OAgkAzBBNiQAfiHNHRyoRp6AWQAhPtqO7v7BgCYAdiWK5k2AeUuuvqNGvd6BnEU7QQgzlZ7GgBE+gDEmn0fo8DjhZAplORVJ99EZarUjABNVo1BotUF9ZxxQLCUR0MI4ADaQ1IQwANMNcBThlohgBdWHMeGIlFrLY7THYkpBHh8OCEqBEk6UgDMxzFosZ5RWLORrRgDXW9Wq9XafS5xQCgXgyFQGAggqJwzxjDoNJFUBEWWly30AClKpcHp0noMAN6KQjOACMh1FAF8ynbiBAokR8HQrAADOUoxX1ZWq9WY6OU2I8qxxhVKlVqjWu-puNxqKAAejLVrgDhlglD4cjMez7O2u0LfTTBmMmebG1bmOLpYrVZrtfrEaj0bRTTa7c7GYCVmnGPbg-LldeRHeY5IDcnLc5c-T3cXB7b+z6a+Hm+3Id3E5jVxudxdF-nJ-MVift3uA5L66gG8IB3MMH2jb4-kBRpgVTY84isCCASBEFV3-a83mAu9QMbaNHWdWCu3gvDXyeK8NwwjQgA
+ * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBACgTgSwLYOAgbtAvFARge3wBsIBDAOygB88EBzBc4aqcgVyKJfaVwjhYBnYInJ0hIXsRZtyAEwgAzRhDkBuAFChIUAJKCAgnDikQUHAAp0pImwgAuKBRABKcwD4o129ASCoRiYgADzO7poAxvjkwlB+gaaO+glmOMEAKuDYTuQg7lY2do7OLo7ednH+KRlZnlieKQB08camBT5uAGSdXoW+McAUERD4igGtIJoaUQNQAMrpAEq6AHIA4uZQAOQAMvhwEEhxYIJsSAD8W5HRsSsAqgCyAEIAooubAEwA7NezTwDy-x2LwMK02ihsgggv1idxWABEXgAxVYveGbWQKZTkVQw5gGRaLAwATQWy3WjmqwlEdDqUAA2ltSFsADTbXCs7YRLYAXTxAUJJPuzzelImwR4fDgdPp3zZAGYvgr5Xzpjd8YLiTBlg9dOldAA1F5ioLBeDIVAYCAy7bUxh0TlyqAiCqqmaxABSc3+KwA+gaDDs7i9NgBvRSERwARg+8oAvlN3cQII0iPg6BYAAZk1ZrTNslpBCw59YuFxqKAAekrUAhRChapiydT6azwtei3zlRSFnbbzLFertch0EbgmbaYzmYBQJBKy7hbaM+BoIHVZrdYbSZILancMRKJWaIXhgmFn3yNR8LXQ83EDHE9bmYJRNJS1zJ57L5JJbWN5rLr3tuKaTlm37En2nYFqeRbgZB-7OnAdgPjuoHPpq2q6Lq+pGp+Z7gZh2GGi8CGAShIFPl6Pr+oGwZ4UWVF+gGQYkeW67DvW95AA
  */
-export const isArray: IsArray = (value: any): value is Array<any> => Array.isArray(value) && value instanceof Array;
+export const isArray: IsArray = <Type = any>(value: any): value is Array<Type> => Array.isArray(value) && value instanceof Array;
