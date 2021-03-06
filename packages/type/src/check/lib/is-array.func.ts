48691f537b46b70fb80696d408f2e760149d1fd3
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:17:52 +0100

    docs(isArray): update

--- packages/type/src/check/lib/is-array.func.ts
+++ packages/type/src/check/lib/is-array.func.ts
@@ -1,6 +1,6 @@
 import { IsArray } from '../type/is-array.type';
 /**
- * Check if any `value` is an `Array` of a generic type and `Array` instance.
+ * Checks if any `value` is an `Array` of a generic type and `Array` instance.
  * Use the `guardArray()` function to type-guard `array` also.
  * @param value Any `value` to check if it's an `Array` of a generic type `Type` and `Array` instance.
  * @returns boolean.
