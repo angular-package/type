99a35fb37a625b535c4cf81ef20626f0784a8d1f
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:14:01 +0100

    refactor(Is): add bigInt, symbol

--- packages/type/src/check/interface/is.interface.ts
+++ packages/type/src/check/interface/is.interface.ts
@@ -7,14 +7,18 @@ import { IsString } from '../type/is-string.type';
 import { IsType } from '../type/is-type.type';
 import { IsUndefined } from '../type/is-undefined.type';
 import { IsBoolean } from '../type/is-boolean.type';
+import { IsBigInt } from '../type/is-big-int.type';
+import { IsSymbol } from '../type/is-symbol.type';
 
 export interface Is {
   array: IsArray;
+  bigInt: IsBigInt;
   boolean: IsBoolean;
   function: IsFunction;
   number: IsNumber;
   object: IsObject;
   primitive: IsPrimitive;
+  symbol: IsSymbol;
   string: IsString;
   type: IsType;
   undefined: IsUndefined;
