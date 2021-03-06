6ca011ce4ce6fcde0e5e64d96772647e53699127
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:18:46 +0100

    refactor(isClass): remove

--- packages/type/src/check/lib/is-class-type.func.ts
+++
@@ -1,8 +0,0 @@
-import { Constructor } from '../../lib/constructor.type';
-/**
- * Check any `object` is generic `Type`.
- * @param object Object class to guard and check instance.
- * @param type y
- */
-// TODO:: check for proper functionality.
-export const isClass = <Type>(object: any, type?: Constructor<Type>): object is Type => object instanceof type;
