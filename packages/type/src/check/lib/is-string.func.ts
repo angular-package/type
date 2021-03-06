d69839431483fa10c538fc823b75095cce3d13fa
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:24:22 +0100

    fix(isString): check type and instance, update docs

--- packages/type/src/check/lib/is-string.func.ts
+++ packages/type/src/check/lib/is-string.func.ts
@@ -1,9 +1,9 @@
 import { IsString } from '../type/is-string.type';
 /**
- * Check is any `value` a `'string'` type.
- * Use `guardString()` to type guard `string` `value` also.
- * @param value Any `value` to check it is a `'string'` type.
+ * Checks if any `value` is a `'string'` type or `String` instance.
+ * Use the`guardString()` function to type-guard `string`  also.
+ * @param value Any `value` to check if it's a `'string'` type or `String` instance.
  * @returns boolean.
- * @example https://www.typescriptlang.org/play?jsx=0#code/MYewdgzgLgBAlhAylATnMBzGBeGAKANwEMAbAVwFMAuGIsATwEobjyL4IZo1McA+GFHoAHCiABmMVpRzZcAcm7oM8gNwAodaEixEAFQBKASQByAcQD6ANQCCAGQCqAURwx5dkCgoBbeMIhk3gD8alrg0DAmDgCyAEJOBtb2zq4ATADsGtoRsQDyuXZONiZJji644qQQFBphkCAkFAB0JCAYeAAG+sbmpc4dADQcyDzt3aaWtmWMjKowAPTzgiiUdRANza3tHVFxCX1Og8Ooyni78YlTzjNzizCVJNVrGy1tnXkFRSVXh0MII6cPoVigcbgslg8nkA
+ * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAkgzgZWAJwJYDsDmUC8UAUAbgIYA2ArhAFxTHogCUuAfFCRdKnFHChpgG4AUAGMA9ul5QuSNFhrxZ-XAXaUadRjTWduvOdhysd0ycDoiIYgGZQlWKAB9HUUJBtsylXDjwByfX4-YVEJKQQAFQAlGAA5AHEVPwAZMWQIAFtpMDhyDIB+YNCzKFiAVQBZACEAUSiVACYAdmFxEqqAeQ7kmoBBWJVrMjgIVrDgUpqAdUiYhJV0CAB3Oz4sfBS0zOzcrIAjUmIjgAs-BhC2uDFSCAA6UjFMfAADWbj454AaaUQ1p7eEgxzlAAPQg1zISjFK43e6PF7lap1L4-exPRG1KJAgSg8FDUgjaHXO4PJ7PTrdPqxFEyP74Ck9frY3FQfGEy7EuFk2LTAEfb60gz4HkzaLvZlgqAAEQgwAgIgmS1QwGOpl4Fis1iEQA
  */
-export const isString: IsString = (value: any): value is string => typeof value === 'string';
+export const isString: IsString = (value: any): value is string => value instanceof String || typeof value === 'string';
