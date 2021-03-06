c5c9f068ed27e0bbfc9c19c4e0ecf869f160efbd
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 23:30:36 +0100

    refactor(Partial): remove.

--- packages/type/src/type/index.ts
+++ packages/type/src/type/index.ts
@@ -1,7 +1,6 @@
 export { Constructor } from './constructor.type';
 export { CycleHook } from './cycle-hook.type';
 export { Func } from './func.type';
-export { Partial } from './partial.type';
 export { Primitive } from './primitive.type';
 export { Primitives } from './primitives.type';
 export { Types } from './types.type';
