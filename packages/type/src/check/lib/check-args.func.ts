2411eace27eda38a356a188386459e17791cb160
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:17:34 +0100

    feat(check): add check for `are` functions.

---
+++ packages/type/src/check/lib/check-args.func.ts
@@ -0,0 +1,18 @@
+import { Primitives } from '../../lib/primitives.type';
+import { isString } from './is-string.func';
+import { IsString } from '../type/is-string.type';
+export const check = (type: Primitives, ...args: any): boolean => {
+  let result = true;
+  let method: IsString;
+  switch (type) {
+    case 'string': method = isString; break;
+    default: method = isString; break;
+  }
+  for (const arg of args) {
+    result = method(arg);
+    if (result === false) {
+      break;
+    }
+  }
+  return result;
+};
