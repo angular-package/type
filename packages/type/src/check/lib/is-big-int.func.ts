ead3298fd7c06c6c951baeb8b82e430ba7891831
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 21:52:00 +0100

    docs(isBigInt): updat example

--- packages/type/src/check/lib/is-big-int.func.ts
+++ packages/type/src/check/lib/is-big-int.func.ts
@@ -4,6 +4,6 @@ import { IsBigInt } from '../type/is-big-int.type';
  * Use the `guardBigInt()` function to type-guard `bigint` also.
  * @param value Any value to check if it's a `'bigint'` type.
  * @returns boolean
- * @example https://www.typescriptlang.org/play?jsx=0#code/MYewdgzgLgBAlhAcgVwLYCMCmAnGBeGACgDcBDAG2UwC4ZSwBPASlrMs3ghjDS1zwB8MKAwAOmEADMYbKvjwEA5Dww5FMAGQbOAMThg4UTCQpUmAbgBQoSLADKAFQBKASUQBxAPoA1AIIAZAFUAUXwYRX8QbExUeFEINAB+RSsbaBhEQIBZACFgpx8AkLCAJgB2VPB0nIB5Gv9g30RCoNCCSQoITErbGByXdzcHFuKCAE4ABgmygEYxsZKAVgAWMuWJ+ZmwHogQckwAOnIQAHNCAANHVw8R4POAGk4UVWxCK7cvP1amCxgAej+MA65C61iqe0OxzO50yuXytweT14OEIsLyBS+IR+5n+gKg2CoYMgEKOpwutXqjWamLujwQzz4hApDSat2xuKBnUwRN2+1J0P6g0QwxpiPpyNegqGbN+AM5IO5QA
+ * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAkgzgIQJYHMYDthQLxQBQBuAhgDYCuEAXFEeiAJQ4B8Ux50ScUARqkpgG4AUAGMA9ujhZOyNJmrxZGLLkKkK1Wg2psKUTjz6ZmUUJDEAzVuujY7UAOS8U-YA+HjJWAMoAVAEowAHIA4gD6AGoAggAyAKoAojiOMWIAThAAtvpgcGSZAPzuohJSUEFxALIICf6RsYnJAEwA7B6lWAgA8l0xCVFB9fFJuBakcBDtXlAIMCHBvkONuACcAAxrLQCMKytNAKwALC2Ha7tb6FNwYiQQAHQkYih4AAZ+gaFLCS8ANPqIqGUeHewXC0WG9HoAigAHoYVAxiQJiVJDd7o9ni8KtVal9fv8lJg8NianVwYlIdC4QjxhAUddbg8nq9ur1+oNyd8-jJAUTWX0Bl9KbD4YjkZ4GejmS9ZvMgotOfieXJgHhZQshVCRaY0hQhEA
  */
 export const isBigInt: IsBigInt = (value: any): value is bigint => typeof value === 'bigint';
