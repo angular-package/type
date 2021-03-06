d8480507621c7345ed2c686a584415c84de3aee8
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:24:42 +0100

    refactor(is): add symbol, undefined, bigint, boolean

--- packages/type/src/check/lib/is.object.ts
+++ packages/type/src/check/lib/is.object.ts
@@ -5,14 +5,22 @@ import { isObject } from './is-object.func';
 import { isPrimitive } from './is-primitive.func';
 import { isString } from './is-string.func';
 import { isType } from './is-type.func';
-import { Is } from '../interface/is-interface';
+import { Is } from '../interface/is.interface';
+import { isUndefined } from './is-undefined.func';
+import { isBoolean } from './is-boolean.func';
+import { isBigInt } from './is-big-int.func';
+import { isSymbol } from './is-symbol.func';
 
 export const is: Partial<Is> = {
   array: isArray,
+  bigInt: isBigInt,
+  boolean: isBoolean,
   function: isFunction,
   number: isNumber,
   object: isObject,
   primitive: isPrimitive,
+  symbol: isSymbol,
   string: isString,
-  type: isType
+  type: isType,
+  undefined: isUndefined
 };
