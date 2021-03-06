a9fc7defd51c08fcb4e263ffa69040c263a7dbcd
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:25:52 +0100

    refactor(index): export new features `are`, `areString`, `isBigInt`, `isBoolean`

--- packages/type/src/check/index.ts
+++ packages/type/src/check/index.ts
@@ -1,6 +1,11 @@
+export { are } from './lib/are.object';
 export { is } from './lib/is.object';
 
+export { areString } from './lib/are-string.func';
+
 export { isArray } from './lib/is-array.func';
+export { isBigInt } from './lib/is-big-int.func';
+export { isBoolean } from './lib/is-boolean.func';
 export { isFunction } from './lib/is-function.func';
 export { isNumber } from './lib/is-number.func';
 export { isObject } from './lib/is-object.func';
