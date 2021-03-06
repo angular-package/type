1f2850854cf55284f7ec77fe8f7807fcfe5112c6
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-06 22:26:57 +0100

    refactor(guard): guard working with `is`.

--- packages/type/src/guard/lib/guard.object.ts
+++ packages/type/src/guard/lib/guard.object.ts
@@ -1,19 +1,7 @@
 import { Guard } from '../interface/guard-interface';
-import { guardArray } from './guard-array.func.type';
-import { guardFunction } from './guard-function.func';
-import { guardNumber } from './guard-number.func';
-import { guardObjectKey } from './guard-object-key.func';
-import { guardPrimitive } from './guard-primitive.func';
-import { guardString } from './guard-string.func';
-import { guardType } from './guard-type.func';
+import { Partial } from './../../lib/partial.type';
+import { guardIs } from './guard-is.object';
 
 export const guard: Partial<Guard> = {
-  array: guardArray,
-  function: guardFunction,
-  number: guardNumber,
-  objectKey: guardObjectKey,
-  object: guardObjectKey,
-  primitive: guardPrimitive,
-  string: guardString,
-  type: guardType
+  is: guardIs
 };
