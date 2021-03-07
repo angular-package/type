65916c3a273481709dbddce3aacfabbecab5ce20
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-07 01:42:34 +0100

    refactor(isType): uses all checkers depends on type argument.

--- packages/type/src/is/lib/is-type.func.ts
+++ packages/type/src/is/lib/is-type.func.ts
@@ -1,13 +1,32 @@
 import { IsType } from '../type/is-type.type';
-import { is } from './is.object';
 import { Types } from '../../type/types.type';
+import { isString } from './is-string.func';
+import { isBigInt } from './is-big-int.func';
+import { isBoolean } from './is-boolean.func';
+import { isNumber } from './is-number.func';
+import { isSymbol } from './is-symbol.func';
+import { isUndefined } from './is-undefined.func';
+import { isObject } from './is-object.func';
+import { isFunction } from './is-function.func';
+import { isClass } from './is-class.func';
 /**
- * Checks if any `value` is a generic constructor type or primitive type.
+ * Checks if any `value` is a generic type constructor, function, object or primitive type.
  * Use the `guardType()` to type-guard generic type `Type` also.
  * @param value Any value to check if it's a generic type from one of the `type`.
  * @param type Constructor generic `Type` or one of the `Primitives`
  * `'bigint'`, `'boolean'`, `'number'`, `'symbol'`, `'string'`, `'undefined'` to check `value` type.
- * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAwg9gOwM7AE4FcDGw6oDwAq4EAfFALxQIQDuUAFAHTMCGqA5kgFxQsIgBtALoBKCmSKQA3AChQkKAAVUASwC2K4CoBuEJBSgByAEZw4AGwh9DUAD5HjK9ioTAb9wwnRrjEVO6MUVQR2Q1l5aEk9PAB5YwArMkp4ZDQsHHw4xLslVQ0tXSRZTEQUKBUkAGU0F3YDem0Wc3QIHj4QER5G5ugKqCDa8SgIuAAzKG6WinJKQwGQsJkS1PKkKINCYhIGppa2-gAaYeIeKKRNyBJOid3e-XXyMgrq4PZ6CLEAfmPIMZue6aUCJQLq3cqpPiYCB-CLFcwsJD6FJBdBoOAweGIqAAbygAA8DABGKRQAC+S1KwFgABkAIKVSoAfQAarTqQBVACiBmodGRaTRGIRSHoImKlKgACEYjFqZzaQA5FlsrkGUZNJAQcUrSUASQA4rqFQRlRzuZQAJwABitAHZCRaLQAmACsABZbW6rY7CQhtWUFeyALKSzkAJVNqsohKdtv9VMqBDDRv1kfNRiDICo3l8qFWi2WSAsEEY5jgbwABvyMILMUgK0cKlE8NXUah0XXtjA6Qy00dW7XhSIxVAAPSj4YYCAU5DF0vl+gV6Wy+VK1lmhurZumYt8bbLuWKvsOMyWazDknjyctGdFyzzyt6w3GtObpvEPDzdj7g1Gk3rrkjhMJwXDcC8xwnNJp0LOcy0rQMQ3DRkCAACTDTlOTfNYPy8Hw-G2BDQwjADOSA3Dc0McCryg29YIXCtCKQgAxGJ2TDLDmy-bZE2TBVUxIoCv0okdqKnGQgA
+ * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAwg9gOwM7AE4FcDGw6oDwAq4EAfFALxQIQDuUAFAHTMCGqA5kgFxQsIgBtALoBKCmSKQA3AChQkKAAVUASwC2K4CoBuEJBSgByAEYr2KhMENQAPkeNw4AGwh9rdwwnRrjEVO6MkEB9nAMMUVQR2MPQEABMIADMLCDjDWXloADFYzAMmZjA2FjUePhAxcjJyjOIoST08AHljACsySnhkNCwcfBb22yVVDS1dJFqFAEkkBoNCYhJ6bRYndAgy-gAaKEyeBqQFyBJKshW16BV9BtkLYD9ElkxoJqmoAG8oAA8eLx8-KAAX1ulgeTxerw+UBAPAiFnYQNkMkwiBQUCuACEzFNLPlzutNhUePjLvpTOZcVVdsQ4IkoCSKORKCYzHd0sjUcB0UgMY4XHw8asCbx+CJiULSVAHM5XAhxPSJejunxnrSoLyZQKbHZ6MtFUzKD1oNqFRdGZRHk4kBAxAAyW3UyBqhkG+x82WGESyFHdbkwJwsJD6SjNNpLOBtCDYQk7PawTkYbC4UPtMVQCOtKNcq5QAbyjNZpUoFUQNWZb2c7kAOW8vlQgouhLTDJzfzr8syzv1TKMbb81gA-NyshZNBA9RcxOKzRZiwhVXSa-9UBXfVcAMpoeEN4XlZuKnNwqIdml0l098JbqLsn1oq4AVXiSRScR3GxFRNN625UFiCWS1CvlS56UH+z6AVA9qOqWZ7dsyYEAakN6VlcAyFiGeaUOUSwkjGUAANYQCAA6wle7D7jO+iYWQnawWarqGAW2DWFBLbKvOMG5pG2BQEOG5kfQhEVLxBFEUqX7QDwRpQDwlrWqud5IDk85aIgb5NtO345speRUrREnmkYiS5KpCAsQ6bFzguUA6aZCnZkg67BA4TjqR+FFafoQQhK5emngZDHeS57IcmuszEDwMxzCGDQ4RKeGZCR9TEIcsUeZK0VkO8MhQOidL0PxkTsPQmQiGI2W5blSA0JomAABYMKVHw5ZVuWYIG0AshSVg8KgEDAOgqBypi2KWBO6xei1rXtdabqamZvX9YNw08u6fDjTasitW1HW9rW-aLQNQ3VvtqAbZN21QDNnVHtEh3Ldym5FedW3bddgTOaE93HRun1OC9U2Ve9hgIS+hjfStj7-i+AOtYCLXw7lfVHXKmQiVc-qBqliwbbGxBTsOJkqIg522HYqHccARykADwIyMiAZBlA65wGoECY0znxfAYACMUhAldjP6CzbMEDQcAc-onwgAYABM-Pw7eXIwE0VbrgQABK94wAQTQawY1B0CL7NC-Qk1K7Aqvq1rOt6wQADqTQG7QzOsxAYsS6b5uVhiTRNAAMgAogAglWBhyRA9nqlMADiUxVgQBgAJwAAwpwA7DzSdJ7LACsAAs6f5yn2c8wgUdZPeVYwDwOn5D8VCnac3xQAA1FAfOhWiVb3gAshigf65QPOy+nUdNBiABSgc6zwkKUFzPA84iXdchP0+2wQAASg9z-PUIwkYiAQNYdMW9b8cxwYhi9zLfb1lcyG+uuACa-cBwYTk+fQhhqCAnpRyrgAEUDlkeOgcgEGFBoBJEt4ZSMCcHAYqAADFWatNba11hrZBOwrgNDwMbSWSw0HW0wXbR2OxCFCzKvzAA9LQqAEcu7wMQSg32AcQ5VhwdyfB0p+QICWOwoOocdgmDWmZGhUB6G7AwBAZhLgEFIPoMgjEsd44EG4Xg4geBbqCLUQnUR5I2SSOkUaeREBFEoJ7v3QemjwqQDwPfJY1iB4a1EffT0XopEMLMXAhRrDlGV2rnY-BOklhBJgF40xsjzGWOURfKsMcQnaN0fQBJMdRG3U8XQnxMS-EWICcg9eM8NG4PsRAZorwljFJ1iY3J6xYmFNfu-f2ySHGpOab7f2mS-rZO8TIhp+S4nIOAaA8BQC2njlGWAqsEDRHQKQnUgZcigA
  */
-export const isType: IsType = <Type>(value: any, type: Types<Type>): value is Type =>
-  is.string(type) ? is[type](value) :  value instanceof type;
+export const isType: IsType = <Type>(value: any, type?: Types<Type>): value is Type => {
+  if (isString(type)) {
+    switch (type) {
+      case 'bigint': return isBigInt(value);
+      case 'boolean': return isBoolean(value);
+      case 'number': return isNumber(value);
+      case 'string': return isString(value);
+      case 'symbol': return isSymbol(value);
+      case 'undefined': return isUndefined(value);
+    }
+  }
+  return type ? isClass<Type>(value, type) : isFunction(value) || isObject<Type>(value);
+};
