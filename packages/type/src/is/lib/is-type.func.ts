c9e00977de8206e497d17faaf1c01d7ab890eb37
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-07 03:20:11 +0100

    refactor(isType): use 'function' and 'object' in `type`.

--- packages/type/src/is/lib/is-type.func.ts
+++ packages/type/src/is/lib/is-type.func.ts
@@ -10,23 +10,24 @@ import { isObject } from './is-object.func';
 import { isFunction } from './is-function.func';
 import { isClass } from './is-class.func';
 /**
- * Checks if any `value` is a generic type constructor, function, object or primitive type.
- * Use the `guardType()` to type-guard generic type `Type` also.
- * @param value Any value to check if it's a generic type from one of the `type`.
- * @param type Constructor generic `Type` or one of the `Primitives`
- * `'bigint'`, `'boolean'`, `'number'`, `'symbol'`, `'string'`, `'undefined'` to check `value` type.
- * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAwg9gOwM7AE4FcDGw6oDwAq4EAfFALxQIQDuUAFAHTMCGqA5kgFxQsIgBtALoBKCmSKQA3AChQkKAAVUASwC2K4CoBuEJBSgByAEYr2KhMENQAPkeNw4AGwh9rdwwnRrjEVO6MkEB9nAMMUVQR2MPQEABMIADMLCDjDWXloADFYzAMmZjA2FjUePhAxcjJyjOIoST08AHljACsySnhkNCwcfBb22yVVDS1dJFqFAEkkBoNCYhJ6bRYndAgy-gAaKEyeBqQFyBJKshW16BV9BtkLYD9ElkxoJqmoAG8oAA8eLx8-KAAX1ulgeTxerw+UBAPAiFnYQNkMkwiBQUCuACEzFNLPlzutNhUePjLvpTOZcVVdsQ4IkoCSKORKCYzHd0sjUcB0UgMY4XHw8asCbx+CJiULSVAHM5XAhxPSJejunxnrSoLyZQKbHZ6MtFUzKD1oNqFRdGZRHk4kBAxAAyW3UyBqhkG+x82WGESyFHdbkwJwsJD6SjNNpLOBtCDYQk7PawTkYbC4UPtMVQCOtKNcq5QAbyjNZpUoFUQNWZb2c7kAOW8vlQgouhLTDJzfzr8syzv1TKMbb81gA-NyshZNBA9RcxOKzRZiwhVXSa-9UBXfVcAMpoeEN4XlZuKnNwqIdml0l098JbqLsn1oq4AVXiSRScR3GxFRNN625UFiCWS1CvlS56UH+z6AVA9qOqWZ7dsyYEAakN6VlcAyFiGeaUOUSwkjGUAANYQCAA6wle7D7jO+iYWQnawWarqGAW2DWFBLbKvOMG5pG2BQEOG5kfQhEVLxBFEUqX7QDwRpQDwlrWqud5IDk85aIgb5NtO345speRUrREnmkYiS5KpCAsQ6bFzguUA6aZCnZkg67BA4TjqR+FFafoQQhK5emngZDHeS57IcmuszEDwMxzCGDQ4RKeGZCR9TEIcsUeZK0VkO8MhQOidL0PxkTsPQmQiGI2W5blSA0JomAABYMKVHw5ZVuWYIG0AshSVg8KgEDAOgqBypi2KWBO6xei1rXtdabqamZvX9YNw08u6fDjTasitW1HW9rW-aLQNQ3VvtqAbZN21QDNnVHtEh3Ldym5FedW3bddgTOaE93HRun1OC9U2Ve9hgIS+hjfStj7-i+AOtYCLXw7lfVHXKmQiVc-qBqliwbbGxBTsOJkqIg522HYqHccARykADwIyMiAZBlA65wGoECY0znxfAYACMUhAldjP6CzbMEDQcAc-onwgAYABM-Pw7eXIwE0VbrgQABK94wAQTQawY1B0CL7NC-Qk1K7Aqvq1rOt6wQADqTQG7QzOsxAYsS6b5uVhiTRNAAMgAogAglWBhyRA9nqlMADiUxVgQBgAJwAAwpwA7DzSdJ7LACsAAs6f5yn2c8wgUdZPeVYwDwOn5D8VCnac3xQAA1FAfOhWiVb3gAshigf65QPOy+nUdNBiABSgc6zwkKUFzPA84iXdchP0+2wQAASg9z-PUIwkYiAQNYdMW9b8cxwYhi9zLfb1lcyG+uuACa-cBwYTk+fQhhqCAnpRyrgAEUDlkeOgcgEGFBoBJEt4ZSMCcHAYqAADFWatNba11hrZBOwrgNDwMbSWSw0HW0wXbR2OxCFCzKvzAA9LQqAEcu7wMQSg32AcQ5VhwdyfB0p+QICWOwoOocdgmDWmZGhUB6G7AwBAZhLgEFIPoMgjEsd44EG4Xg4geBbqCLUQnUR5I2SSOkUaeREBFEoJ7v3QemjwqQDwPfJY1iB4a1EffT0XopEMLMXAhRrDlGV2rnY-BOklhBJgF40xsjzGWOURfKsMcQnaN0fQBJMdRG3U8XQnxMS-EWICcg9eM8NG4PsRAZorwljFJ1iY3J6xYmFNfu-f2ySHGpOab7f2mS-rZO8TIhp+S4nIOAaA8BQC2njlGWAqsEDRHQKQnUgZcigA
+ * Checks if any `value` is a generic `Type` type constructor, `'function'`, `'object'` or primitive type.
+ * Use the `guardType()` to type-guard generic `Type` type also.
+ * @param value Any value to check if it's a generic `Type` type from one of the `type`.
+ * @param type Generic constructor `Type`, `'function'`, `'object'` or one of the `Primitives` `'bigint'`, `'boolean'`, `'number'`, `'symbol'`, `'string'`, `'undefined'` to check `value` type.
+ * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAwg9gOwM7AE4FcDGw6oDwAq4EAfFALxQIQDuUAFAHTMCGqA5kgFxQsIgBtALoBKCmSKQA3AChQkKAAVUASwC2K4CoBuEJBSgByAEYr2KhMENQAPkeNw4AGwh9rdwwnRrjEVO6MkEB9nAMMUVQR2MPQEABMIADMLCDjDWXloADFYzAMmZjA2FjUePhAxcjJyjOIoST08AHljACsySnhkNCwcfBb22yNE3K1EMLg2iGwA5XVNHT1ahQBJJAaDQmISem0WJ3QIMv4AGihMngakLcgSSrI9g+gVfQbZC2A-RJZMaCaVqAAbygAA8eF4fH4oABfd6WL4-P7-IFQEA8CIWdgw2QyTCIFBQF4AITMK0s+Ueh2OFR4lOe+lM5nJVXOxDgiSgdIo5EoJjMH3SuPxwEJSCJjhcfAp+ypvH4IlpMvpUAczlcCHEnKVhO6fF+7Kg4rVUpsdnou21PMoPWgpq1T25lG+TiQEDEADJ3azIAauVb7BL1YYRLI8d1RTAnCwkPpKM02jtJq1psBqWcLrBhRhsLh4+0FVAkynRVABpqi9gdSg9RADZlQ8LRQA5by+VDSp7Ugtcl5UVtQlmZX2WnlGCFt6wAflFWQsmggFqeYkVDos1YQ+o5LchqAb4ZeAGU0JiO7Lyt3tb2MVFNUOOX7R+Fj1FBWGCS8AKrxJIpOKno5yjS9qHCWsQJMk1B-iyD6UGBP6QVAnrerW94jrycEQakr6Ni8AzFnGZaUOUOx0mmUAANYQCAk7os+7AXqu+iEWQd7AdA-qGBWViIV6Pa6huKGllMlbToedH0JRFRQNOkk6mxUA8DaClQM6rp7u+SA5BuYwapQi5nvKK4gb2Wl5IObKoQ6HEjNpKjjDx8lrsANYGqZOnqSKh7BA4Tj-l2RnKkEIS+eZPqWSBHFBT5gpCvu6zEDwawbHGDQkUqZEZlcNykAxxmvHULKAjIUCEhy9BiZE7D0JkIhiEVJUlUgNCaJgAAWDA1UCxUNSVmDRtAfJMlYPCoBAwDoKgGrEqSlj6W6sg9b1-UBsaCCGCNY0TVNYqBnwc0ht1PV9a6wyjHZa0beNk0zmdiD7Qti3HQN45+OtUCjVd23bm292HQ1T1GFxb0fVtop4dg2VpUuD1HctT6VcDm3XRVmK-YtUAA+E3mhJdoNecFaOPXDGG-ojn2il+4G-oTJXQt1dMlSD12ZNJEZRjGkNzemxDLip+xqTIsIyLi7P6AecBqBAkbRvowIggYACMUgwhjotQOLksEDQcDSzGKIgAYABMyt02+IowE0TYHgQABKH4wAQTQ2wY1B0BrUui-QB1m7AlvW3bDtOwQADqTQu7Q6sSxAWs6573uNkSTRNAAMgAogAgk2BiqRAHmGisADiKxNgQBgAJwAAwVwA7ArZdl4bACsAAs1fNxX9cKwgedZB+TYwDwpn5GCfY7vcoJQAA1FASuxQSTYfgAskSqfO5QCuG9XedNESABSqcOzwyKUHLPAK9ic8ijv++BwQAASq9H8f+s8Jx1DWELPv+8XBcGIYi8Gxeu2F42FwwHgAJrLxTgYA82MnD0EMGoEAwY859wACKpyyMXVOaCDAk0gjiN8apGBODgFVAABhbK2tt7aOxtuQs4LwGh4HdrrJAOwqH+1oUHUOZxWGi1qsrAA9EIvmLoIBz2IaQihicU4ZybAw0UzDVSSgQDsWRadM5nBMLtNagioAiPOBgCRRCXAkLIfQchRJC7FwIIophxA8DXnYOomxJdtGMgFPowxNpJFmOkZYhey9V72PipAPAQCdhBJXjbbRQDgwhgMaI3xpiIDmIob3fuoTmGmR2JkmA2ibLYHOgk4RyTjF+LSQE8h38mwF2yY45xOxakF20c40pSSjGHEqekyx18D52MYWEiAzR-g7H6Q7bRQNvHlO6ak3pNTIGJ2Tg08JTT6AQKgcnNpcCOk+IqfM6p6DMHYLQashcxysFNhwdo-BWEZldIkUAA
  */
-export const isType: IsType = <Type>(value: any, type?: Types<Type>): value is Type => {
+export const isType: IsType = <Type>(value: any, type: Types<Type>): value is Type => {
   if (isString(type)) {
     switch (type) {
       case 'bigint': return isBigInt(value);
       case 'boolean': return isBoolean(value);
+      case 'function': return isFunction(value);
       case 'number': return isNumber(value);
+      case 'object': return isObject<Type>(value);
       case 'string': return isString(value);
       case 'symbol': return isSymbol(value);
       case 'undefined': return isUndefined(value);
     }
   }
-  return type ? isClass<Type>(value, type) : isFunction(value) || isObject<Type>(value);
+  return type ? isClass<Type>(value, type) : false;
 };
