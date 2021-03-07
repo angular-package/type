4fb020f69c4ee23430610a713812a5f0704e5b6f
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-07 02:24:25 +0100

    refactor: add 'function' and 'object' type for features.

--- packages/type/src/type/types.type.ts
+++ packages/type/src/type/types.type.ts
@@ -1,3 +1,3 @@
 import { Constructor } from './constructor.type';
 import { Primitives } from './primitives.type';
-export type Types<Obj> = Constructor<Obj> | Primitives;
+export type Types<Obj> = Constructor<Obj> | 'function' | 'object' | Primitives;
