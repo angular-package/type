39882ebc6837e317967d383fef4ed8fd96b57e2c
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-09 00:18:00 +0100

    refactor(guardNumber): use GuardNumber type.

--- packages/type/src/guard/lib/guard-number.func.ts
+++ packages/type/src/guard/lib/guard-number.func.ts
@@ -1,8 +1,9 @@
 import { isNumber } from '../../is/lib/is-number.func';
+import { GuardNumber } from '../type/guard-number.type';
 /**
  * Guard the `value` to be a `number` type.
  * Use `isNumber()` function for check ONLY.
  * @param value Type `number` value to guard.
  * @returns boolean
  */
-export const guardNumber = (value: number): value is number => isNumber(value);
+export const guardNumber: GuardNumber = (value: number): value is number => isNumber(value);
