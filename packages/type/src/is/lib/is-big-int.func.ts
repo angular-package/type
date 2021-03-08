233d3548d03ac049562b517b05eba2ed63c63c3b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-08 23:32:04 +0100

    docs(isBigInt): update.

--- packages/type/src/is/lib/is-big-int.func.ts
+++ packages/type/src/is/lib/is-big-int.func.ts
@@ -1,8 +1,7 @@
 import { IsBigInt } from '../type/is-big-int.type';
 /**
  * Checks if any `value` is a `'bigint'` type.
- * Use the `guardBigInt()` function to type-guard `bigint` also.
- * @param value Any value to check if it's a `'bigint'` type.
+ * @param value Any `value` to check if it's a `'bigint'` type.
  * @returns boolean
  */
 export const isBigInt: IsBigInt = (value: any): value is bigint => typeof value === 'bigint';
