17984457b03e29b3edae9707e5ad95a8077d6ee7
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-08 22:02:11 +0100

    refactor(isFunction): add instanceof Object to check.

--- packages/type/src/is/lib/is-function.func.ts
+++ packages/type/src/is/lib/is-function.func.ts
@@ -1,9 +1,10 @@
 import { IsFunction } from '../type/is-function.type';
 import { Func } from '../../type/func.type';
 /**
- * Checks if any `value` is a `'function'` type and `Function` instance.
- * @param value Any `value` to check if it's a `'function'` type and `Function` instance.
+ * Checks if any `value` is a `'function'` type, instance of `Function` and `Object`.
+ * @param value Any `value` to check if it's a `'function'` type, instance of `Function` and `Object`.
  * @returns boolean.
- * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAYgrgOwMZQLxQBQDodgIYBOeAtgFxR4IgCUaAfBVQNwBQokUAkgM7zLABLAPYI0mAG54ANnAjlKNelEkzoA7rERJWSEd2BR1fJIJHkex06PQYVs+VWrk7ajcaXsIQgGbLpstFR0AHJvLStgqAAyKL9VQwR9SiQvX0thBB0pPG4NAGE9YAI4EyECPOzcqABvKAAPMQBWJigAXxZdRIMAIQB5XoAZAFEAQQA5MSLZHUKobs4AcU4xgBUxAE4ABk2AdgBGdfWAJkaAFh3TzcO9zI7ZsYBVAFluoYAlMT2jnZmuqABlFZvZYLMTBJ4gKAIODEABGEAIhm4wV++lgDzGeXI7hsdXI0LhCKcjEhqAYtQIEGAcAIogaAGooM02nc-stAeM8kMxAgIAB3KAFLrFUrlSrcDDUVFCKQQLBSIQAcwwAAMhfoRcAyhUctwVQAaJHpEQYdVTUU63LUKVQAD0tqgUwgrO4MrlCuVwXmS1WwUNRnCGQw3uWK2tLXtUG80m4zs6rtl8qVGC9-WG4z9RsDJr6g1GY3Ddod0aksZdbqTnseL3emYD-CD1deb0LkZLZfjFY9KcBwLGCzrvGzCAwvZBreLMbjei7yZVMAxeQNWYbJoXmInjuK08Ss897JWnKGg+NI4PR837YgQA
+ * @example https://www.typescriptlang.org/play?target=7&jsx=0#code/C4TwDgpgBAYgrgOwMZQLxQBQDodgIYBOeAtgFxR4IgCUaAfBVQNwBQokUAkgM7zLABLAPYI0mAG54ANnAjlKNelEkzoA7rERJWSKXm4aAwnoNQA3lAAeYgKxMoAXxZIR3YFHV8kgkeR5efUXQMFVl5KmpyULUNLyV2CCEAM2VpWTRUdAByJK1ArKgAMkLU1Q8EN0okRJSA4VFi0vSBCuAqmqgAeQAjACsIbx1XdwAhTs6AGQBRAEEAOTFgAlkh1qgRzgBxTjmAFTEATgAGI4B2AEYDg4AmGwAWU7ujq-OEVbcoOYBVAFkRqYASmJztdTu93ABlXYAnabMRZH4gKAIODEboQAgebhZcGwL5zQzkOLBSzkFFojGRRhI1AMCwECDAOAEUTWADUUDsjmcwy4cyh80MUzECAgAHcoMZ9NwMNR3kIpBAsFIhABzDAAAylBg1ABosXURBhtdxqHKoAB6C1QJayHkVBVKlXqrIbbZ7LL6zx5eoYN07XZm+xWqBJaTcCD27iO5VqjCu8bTeaeg0+o1jSazOZBy3WsNSCNRmPO+PfP6AlPe-i+sv-AE5kP5wsuB2K2MuqEwuabSu8NMIDCd2ENvPhyMt6NtksamD4wx61PVo2zgkjm3LceuYtxrI7AUEqa9w0Dve7QVTNdNiBAA
  */
-export const isFunction: IsFunction = (value: any): value is Func => typeof value === 'function' && value instanceof Function;
+export const isFunction: IsFunction = (value: any): value is Func =>
+  typeof value === 'function' && value instanceof Function && value instanceof Object;
