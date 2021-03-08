f8711762ef8fa92dcc5919c5506559c76af1ca59
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-08 22:05:10 +0100

    refactor(isString): checks `String` instance with `Object`.

--- packages/type/src/is/lib/is-string.func.ts
+++ packages/type/src/is/lib/is-string.func.ts
@@ -1,9 +1,10 @@
 import { IsString } from '../type/is-string.type';
 /**
- * Checks if any `value` is a `'string'` type or `String` instance.
+ * Checks if any `value` is a `'string'` type or `'object'` type and instance of `String` and `Object`.
  * Use the`guardString()` function to type-guard `string`  also.
- * @param value Any `value` to check if it's a `'string'` type or `String` instance.
+ * @param value Any `value` to check if it's a `'string'` type or `'object'` type and instance of `String` and `Object`.
  * @returns boolean.
- * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAkgzgZWAJwJYDsDmUC8UAUAbgIYA2ArhAFxTHogCUuAfFCRdKnFHChpgG4AUAGMA9ul5QuSNFhrxZ-XAXaUadRjTWduvOdhysd0ycDoiIYgGZQlWKAB9HUUJBtsylXDjwByfX4-YVEJKQQAFQAlGAA5AHEVPwAZMWQIAFtpMDhyDIB+YNCzKFiAVQBZACEAUSiVACYAdmFxEqqAeQ7kmoBBWJVrMjgIVrDgUpqAdUiYhJV0CAB3Oz4sfBS0zOzcrIAjUmIjgAs-BhC2uDFSCAA6UjFMfAADWbj454AaaUQ1p7eEgxzlAAPQg1zISjFK43e6PF7lap1L4-exPRG1KJAgSg8FDUgjaHXO4PJ7PTrdPqxFEyP74Ck9frY3FQfGEy7EuFk2LTAEfb60gz4HkzaLvZlgqAAEQgwAgIgmS1QwGOpl4Fis1iEQA
+ * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAkgzgZWAJwJYDsDmUC8UAUAbgIYA2ArhAFxTHogCUuAfFCRdKnFHChpgG4AUAGMA9ul5QuSNFhrxZ-XAXaUadRjTWduvOdhzMhUAqEhiAZmzKVcOPAHIxAIwBWEEcEdQAZL5sOaUlgOhEIKygAeXdPYD8AnWDeMIjrJSwmAB8sqHM0wLsHJ31+R2FRCSkEABUAJRgAOQBxFUcAGTFkCABbaTA4ch6AfnLKkKhGgFUAWQAhAFE6lQAmAHZhcQm5qKj2hYBBRpVLMjgITar42oaWgH0m2qOAYQWVDMx8G6bmhkuJ773RoLADqD0aT0arxU6AgAHcoB8vvUfn8hOM4GJSBAAHSkMSfAAGOz2h0ahIANNJEHwsPgSfsjgw-lAAPSsqCnUjnDFY3H4onTeZLSnUpFCxZ1ZkCNkcrk8raY7F4gn4QmA5qimS0z4a6WyvLISi85UCtUa8GQ15amkGZG3ZqWmovBb69mG42KvkqokW4Fgx7OqELG1Iv2gp0ut0clDGoA
  */
-export const isString: IsString = (value: any): value is string => value instanceof String || typeof value === 'string';
+export const isString: IsString = (value: any): value is string =>
+  (typeof value === 'object' && value instanceof Object && value instanceof String) || typeof value === 'string';
