695c47a01e3c3c1b6d8d100daa768b3486b6fcac
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-08 22:00:59 +0100

    refactor(isBoolean): add checks to instance Boolean.

--- packages/type/src/is/lib/is-boolean.func.ts
+++ packages/type/src/is/lib/is-boolean.func.ts
@@ -7,4 +7,4 @@ import { IsBoolean } from '../type/is-boolean.type';
  * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAkgzgIQPZIDYQIYDsoF4oAUAbhqgK4QBcU2IAlHgHxQnnQCWcUARiutgG4AUAGMkWOMCidkaTFmrxZ-HPmKkK1WnWqsK0rrznYmQqCw0cJwbCIhIAZlGXyoAHzeF1bPLnzAAJ30PCx8-fAdSOAgGADJYqFBIR1D9cKgAciMVDLphUXFJKABlABUAJRgAOQBxPEyAGSQAiABbaTA4MlaAfgzhMWsoKoBVAFkEAFFy+oAmAHYBwqkEAHlVhsmAQSr6yNRopaG1je2qgH0Kkcn6wIojopPNnfPqsp2AYRv8LAgAd2cfHkBH20TyQgKEjkADpUEgAOYEAAGZUqtSRABoDC5sARUdUanQ8lAAPQkqCgiCQuAwuGIpGjCbTTHYoG4xlTcpEgSk8mU6m0hHIp5nFkyNlYAginbc3mJIJUwY09CwoVI6UXK6TMWICVS9bPTXla6ysnyigClV04UGs6vKrvKpfHU4yUa+2Or6m8l3KlAA
  */
 export const isBoolean: IsBoolean = (value: any): value is boolean =>
-  value instanceof Boolean || ((value === true || value === false) && typeof value === 'boolean');
+  (value instanceof Object && value instanceof Boolean) || ((value === true || value === false) && typeof value === 'boolean');
