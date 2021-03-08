fcc1bc1246cff076a690419fefff355911a969b2
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-08 22:05:45 +0100

    recactor(isNumber): add instanceof `Object`.

--- packages/type/src/is/lib/is-number.func.ts
+++ packages/type/src/is/lib/is-number.func.ts
@@ -1,10 +1,10 @@
 import { IsNumber } from '../type/is-number.type';
 /**
- * Checks if any `value` is a `'number'` type or `Number` instance.
+ * Checks if any `value` is a `'number'` type or instance of `Number` and `Object`.
  * Use the `guardNumber()` function to type-guard `number` also.
  * @param value Any value to check if it's a `'number'` type.
  * @returns boolean
- * @example https://www.typescriptlang.org/play?jsx=0#code/C4TwDgpgBAkgzgOQK4FsBGEBOUC8UAUAbgIYA2SEAXFMQHYgCUuAfFCedAJZxS2oaYA3ACgAxgHtacYFG7J0WavHkDcBdhWp1G1DVx58F2HM2FQooSOIBmbMhVw48AckMDnUAPyy4AMU60nMAQRPYQTLphslLAdKIQNlAqWCISMVAAygAqAEowCADiAPoAagCCADIAqgCias4V4pgQKLJgcKiezqmS0klVALIAQjU5pZW1agBMAOw96UMA8osVNWUI49V1eNZkcBDzfUMwBflZm5N4AJwADDczAIxXV1MArAAsM+83zw+0h+JSBAAHSkcQAc3wAANsnlChcalCADQ+ZKYfCw-LFcpbBgMQRQAD0hKgu1I+zEvUBILBkKhCEGIzGONqyNR-Cw+AZw1GCLxBOJFkwFEpUmpoIh0KWKzWGxZiJRcg56Olq3WfPxRJJZIpaTg4tpUpOZwRbKVRnwx1OCHO8v5WtJewgwiAA
+ * @example https://www.typescriptlang.org/play?target=7&jsx=0#code/C4TwDgpgBAkgzgOQK4FsBGEBOUC8UAUAbgIYA2SEAXFMQHYgCUuAfFCedAJZxS2oaYA3ACgAxgHtacYFG7J0WavHkDcBdhWp1G1DVx58F2HM2FQooSOIBmbMhVw48AckMDnUAPyy4AMU60nMAQRPYQTLphslLAdKIQNlAqWFAAZKl2HNHScQm2APJoAFYQosAiEjFQAEIwAOIwCAAqagCcAAztAOwAjK2tAEwArAAsXSPt-T20FZLSNfn5ADIAogCCCGrWZHAQs1UIAKoAstUrAEpqA13780enFwD6jQDKTRsAwitqtBAA7kl+Fh8EMGLcZG9zo06mpnEtxJgIChZGA4KhPM5buJSBAAHSkcQAc3wAANag1miSADQ+ZKYfDkxpNBhgqAAejZUG2pF2Yjm2LxBOJZMWqw21NpQPp1VF6wQLME7M53N5lTgAvxRNJ9zO5wlcil+B1FwVSosmAofKkGqF2pOuueCDenxW+sQhuN50dzoQX1NHPNlrVNq1JMh0LddPw4YQdX9yp2EGEwiAA
  */
 export const isNumber: IsNumber = (value: any): value is number =>
-  typeof value === 'number' ? isFinite(value) : value instanceof Number;
+  typeof value === 'number' ? isFinite(value) : value instanceof Number && value instanceof Object;
