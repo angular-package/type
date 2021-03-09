94bf13c1ecc47308d9801e18721acfff5b692e72
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-09 04:52:40 +0100

    refactor(api): update.

--- packages/type/src/public-api.ts
+++ packages/type/src/public-api.ts
@@ -1,19 +1,44 @@
+import { isInstance } from './is/lib/is-instance.func';
+import { isNull } from './is/lib/is-null.func';
 /*
  * Public API Surface of type
  */
-
-// `guard` object and functions.
-export { guard, guardArray, guardFunction, guardNumber, guardObject, guardObjectKey, guardPrimitive, guardString, guardType }
-    from './guard';
+// `guard` object.
+export { guard } from './guard';
+// `guard` functions.
+export {
+    guardArray,
+    guardFunction,
+    guardNumber,
+    guardObject,
+    guardObjectKey,
+    guardPrimitive,
+    guardString,
+    guardType
+} from './guard';
 
 // `are` object and functions.
 export { are, areString } from './are';
 
-// `is` functions.
-export { isArray, isBigInt, isBoolean, isClass, isFunction, isNumber, isObject, isPrimitive, isString, isSymbol, isType, isUndefined } from './is';
-
 // `is` object.
 export { is } from './is';
+
+// `is` functions.
+export {
+    isArray,
+    isBigInt,
+    isBoolean,
+    isInstance,
+    isFunction,
+    isNumber,
+    isNull,
+    isObject,
+    isPrimitive,
+    isString,
+    isSymbol,
+    isType,
+    isUndefined
+} from './is';
 
 // Types.
 export { Constructor, CycleHook, Func, Primitive, Primitives, Types } from './type';
