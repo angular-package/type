bfb8d827881724920640167523d73bb19206a273
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-14 13:47:05 +0100

    refactor(is): export `isDefined`.

--- packages/type/src/is/index.ts
+++ packages/type/src/is/index.ts
@@ -2,8 +2,9 @@
 export { isArray } from './lib/is-array.func';
 export { isBigInt } from './lib/is-big-int.func';
 export { isBoolean } from './lib/is-boolean.func';
-export { isInstance } from './lib/is-instance.func';
+export { isDefined } from './lib/is-defined.func';
 export { isFunction } from './lib/is-function.func';
+export { isInstance } from './lib/is-instance.func';
 export { isNumber } from './lib/is-number.func';
 export { isNull } from './lib/is-null.func';
 export { isObject } from './lib/is-object.func';
