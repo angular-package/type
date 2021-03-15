0f1b6265353d695f042e8afdf616d2f3bcded6c7
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-15 06:42:33 +0100

    refactor(isBoolean): add use `typeOf` and add typeof to instance object check.

--- packages/type/src/is/lib/is-boolean.func.ts
+++ packages/type/src/is/lib/is-boolean.func.ts
@@ -1,9 +1,19 @@
 import { IsBoolean } from '../type/is-boolean.type';
+import { typeOf } from '../../lib/type-of.func';
 /**
  * Checks if any `value` is a `'boolean'` type  or instance of `Boolean` and `Object`.
  * @param value Any `value` to check if it's a `'boolean'` type or instance of `Boolean` and `Object`.
  * @returns boolean.
  */
 export const isBoolean: IsBoolean = (value: any): value is boolean =>
-  (value instanceof Boolean && value instanceof Object) ||
-  ((value === true || value === false) && typeof value === 'boolean');
+  typeOf(value) === 'boolean' &&
+  (
+    typeof value === 'object'  &&
+    value instanceof Boolean === true &&
+    value instanceof Object === true
+    ||
+    value instanceof Boolean === false &&
+    value instanceof Object === false &&
+    typeof value === 'boolean' &&
+    (value === true || value === false)
+  );
