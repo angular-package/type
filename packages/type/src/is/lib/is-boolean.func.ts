0025af44491d441c2cd7b89cd2c851dfdefe500b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-15 11:42:05 +0100

    refactor(isBoolean): change checks, update description.

--- packages/type/src/is/lib/is-boolean.func.ts
+++ packages/type/src/is/lib/is-boolean.func.ts
@@ -1,19 +1,24 @@
 import { IsBoolean } from '../type/is-boolean.type';
 import { typeOf } from '../../lib/type-of.func';
 /**
- * Checks if any `value` is a `'boolean'` type  or instance of `Boolean` and `Object`.
- * @param value Any `value` to check if it's a `'boolean'` type or instance of `Boolean` and `Object`.
+ * Checks if any `value` is a `'boolean'` type not instance of `Boolean` and `Object` or `'object'` type instance of `Boolean` and `Object`.
+ * @param value Any `value` to check if it's a `'boolean'` type not instance of `Boolean` and `Object`
+ * or `'object'` type instance of `Boolean` and `Object`.
  * @returns boolean.
  */
 export const isBoolean: IsBoolean = (value: any): value is boolean =>
   typeOf(value) === 'boolean' &&
   (
-    typeof value === 'object'  &&
-    value instanceof Boolean === true &&
-    value instanceof Object === true
+    (
+      typeof value === 'object' &&
+      value instanceof Boolean === true &&
+      value instanceof Object === true
+    )
     ||
-    value instanceof Boolean === false &&
-    value instanceof Object === false &&
-    typeof value === 'boolean' &&
-    (value === true || value === false)
+    (
+      value instanceof Boolean === false &&
+      value instanceof Object === false &&
+      typeof value === 'boolean' &&
+      (value === true || value === false)
+    )
   );
