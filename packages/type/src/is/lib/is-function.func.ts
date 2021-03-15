5ae2ef93b6988443a4fce40a1a7853af0c3ab199
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-15 11:52:25 +0100

    refactor(isFunction): use `typeOf()`.

--- packages/type/src/is/lib/is-function.func.ts
+++ packages/type/src/is/lib/is-function.func.ts
@@ -1,9 +1,13 @@
 import { IsFunction } from '../type/is-function.type';
 import { Func } from '../../type/func.type';
+import { typeOf } from '../../lib/type-of.func';
 /**
  * Checks if any `value` is a `'function'` type, instance of `Function` and `Object`.
  * @param value Any `value` to check if it's a `'function'` type, instance of `Function` and `Object`.
  * @returns boolean.
  */
 export const isFunction: IsFunction = (value: any): value is Func =>
-  typeof value === 'function' && value instanceof Function && value instanceof Object;
+  typeOf(value) === 'function' &&
+  typeof value === 'function' &&
+  value instanceof Function === true &&
+  value instanceof Object === true;
