a575deefa3d7b30cd633df78e64206ba3c0a6558
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-15 11:47:34 +0100

    refactor(isDefined): order check, update description.

--- packages/type/src/is/lib/is-defined.func.ts
+++ packages/type/src/is/lib/is-defined.func.ts
@@ -1,11 +1,11 @@
 import { IsDefined } from '../type/is-defined.type';
 import { typeOf } from '../../lib/type-of.func';
 /**
- * Checks if unknown `value` is NOT a `'undefined'` type.
- * @param value Any `value` to check if it's NOT an `'undefined'` type.
+ * Checks if unknown `value` is NOT a `'undefined'` type and is not `undefined`.
+ * @param value Unknown `value` to check if it's NOT an `'undefined'` type and is not `undefined`.
  * @returns boolean.
  */
 export const isDefined: IsDefined = (value: unknown): boolean =>
-  value !== undefined &&
+  typeOf(value) !== 'undefined' &&
   typeof value !== 'undefined' &&
-  typeOf(value) !== 'undefined';
+  value !== undefined;
