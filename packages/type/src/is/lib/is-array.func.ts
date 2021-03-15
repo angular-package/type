dd562587b0a2cca51aaa63065d7e3c9ddeb55324
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-15 11:29:40 +0100

    refactor(isArray): use `typeOf()`, change check order, update description.

--- packages/type/src/is/lib/is-array.func.ts
+++ packages/type/src/is/lib/is-array.func.ts
@@ -1,13 +1,13 @@
 import { IsArray } from '../type/is-array.type';
 import { typeOf } from '../../lib/type-of.func';
 /**
- * Checks if any `value` is an `Array` of a generic `Type` type and `Array` instance.
+ * Checks if any `value` is an `Array` of an `'object'` generic `Type` type and `Array` instance.
  * Use the `guardArray()` function to type-guard `array` also.
- * @param value Any `value` to check if it's an `Array` of a generic type `Type` and `Array` instance.
+ * @param value Any `value` to check if it's an `Array` of an `'object'` generic `Type` type and `Array` instance.
  * @returns boolean.
  */
 export const isArray: IsArray = <Type>(value: any): value is Array<Type> =>
+  typeOf(value) === 'array' &&
   Array.isArray(value) === true &&
   value instanceof Array === true &&
-  typeof value === 'object' &&
-  typeOf(value) === 'array';
+  typeof value === 'object';
