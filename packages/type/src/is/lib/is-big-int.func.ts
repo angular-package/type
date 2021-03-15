17db2b250b91b79e4f1fcaf405106db33b009472
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-15 06:41:33 +0100

    refactor(isBigInt): use `typeOf`.

--- packages/type/src/is/lib/is-big-int.func.ts
+++ packages/type/src/is/lib/is-big-int.func.ts
@@ -1,7 +1,8 @@
 import { IsBigInt } from '../type/is-big-int.type';
+import { typeOf } from '../../lib/type-of.func';
 /**
  * Checks if any `value` is a `'bigint'` type.
  * @param value Any `value` to check if it's a `'bigint'` type.
  * @returns boolean
  */
-export const isBigInt: IsBigInt = (value: any): value is bigint => typeof value === 'bigint';
+export const isBigInt: IsBigInt = (value: any): value is bigint => typeof value === 'bigint' && typeOf(value) === 'bigint';
