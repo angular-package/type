8f8e307a9fc998cdc98069661426b12b9cd63337
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-15 12:00:52 +0100

    refactor(isNull): use `typeOf()`.

--- packages/type/src/is/lib/is-null.func.ts
+++ packages/type/src/is/lib/is-null.func.ts
@@ -1,7 +1,11 @@
 import { IsNull } from '../type/is-null.type';
+import { typeOf } from '../../lib/type-of.func';
 /**
  * Checks if any `value` is a `'object'` type and equal `null`.
  * @param value Any `value` to check if it's a `null` value and `'object'` type.
  * @returns boolean
  */
-export const isNull: IsNull = (value: any): value is null => value === null && typeof value === 'object';
+export const isNull: IsNull = (value: any): value is null =>
+  typeOf(value) === 'null' &&
+  typeof value === 'object' &&
+  value === null;
