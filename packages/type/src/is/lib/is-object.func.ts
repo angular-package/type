5f783a3913f1e3b700ef24186e40ef7a36024e0e
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-14 01:28:03 +0200

    refactor(isObject): Generic `Obj` equal to `object`, update description.

--- packages/type/src/is/lib/is-object.func.ts
+++ packages/type/src/is/lib/is-object.func.ts
@@ -5,13 +5,11 @@ import { typeOf } from '../../lib/type-of.func';
  * Checks if any `value` is a generic `Obj` `'object'` type and `Object` instance with the possibility of containing `key`.
  * Use the `guardObject()` function to type-guard generic object type also.
  * @param value Any `value` to check if it's a generic `'object'` type and `Object` instance.
- * @param [key] Property name to find in argument `value`.
+ * @param key Name to find in argument `value`.
  * @returns boolean
  */
-export const isObject: IsObject = <Obj>(value: any, key?: string): value is Obj =>
-  typeOf(value) === 'object' &&
-  typeof value === 'object' &&
-  value instanceof Object === true
+export const isObject: IsObject = <Obj = object>(value: any, key?: string): value is Obj =>
+  (typeOf(value) === 'object' && typeof value === 'object' && value instanceof Object === true)
     ? isString(key)
       ? key in value
     : true
