ac70cf84e8e105998e0eba2231d6aca34e8a7564
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-14 01:19:13 +0200

    refactor(isString): add callback function, use isStringObject and isStringType functions.

--- packages/type/src/is/lib/is-string.func.ts
+++ packages/type/src/is/lib/is-string.func.ts
@@ -1,25 +1,19 @@
-import { IsString } from '../type/is-string.type';
+import { errorCallback } from '../../lib/error-callback.func';
+import { isStringObject } from './is-string-object.func';
+import { isStringType } from './is-string-type.func';
 import { typeOf } from '../../lib/type-of.func';
+// Type.
+import { IsString } from '../type/is-string.type';
+import { ResultCallback } from '../../type/result-callback.type';
+
 /**
  * Checks if any `value` is a `'string'` type, not instance of `Object` and `String`
  * or `'object'` type and instance of `String` and `Object`.
  * Use the`guardString()` function to type-guard `string`  also.
  * @param value Any value to check if it's a `'string'` type, not an instance of `Object` and `String`
  * or `'object'` type and instance of `String` and `Object`.
+ * @param callback ResultCallback function to handle result before return.
  * @returns boolean.
  */
-export const isString: IsString = (value: any): value is string =>
-  typeOf(value) === 'string' &&
-  (
-    (
-      typeof value === 'object' &&
-      value instanceof Object === true &&
-      value instanceof String === true
-    )
-    ||
-    (
-      value instanceof Object === false &&
-      value instanceof String === false &&
-      typeof value === 'string'
-    )
-  );
+export const isString: IsString = (value: any, callback: ResultCallback = errorCallback): value is string =>
+  callback(typeOf(value) === 'string' && (isStringObject(value) || isStringType(value)));
