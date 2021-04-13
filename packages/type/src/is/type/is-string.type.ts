915c01b9fbfe9daac60fcc3284a4fa9ec5c208ec
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-14 01:23:55 +0200

    refactor(IsString): add callback.

--- packages/type/src/is/type/is-string.type.ts
+++ packages/type/src/is/type/is-string.type.ts
@@ -1,1 +1,2 @@
-export type IsString = (value: any) => value is string;
+import { ResultCallback } from '../../type/result-callback.type';
+export type IsString = (value: any, callback?: ResultCallback) => value is string;
