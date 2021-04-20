7204cf331cfa7c6bef122723a2e7525b5f678078
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-20 23:46:12 +0200

    refactor(isKey): check number as first.

--- packages/type/src/is/lib/is-key.func.ts
+++ packages/type/src/is/lib/is-key.func.ts
@@ -10,4 +10,4 @@ import { Key } from '../../type/key.type';
  * @param value Any `value` to check.
  * @returns A `boolean` indicating whether or not the `value` is a `Key` type.
  */
-export const isKey: IsKey = (value: any): value is Key => isString(value) || isNumber(value) || isSymbol(value);
+export const isKey: IsKey = (value: any): value is Key => isNumber(value) || isString(value) || isSymbol(value);
