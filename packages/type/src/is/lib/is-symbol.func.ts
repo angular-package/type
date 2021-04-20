2a49333b0eae8511f8abff80357cf29d484f90d4
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-21 00:54:28 +0200

    docs(isSymbol): update.

--- packages/type/src/is/lib/is-symbol.func.ts
+++ packages/type/src/is/lib/is-symbol.func.ts
@@ -3,7 +3,7 @@ import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { IsSymbol } from '../type/is-symbol.type';
 /**
- * Checks if any `value` is a `'symbol'` type.
+ * Checks if any `value` is a `symbol` type.
  * @param value Any `value` to check.
  * @returns A `boolean` indicating whether or not the `value` is a `symbol`.
  */
