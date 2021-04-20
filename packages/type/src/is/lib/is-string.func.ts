d4d65c2f03170fab8da6f7299199abfbd47c4107
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-21 00:46:49 +0200

    docs(isString): update.

--- packages/type/src/is/lib/is-string.func.ts
+++ packages/type/src/is/lib/is-string.func.ts
@@ -5,15 +5,11 @@ import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { IsString } from '../type/is-string.type';
 import { ResultCallback } from '../../type/result-callback.type';
-
 /**
- * Checks if any `value` is a `'string'` type, not instance of `Object` and `String`
- * or `'object'` type and instance of `String` and `Object`.
- * Use the`guardString()` function to type-guard `string`  also.
- * @param value Any value to check if it's a `'string'` type, not an instance of `Object` and `String`
- * or `'object'` type and instance of `String` and `Object`.
- * @param callback ResultCallback function to handle result before return.
- * @returns boolean.
+ * Checks if any `value` is a `string` type, not instance of `Object` and `String` or `object` type and instance of `String` and `Object`.
+ * @param value Any `value` to check.
+ * @param callback `ResultCallback` function to handle result before return.
+ * @returns A `boolean` indicating whether or not the `value` is a `string`.
  */
 export const isString: IsString = (value: any, callback: ResultCallback = errorCallback): value is string =>
   callback(typeOf(value) === 'string' && (isStringObject(value) || isStringType(value)));
