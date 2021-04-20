989e49b4d7d9afb24633d4a98a8ddad9e5f9f8ba
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-20 23:08:11 +0200

    refactor(isInstance): update.
    - reorder imports.
    - use `isFunction` to check `instance`.
    - update jsdoc.

--- packages/type/src/is/lib/is-instance.func.ts
+++ packages/type/src/is/lib/is-instance.func.ts
@@ -1,13 +1,19 @@
+// Function.
+import { isObject } from './is-object.func';
+import { isString } from './is-string.func';
+// Type.
 import { Constructor } from '../../type/constructor.type';
 import { IsInstance } from '../type/is-instance.type';
-import { isString } from './is-string.func';
-import { isObject } from './is-object.func';
+import { isFunction } from './is-function.func';
 /**
- * Checks if any `value` is a generic `Obj` type `constructor` instance and is an `Object`.
- * @param value Any generic `Obj` type `value` instance to compare with `type` instance.
- * @param instance Creates generic `Obj` type instance to compare with argument `value`.
+ * Checks if any `value` is an `object` of a generic `Obj` type equal to an `instance` of `Constructor` type.
+ * @param value Any `value` to compare with the `instance`.
+ * @param instance The name of the generic `Obj` type to create an `instance` to compare with the `value`.
+ * @returns  A `boolean` indicating whether or not the `value` is an `instance`.
  */
 export const isInstance: IsInstance = <Obj>(value: any, instance: Constructor<Obj>): value is Obj =>
-  isObject<Obj>(value) &&
-  value instanceof instance === true &&
-  isString(instance.prototype.constructor.name);
+  isObject<Obj>(value) ?
+    isFunction(instance) ?
+      value instanceof instance === true && isString(instance.prototype.constructor.name)
+    : false
+  : false;
