a9f54c70983f3428fa6970a3e184666357e83e76
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-20 12:35:14 +0200

    chore(isBigInt): update jsdoc, reorder imports.

--- packages/type/src/is/lib/is-big-int.func.ts
+++ packages/type/src/is/lib/is-big-int.func.ts
@@ -1,9 +1,11 @@
-import { IsBigInt } from '../type/is-big-int.type';
+// Function.
 import { typeOf } from '../../lib/type-of.func';
+// Type.
+import { IsBigInt } from '../type/is-big-int.type';
 /**
- * Checks if any `value` is a `'bigint'` type.
- * @param value Any `value` to check if it's a `'bigint'` type.
- * @returns boolean
+ * Checks if any `value` is a `bigint` type.
+ * @param value Any `value` to check.
+ * @returns A `boolean` indicating whether or not the `value` is a `bigint`.
  */
 export const isBigInt: IsBigInt = (value: any): value is bigint =>
   typeOf(value) === 'bigint' &&
