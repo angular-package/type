2fea29358541cdc82b03bd2521ec9f7bf5d8cf99
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-20 12:32:06 +0200

    chore(isArray): upgrade jsdoc, reorder import.

--- packages/type/src/is/lib/is-array.func.ts
+++ packages/type/src/is/lib/is-array.func.ts
@@ -1,10 +1,11 @@
-import { IsArray } from '../type/is-array.type';
+// Function.
 import { typeOf } from '../../lib/type-of.func';
+// Type.
+import { IsArray } from '../type/is-array.type';
 /**
- * Checks if any `value` is an `Array` of `'object'` generic `Type` type and `Array` instance.
- * Use the `guardArray()` function to type-guard `array` also.
- * @param value Any `value` to check if it's an `Array` of `'object'` generic `Type` type and `Array` instance.
- * @returns boolean.
+ * Checks if any `value` is an `Array`, `Array` instance and `object` type.
+ * @param value Any `value` to check.
+ * @returns A `boolean` indicating whether or not the `value` is an `Array`.
  */
 export const isArray: IsArray = <Type>(value: any): value is Array<Type> =>
   typeOf(value) === 'array' &&
