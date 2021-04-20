1a59555be8b8081ba84701296d3324760ecc1aa6
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-20 23:17:50 +0200

    refactor(isKey): update.
    - reorder imports.
    - import `Key` type.
    - boolean result is `Key`.
    - update jsdoc.

--- packages/type/src/is/lib/is-key.func.ts
+++ packages/type/src/is/lib/is-key.func.ts
@@ -1,13 +1,13 @@
 // Function.
-import { isString } from './is-string.func';
 import { isNumber } from './is-number.func';
+import { isString } from './is-string.func';
 import { isSymbol } from './is-symbol.func';
 // Type.
 import { IsKey } from '../type/is-key.type';
-
+import { Key } from '../../type/key.type';
 /**
- * Determines if any `value` is one of the string, number, or symbol type.
- * @param value Any value to check.
- * @returns A boolean indicating whether or not the value is one of the string, number or symbol type.
+ * Determines if any `value` is one of the `string`, `number`, or `symbol`.
+ * @param value Any `value` to check.
+ * @returns A `boolean` indicating whether or not the `value` is a `Key` type.
  */
-export const isKey: IsKey = (value: any): boolean => isString(value) || isNumber(value) || isSymbol(value);
+export const isKey: IsKey = (value: any): value is Key => isString(value) || isNumber(value) || isSymbol(value);
