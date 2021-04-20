9fbc21909a330e64829c8b681c3e4784ea8fc564
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-20 23:44:22 +0200

    chore(isNull): reorders imports, update jsdoc.

--- packages/type/src/is/lib/is-null.func.ts
+++ packages/type/src/is/lib/is-null.func.ts
@@ -1,9 +1,11 @@
-import { IsNull } from '../type/is-null.type';
+// Function.
 import { typeOf } from '../../lib/type-of.func';
+// Type.
+import { IsNull } from '../type/is-null.type';
 /**
- * Checks if any `value` is an `'object'` type and equal to `null`.
- * @param value Any `value` to check if it's a `null` value and an `'object'` type.
- * @returns boolean
+ * Checks if any `value` is an `object` type and equal to `null`.
+ * @param value Any `value` to check.
+ * @returns A `boolean` indicating whether or not the `value` is `null`.
  */
 export const isNull: IsNull = (value: any): value is null =>
   typeOf(value) === 'null' &&
