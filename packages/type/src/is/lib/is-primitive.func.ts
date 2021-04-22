80679d64e06fb66ce0fb11e642c54859360df963
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-22 14:00:45 +0200

    refactor(): add check, update imports, update jsdoc
    - add check `null`
    - reorder alphabetically imports
    - categorize imports
    - update jsdoc `returns`, `type` and general description

--- packages/type/src/is/lib/is-primitive.func.ts
+++ packages/type/src/is/lib/is-primitive.func.ts
@@ -1,16 +1,19 @@
-import { IsPrimitive } from '../type/is-primitive.type';
-import { Primitives } from '../../type/primitives.type';
-import { isString } from './is-string.func';
+// Function.
 import { isBigInt } from './is-big-int.func';
 import { isBoolean } from './is-boolean.func';
+import { isNull } from './is-null.func';
 import { isNumber } from './is-number.func';
+import { isString } from './is-string.func';
 import { isSymbol } from './is-symbol.func';
 import { isUndefined } from './is-undefined.func';
+// Type.
+import { IsPrimitive } from '../type/is-primitive.type';
+import { Primitives } from '../../type/primitives.type';
 /**
- * Checks if any `value` is a generic type from one of the `Primitives`.
- * Use the `guardPrimitive()` function to type-guard generic type also.
- * @param value Any `value` to check if it's a generic type from the argument `type`.
- * @param type One of the `Primitives` `'bigint'`, `'boolean'`, `'number'`, `'string'`, `'symbol'`, `'undefined'` type to check `value`.
+ * Checks if any `value` is a generic `Type` from the `Primitives`.
+ * @param value Any `value` to check if it's a type from the `type`.
+ * @param type Name of the type from the `Primitives` to check the `value`.
+ * @returns A `boolean` indicating whether or not the `value` is a type from the `Primitives`.
  */
 export const isPrimitive: IsPrimitive = <Type>(value: any, type: Primitives): value is Type => {
   if (isString(type)) {
@@ -18,6 +21,7 @@ export const isPrimitive: IsPrimitive = <Type>(value: any, type: Primitives): va
       case 'bigint': return isBigInt(value);
       case 'boolean': return isBoolean(value);
       case 'number': return isNumber(value);
+      case 'null': return isNull(value);
       case 'string': return isString(value);
       case 'symbol': return isSymbol(value);
       case 'undefined': return isUndefined(value);
