d03b9dca960732d3f3c13e304b6a4352543aa275
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-22 17:29:04 +0200

    refactor(Primitives): add 'null' type.

--- packages/type/src/type/primitives.type.ts
+++ packages/type/src/type/primitives.type.ts
@@ -1,1 +1,1 @@
-export type Primitives = 'bigint' | 'boolean' | 'number' | 'symbol' | 'string' | 'undefined';
+export type Primitives = 'bigint' | 'boolean' | 'null' | 'number' | 'symbol' | 'string' | 'undefined';
