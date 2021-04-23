a97daa5087d46cdb8bc5bf39e1017a3227975d5a
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-23 18:27:09 +0200

    chore: remove unnecessary `notDefined`

--- packages/type/src/is/test/variables.const.ts
+++
@@ -1,8 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
-/**
- * Check typeof === 'undefined', 'boolean', 'function', 'number', 'object', 'string', 'bigint', 'symbol'
- * Check instanceof String, Boolean, Number, Function, Object
- */
-export let notDefined: any; // typeof === 'undefined'
-
-
