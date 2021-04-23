e8eab026bca053b011e6a3a9b9f3565828305059
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-23 12:44:41 +0200

    chore(isUndefined): update jsdoc, imports
    - categorize and sort alphabetically imports
    - update `value` and `returns` field jsdoc

--- packages/type/src/is/lib/is-undefined.func.ts
+++ packages/type/src/is/lib/is-undefined.func.ts
@@ -1,9 +1,11 @@
-import { IsUndefined } from '../type/is-undefined.type';
+// Function.
 import { typeOf } from '../../lib/type-of.func';
+// Type.
+import { IsUndefined } from '../type/is-undefined.type';
 /**
- * Checks if any `value` is an `'undefined'` type and equal to `undefined`.
- * @param value Any `value` to check if it's an `'undefined'` type and equal to `undefined`.
- * @returns boolean.
+ * Checks if any `value` is an `undefined` type and equal to `undefined`.
+ * @param value Any `value` to check.
+ * @returns A `boolean` indicating whether or not the `value` is `undefined`.
  */
 export const isUndefined: IsUndefined = (value: any): value is undefined =>
   typeOf(value) === 'undefined' &&
