79e855bb44682c2577a2b92736f22a08928160e3
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-23 12:52:07 +0200

    chore(is): update
    Added
    - `isKey` function in `key` property
    - `objectKey` function in `objectKey` property
    
    Changed
    - `bigInt` will be removed in favor of `bigint` to have strict names as `Primitives`
    - reorder alphabetically imports

--- packages/type/src/is/lib/is.object.ts
+++ packages/type/src/is/lib/is.object.ts
@@ -1,34 +1,39 @@
-// Interface.
-import { Is } from '../interface/is.interface';
-// Functions.
+// Function.
 import { isArray } from './is-array.func';
 import { isBigInt } from './is-big-int.func';
 import { isBoolean } from './is-boolean.func';
 import { isDefined } from './is-defined.func';
 import { isFunction } from './is-function.func';
 import { isInstance } from './is-instance.func';
+import { isKey } from './is-key.func';
 import { isNull } from './is-null.func';
 import { isNumber } from './is-number.func';
 import { isObject } from './is-object.func';
+import { isObjectKey } from './is-object-key.func';
 import { isPrimitive } from './is-primitive.func';
 import { isString } from './is-string.func';
 import { isSymbol } from './is-symbol.func';
 import { isType } from './is-type.func';
 import { isUndefined } from './is-undefined.func';
-// Objects.
+// Object.
 import { isNot } from '../not/lib/is-not.object';
+// Interface.
+import { Is } from '../interface/is.interface';
 
 export const is: Is = {
   array: isArray,
-  bigInt: isBigInt,
+  bigint: isBigInt,
+  bigInt: isBigInt, // deprecated
   boolean: isBoolean,
   defined: isDefined,
   function: isFunction,
   instance: isInstance,
+  key: isKey,
   not: isNot,
   null: isNull,
   number: isNumber,
   object: isObject,
+  objectKey: isObjectKey,
   primitive: isPrimitive,
   string: isString,
   symbol: isSymbol,
