0fb76c3b8db23d4b0e25218952f98507db1042bf
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-25 18:31:44 +0200

    refactor(isBooleanObject): update
    - add `IsBooleanObject` type
    - use `callback` param with default `errorCallback`
    - add jsdoc

--- packages/type/src/is/lib/is-boolean-object.func.ts
+++ packages/type/src/is/lib/is-boolean-object.func.ts
@@ -1,4 +1,15 @@
-export const isBooleanObject = (value: any) =>
-  typeof value === 'object' &&
-  value instanceof Boolean === true &&
-  value instanceof Object === true;
+// Function.
+import { errorCallback } from '../../lib/error-callback.func';
+// Type.
+import { IsBooleanObject } from '../type/is-boolean-object.type';
+import { ResultCallback } from '../../type/result-callback.type';
+/**
+ * Checks if any `value` is an `object` type and instance of `Boolean` and `Object`.
+ * @function `isBooleanObject`
+ * @param value Any `value` to check.
+ * @param callback `ResultCallback` function to handle result before returns.
+ * @callback `errorCallback`.
+ * @returns A `boolean` indicating whether or not the `value` is a `Boolean` instance.
+ */
+export const isBooleanObject: IsBooleanObject = (value: any, callback: ResultCallback = errorCallback): value is boolean =>
+  callback(typeof value === 'object' && value instanceof Boolean === true && value instanceof Object === true);
---
+++ packages/type/src/is/type/is-boolean-object.type.ts
@@ -0,0 +1,2 @@
+import { ResultCallback } from '../../type/result-callback.type';
+export type IsBooleanObject = (value: any, callback?: ResultCallback) => value is boolean;
