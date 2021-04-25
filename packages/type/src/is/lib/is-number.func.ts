227111f2dfe969ddd2d0d14524a4e8077eadd1b0
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-25 22:42:09 +0200

    refactor(isNumber): update
    - add `ResultCallback` parameter `callback` with default `errorCallback`

--- packages/type/src/is/lib/is-number.func.ts
+++ packages/type/src/is/lib/is-number.func.ts
@@ -1,15 +1,15 @@
 // Function.
+import { errorCallback } from '../../lib/error-callback.func';
 import { isNumberObject } from './is-number-object.func';
 import { isNumberType } from './is-number-type.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { IsNumber } from '../type/is-number.type';
+import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any `value` is a `number` type not an instance of `Number` and `Object` or `object` type instance of `Number` and `Object`.
  * @param value Any value to check.
  * @returns A `boolean` indicating whether or not the `value` is a `number`.
  */
-export const isNumber: IsNumber = (value: any): value is number =>
-  typeOf(value) === 'number' &&
-  isFinite(value) === true &&
-  (isNumberObject(value) || isNumberType(value));
+export const isNumber: IsNumber = (value: any, callback: ResultCallback = errorCallback): value is number =>
+  callback(typeOf(value) === 'number' && isFinite(value) === true && (isNumberType(value) || isNumberObject(value)));
