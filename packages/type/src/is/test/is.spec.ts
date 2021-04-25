9167f503ea266c178029178570c2a2665d55ec8e
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-25 19:05:02 +0200

    test(is): update

--- packages/type/src/is/test/is.spec.ts
+++ packages/type/src/is/test/is.spec.ts
@@ -3,7 +3,5 @@ import { is } from '../lib/is.object';
 
 describe('is.', () => {
    // TRUE
-   it('is DEFINED', () => {
-    expect(is).toBeDefined();
-   });
+   it('is DEFINED', () => expect(is).toBeDefined());
 });
