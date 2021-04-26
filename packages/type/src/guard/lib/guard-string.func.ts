47f746e3f80e686522f55bf4c3a4744a09af122b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 13:37:41 +0200

    docs(guardString): categorize imports, update jsdoc

--- packages/type/src/guard/lib/guard-string.func.ts
+++ packages/type/src/guard/lib/guard-string.func.ts
@@ -1,8 +1,10 @@
-import { GuardString } from '../type/guard-string.type';
+// Function.
 import { isString } from '../../is/lib/is-string.func';
+// Type.
+import { GuardString } from '../type/guard-string.type';
 /**
- * Guard the `value` to be a `string` type.
- * Use `isString()` function for check ONLY.
- * @param value String type value to guard.
+ * Guard the `value` to be a `string`.
+ * @param value A `string` type `value` to guard.
+ * @returns A `boolean` indicating whether or not the `value` is a `string`.
  */
 export const guardString: GuardString = (value: string): value is string => isString(value);
