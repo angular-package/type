c15af0451b6e748eecad11c9a59896cb8fa10613
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 12:26:38 +0200

    refactor(guardKey): update
    - change the return type
    - use `isKey` function to check
    - update jsdoc

--- packages/type/src/guard/lib/guard-key.func.ts
+++ packages/type/src/guard/lib/guard-key.func.ts
@@ -1,14 +1,11 @@
 // Function.
-import { isNumber } from '../../is/lib/is-number.func';
-import { isString } from '../../is/lib/is-string.func';
-import { isSymbol } from '../../is/lib/is-symbol.func';
+import { isKey } from '../../is/lib/is-key.func';
 // Type.
 import { GuardKey } from '../type/guard-key.type';
 import { Key } from '../../type/key.type';
-
 /**
- * Guard the `value` of `Key type to be one of the `string`, `number`, or `symbol` type.
- * @param value A generic `Key` type `value` to guard.
- * @returns A `boolean` indicating whether or not the `value` is one of the `string`, `number` or `symbol` type.
+ * Guard the `value` to be one of the `string`, `number`, or `symbol` type.
+ * @param value A `Key` type `value` to guard.
+ * @returns A `boolean` indicating whether or not the `value` is a `Key`.
  */
-export const guardKey: GuardKey = (value: Key): boolean => isString(value) || isNumber(value) || isSymbol(value);
+export const guardKey: GuardKey = (value: Key): value is Key => isKey(value);
