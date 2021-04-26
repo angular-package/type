0968a95ab7f0b54edf671203bda33b3569f95465
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 12:12:11 +0200

    refactor(guardArray): change parameter name to `value` and update jsdoc

--- packages/type/src/guard/lib/guard-array.func.ts
+++ packages/type/src/guard/lib/guard-array.func.ts
@@ -3,8 +3,8 @@ import { isArray } from '../../is/lib/is-array.func';
 // Type.
 import { GuardArray } from '../type/guard-array.type';
 /**
- * Guard the `array` value to be a generic `Array` of `Type`.
- * @param array A generic `Type` `Array` value to guard.
- * @returns A `boolean` indicating whether or not the `value` is an `Array` of `Type`.
+ * Guard the `value` to be an `Array` of a generic `Type`.
+ * @param value A generic `Type` `Array` `value` to guard.
+ * @returns A `boolean` indicating whether or not the `value` is an `Array` of a generic `Type`.
  */
-export const guardArray: GuardArray = <Type>(array: Array<Type>): array is Array<Type> => isArray<Type>(array);
+export const guardArray: GuardArray = <Type>(value: Array<Type>): value is Array<Type> => isArray<Type>(value);
