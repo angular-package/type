85bc471e8f53a19f3f3c54733f681b2cbbe01061
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 17:04:14 +0200

    refactor(isType): update
    - change a generic `Type` to use the `Type`
    - update jsdoc

--- packages/type/src/is/lib/is-type.func.ts
+++ packages/type/src/is/lib/is-type.func.ts
@@ -1,25 +1,21 @@
 // Function.
-import { isBigInt } from './is-big-int.func';
-import { isBoolean } from './is-boolean.func';
 import { isFunction } from './is-function.func';
 import { isInstance } from './is-instance.func';
-import { isNumber } from './is-number.func';
 import { isObject } from './is-object.func';
 import { isPrimitive } from './is-primitive.func';
 import { isString } from './is-string.func';
-import { isSymbol } from './is-symbol.func';
-import { isUndefined } from './is-undefined.func';
 // Type.
 import { IsType } from '../type/is-type.type';
+import { Type } from '../../type/type.type';
 import { Types } from '../../type/types.type';
 import { isNotNull } from '../not/lib/is-not-null.func';
 /**
- * Checks if any `value` is a generic `Type` from the `Types`.
+ * Checks if any `value` is a `Type` from the `type` of the `Types` type.
  * @param value Any `value` to check if its type is from the `type`.
- * @param type One type from the `Types` to check the `value`.
- * @returns A `boolean` indicating whether or not the `value` is a type from the `Types`.
+ * @param type A `string` or generic `Constructor` type from the `Types` to check the `value`.
+ * @returns A `boolean` indicating whether or not the `value` is a `type` from the `Types`.
  */
-export const isType: IsType = <Type>(value: any, type: Types<Type>): value is Type => {
+export const isType: IsType = <T extends Type>(value: any, type: Types<T>): value is T => {
   if (isString(type)) {
     switch (type) {
       // Primitives.
@@ -33,10 +29,10 @@ export const isType: IsType = <Type>(value: any, type: Types<Type>): value is Ty
       // Function.
       case 'function': return isFunction(value);
       // Object.
-      case 'object': return isObject<Type>(value);
+      case 'object': return isObject<T>(value);
     }
   } else if (isNotNull(type)) {
-    return isInstance<Type>(value, type);
+    return isInstance<T>(value, type);
   }
   return false;
 };
