953c75a1600d295e43a6ab03504c7d5e0ddf6072
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 12:16:49 +0200

    refactor(guardFunction): change parameter name to value, update jsdoc, categorize imports

--- packages/type/src/guard/lib/guard-function.func.ts
+++ packages/type/src/guard/lib/guard-function.func.ts
@@ -1,9 +1,11 @@
-import { GuardFunction } from '../type/guard-function.type';
+// Function.
 import { isFunction } from '../../is/lib/is-function.func';
+// Type.
+import { GuardFunction } from '../type/guard-function.type';
 import { Func } from '../../type/func.type';
 /**
- * Guard the `func` value to be a `Func` type.
- * @param func `Func` type value to guard.
- * @returns boolean.
+ * Guard the `value` to be a `Func` type.
+ * @param value Type `Func` `value` to guard.
+ * @returns A `boolean` indicating whether or not the `value` is a `Func` type.
  */
-export const guardFunction: GuardFunction = (func: Func): func is Func => isFunction(func);
+export const guardFunction: GuardFunction = (value: Func): value is Func => isFunction(value);
