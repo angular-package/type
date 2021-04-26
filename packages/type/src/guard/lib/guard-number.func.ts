cce8679eba2c3de275ee3b6ff5f4421bde4c7c5b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 12:30:48 +0200

    docs(guardNumber): categorize imports and update jsdoc `returns` and `value`

--- packages/type/src/guard/lib/guard-number.func.ts
+++ packages/type/src/guard/lib/guard-number.func.ts
@@ -1,9 +1,10 @@
+// Function.
 import { isNumber } from '../../is/lib/is-number.func';
+// Type.
 import { GuardNumber } from '../type/guard-number.type';
 /**
- * Guard the `value` to be a `number` type.
- * Use `isNumber()` function for check ONLY.
- * @param value Type `number` value to guard.
- * @returns boolean
+ * Guard the `value` to be a `number`.
+ * @param value A `number` type `value` to guard.
+ * @returns A `boolean` indicating whether or not the `value` is a `number`.
  */
 export const guardNumber: GuardNumber = (value: number): value is number => isNumber(value);
