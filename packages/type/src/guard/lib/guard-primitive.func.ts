a8ea20db3c448e42b557ccb13df87c7392853208
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 13:34:51 +0200

    refactor(guardPrimitive): update
    - `Type` extends `Primitive`
    - categorize imports
    - update jsdoc

--- packages/type/src/guard/lib/guard-primitive.func.ts
+++ packages/type/src/guard/lib/guard-primitive.func.ts
@@ -1,10 +1,14 @@
-import { GuardPrimitive } from '../type/guard-primitive.type';
-import { Primitives } from '../../type/primitives.type';
+// Function.
 import { isPrimitive } from '../../is/lib/is-primitive.func';
+// Type.
+import { GuardPrimitive } from '../type/guard-primitive.type';
+import { Primitive } from '../../type/primitive.type';
+import { Primitives } from '../../type/primitives.type';
 /**
- * Guard the `value` to be a generic `Type` from one of the `Primitives`.
- * Use `isPrimitive()` function for check ONLY.
- * @param value A generic `Type` type value to guard.
- * @param type One of the `Primitives` `'boolean'`, `'bigint'`, `'number'`, `'string'`, `'symbol'`, `'undefined'` to check `value`.
+ * Guard the `value` to be the `Type` from the `Primitives`.
+ * @param value A generic `Type` `value` to guard.
+ * @param type One of the `Primitives` to check the `value`.
+ * @returns A `boolean` indicating whether or not the `value` is the `Type` from the `Primitives`.
  */
-export const guardPrimitive: GuardPrimitive = <Type>(value: Type, type: Primitives): value is Type => isPrimitive(value, type);
+export const guardPrimitive: GuardPrimitive = <Type extends Primitive>(value: Type, type: Primitives): value is Type =>
+  isPrimitive<Type>(value, type);
