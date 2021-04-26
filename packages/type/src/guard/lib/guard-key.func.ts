c0576bb7b605e7eb3dcf54b7a4c3a8fa35e0bb76
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 12:27:53 +0200

    docs(guardKey): update

--- packages/type/src/guard/lib/guard-key.func.ts
+++ packages/type/src/guard/lib/guard-key.func.ts
@@ -4,7 +4,7 @@ import { isKey } from '../../is/lib/is-key.func';
 import { GuardKey } from '../type/guard-key.type';
 import { Key } from '../../type/key.type';
 /**
- * Guard the `value` to be one of the `string`, `number`, or `symbol` type.
+ * Guard the `value` to be one of the `string`, `number`, or `symbol`.
  * @param value A `Key` type `value` to guard.
  * @returns A `boolean` indicating whether or not the `value` is a `Key`.
  */
