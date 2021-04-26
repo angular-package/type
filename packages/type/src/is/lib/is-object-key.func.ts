1fe1841654e32f190a5e9963f237f63cb1ffc356
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 23:42:45 +0200

    refactor(isObjectKey): change param name to `value`, extends Type with `object` and update jsdoc

--- packages/type/src/is/lib/is-object-key.func.ts
+++ packages/type/src/is/lib/is-object-key.func.ts
@@ -6,16 +6,16 @@ import { isObject } from './is-object.func';
 import { IsObjectKey } from '../type/is-object-key.type';
 import { Key } from '../../type/key.type';
 /**
- * Checks if any `object` has its own specified keys of the `Key` type.
- * @param object Any `object` to check if it contains a specified `key`.
- * @param key `Key` type or an array of `Key` type to check.
- * @returns A `boolean` indicating whether or not the `object` has its own specified keys.
+ * Checks if any `value` is an `object` with its own specified keys of the `Key` type.
+ * @param value Any `value` to check if it contains a specified `key`.
+ * @param key A `Key` type or an array of `Key` type to check the `value`.
+ * @returns A `boolean` indicating whether or not the `value` has its own specified keys.
  */
-export const isObjectKey: IsObjectKey = <Type = object>(object: any, key: Key | Key[]): object is Type =>
-  isObject<Type>(object) ?
+export const isObjectKey: IsObjectKey = <Type extends object>(value: any, key: Key | Key[]): value is Type =>
+  isObject<Type>(value) ?
     isArray(key) ?
-      key.every(k => isKey(k) ? ({}).hasOwnProperty.call(object, k) === true : false)
+      key.every(k => isKey(k) ? ({}).hasOwnProperty.call(value, k) === true : false)
     : isKey(key) ?
-        ({}).hasOwnProperty.call(object, key)
+        ({}).hasOwnProperty.call(value, key)
       : false
   : false;
