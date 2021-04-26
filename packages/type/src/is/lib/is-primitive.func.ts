a2a53116f2c60e923ea9e85025c0bfa0d9406b12
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 16:52:17 +0200

    refactor(isPrimitive): update
    - change the generic `Type` to use `Primitive` type
    - update jsdoc

--- packages/type/src/is/lib/is-primitive.func.ts
+++ packages/type/src/is/lib/is-primitive.func.ts
@@ -8,14 +8,15 @@ import { isSymbol } from './is-symbol.func';
 import { isUndefined } from './is-undefined.func';
 // Type.
 import { IsPrimitive } from '../type/is-primitive.type';
+import { Primitive } from '../../type/primitive.type';
 import { Primitives } from '../../type/primitives.type';
 /**
- * Checks if any `value` is a generic `Type` from the `Primitives`.
- * @param value Any `value` to check if it's a generic `Type` from the `type`.
- * @param type Name of the type from the `Primitives` to check the `value`.
- * @returns A `boolean` indicating whether or not the `value` is a type from the `Primitives`.
+ * Checks if any `value` is a `Primitive` type from the `type` of the `Primitives` type.
+ * @param value Any `value` to check if it's a `Primitive` from the `type`.
+ * @param type A `string` name of the `type` from the `Primitives` to check the `value`.
+ * @returns A `boolean` indicating whether or not the `value` is a `type` from the `Primitives`.
  */
-export const isPrimitive: IsPrimitive = <Type>(value: any, type: Primitives): value is Type => {
+export const isPrimitive: IsPrimitive = <T extends Primitive>(value: any, type: Primitives): value is T => {
   if (isString(type)) {
     switch (type) {
       case 'bigint': return isBigInt(value);
