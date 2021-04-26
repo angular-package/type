d6510b405409b837eb89d2315d8be357e7d63e90
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 17:07:59 +0200

    refactor(guardObject): update
    - generic `Obj` extends `object`
    - update `value ` jsdoc

--- packages/type/src/guard/lib/guard-object.func.ts
+++ packages/type/src/guard/lib/guard-object.func.ts
@@ -4,7 +4,7 @@ import { isObject } from '../../is/lib/is-object.func';
 import { GuardObject } from '../type/guard-object.type';
 /**
  * Guard the `value` to be an `object` of a generic `Obj` type.
- * @param value A generic `Obj` type `value` to guard.
+ * @param value A generic `Obj` type value to guard.
  * @returns A `boolean` indicating whether or not the `value` is an `object` of a generic `Obj`.
  */
-export const guardObject: GuardObject = <Obj>(value: Obj): value is Obj => isObject<Obj>(value);
+export const guardObject: GuardObject = <Obj extends object>(value: Obj): value is Obj => isObject<Obj>(value);
