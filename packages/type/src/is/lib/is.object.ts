b43a7d11d1589e8229924b1c7db156fca50a3e99
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 18:48:15 +0200

    refactor(is): add functions
    - `bigint`
    - `booleanObject`
    - `booleanType`
    - `numberObject`
    - `numberType`
    - `stringObject`
    - `stringType`

--- packages/type/src/is/lib/is.object.ts
+++ packages/type/src/is/lib/is.object.ts
@@ -2,16 +2,22 @@
 import { isArray } from './is-array.func';
 import { isBigInt } from './is-big-int.func';
 import { isBoolean } from './is-boolean.func';
+import { isBooleanObject } from './is-boolean-object.func';
+import { isBooleanType } from './is-boolean-type.func';
 import { isDefined } from './is-defined.func';
 import { isFunction } from './is-function.func';
 import { isInstance } from './is-instance.func';
 import { isKey } from './is-key.func';
 import { isNull } from './is-null.func';
 import { isNumber } from './is-number.func';
+import { isNumberObject } from './is-number-object.func';
+import { isNumberType } from './is-number-type.func';
 import { isObject } from './is-object.func';
 import { isObjectKey } from './is-object-key.func';
 import { isPrimitive } from './is-primitive.func';
 import { isString } from './is-string.func';
+import { isStringObject } from './is-string-object.func';
+import { isStringType } from './is-string-type.func';
 import { isSymbol } from './is-symbol.func';
 import { isType } from './is-type.func';
 import { isUndefined } from './is-undefined.func';
@@ -19,12 +25,14 @@ import { isUndefined } from './is-undefined.func';
 import { isNot } from '../not/lib/is-not.object';
 // Interface.
 import { Is } from '../interface/is.interface';
-
+// `is`.
 export const is: Is = {
   array: isArray,
-  bigint: isBigInt,
   bigInt: isBigInt, // deprecated
+  bigint: isBigInt,
   boolean: isBoolean,
+  booleanObject: isBooleanObject,
+  booleanType: isBooleanType,
   defined: isDefined,
   function: isFunction,
   instance: isInstance,
@@ -32,10 +40,14 @@ export const is: Is = {
   not: isNot,
   null: isNull,
   number: isNumber,
+  numberObject: isNumberObject,
+  numberType: isNumberType,
   object: isObject,
   objectKey: isObjectKey,
   primitive: isPrimitive,
   string: isString,
+  stringObject: isStringObject,
+  stringType: isStringType,
   symbol: isSymbol,
   type: isType,
   undefined: isUndefined
