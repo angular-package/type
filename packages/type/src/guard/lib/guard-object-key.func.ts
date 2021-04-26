a37001bb0c846c30baa74561c9c94f997127c898
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 23:09:04 +0200

    refactor(guardObjectKey): extends `Obj` with `object` , update `value` jsdoc

--- packages/type/src/guard/lib/guard-object-key.func.ts
+++ packages/type/src/guard/lib/guard-object-key.func.ts
@@ -5,9 +5,9 @@ import { isKey } from '../../is/lib/is-key.func';
 import { GuardObjectKey } from '../type/guard-object-key.type';
 /**
  * Guard the `value` to be an `object` of a generic `Obj` type that contains the `key` property of the `Key` type.
- * @param value A generic `Obj` type `value` that contains the `key` property to guard.
+ * @param value A generic `Obj` type `value` that contains the `key` to guard.
  * @param key A `Key` type name of the property that the `object` contains.
  * @returns A `boolean` indicating whether or not the `value` is an `object` of a generic `Obj` containing the `Key`.
  */
-export const guardObjectKey: GuardObjectKey = <Obj, Key extends keyof Obj>(value: Obj, key: Key): value is Obj =>
+export const guardObjectKey: GuardObjectKey = <Obj extends object, Key extends keyof Obj>(value: Obj, key: Key): value is Obj =>
   guardObject<Obj>(value) ? isKey(key) ? key in value : true : false;
