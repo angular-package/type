3f8e1715a4ea9a504263f60c8f0c15979ae5f80f
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 13:03:01 +0200

    refactor(guardObjectKey): update
    - change parameter name from `object` to `value`
    - categorize imports
    - update jsdoc

--- packages/type/src/guard/lib/guard-object-key.func.ts
+++ packages/type/src/guard/lib/guard-object-key.func.ts
@@ -1,12 +1,13 @@
+// Function.
 import { guardObject } from './guard-object.func';
+import { isKey } from '../../is/lib/is-key.func';
+// Type.
 import { GuardObjectKey } from '../type/guard-object-key.type';
-import { isString } from '../../is/lib/is-string.func';
 /**
- * Guard the `object` to be a generic `Obj` type and to contains the `key` property.
- * Use `isObject()` function for check ONLY.
- * @param object Generic `Obj` type `value` that contains the `key` property to guard.
- * @param key Name of the property that the `object` contains.
- * @returns boolean
+ * Guard the `value` to be an `object` of a generic `Obj` type that contains the `key` property of the `Key` type.
+ * @param value A generic `Obj` type `value` that contains the `key` property to guard.
+ * @param key A `Key` type name of the property that the `object` contains.
+ * @returns A `boolean` indicating whether or not the `value` is an `object` of a generic `Obj` containing the `Key`.
  */
-export const guardObjectKey: GuardObjectKey = <Obj, Key extends keyof Obj>(object: Obj, key: Key): object is Obj =>
-  guardObject<Obj>(object) ? isString(key) ? key in object : true : false;
+export const guardObjectKey: GuardObjectKey = <Obj, Key extends keyof Obj>(value: Obj, key: Key): value is Obj =>
+  guardObject<Obj>(value) ? isKey(key) ? key in value : true : false;
