c2ab3300e4429dbd92acd7dd271f5cb794c25de6
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-26 23:08:05 +0200

    chore: update jsdoc, prettier code

--- packages/type/src/guard/lib/guard-primitive.func.ts
+++ packages/type/src/guard/lib/guard-primitive.func.ts
@@ -5,10 +5,10 @@ import { GuardPrimitive } from '../type/guard-primitive.type';
 import { Primitive } from '../../type/primitive.type';
 import { Primitives } from '../../type/primitives.type';
 /**
- * Guard the `value` to be the `Type` from the `Primitives`.
- * @param value A generic `Type` `value` to guard.
- * @param type One of the `Primitives` to check the `value`.
- * @returns A `boolean` indicating whether or not the `value` is the `Type` from the `Primitives`.
+ * Guard the `value` to be the `Primitive` from a `type` of the `Primitives`.
+ * @param value A `Primitive` type `value` to guard.
+ * @param type A `string` type from the `Primitives` to check the `value`.
+ * @returns A `boolean` indicating whether or not the `value` is the `Primitive` from the `type`.
  */
-export const guardPrimitive: GuardPrimitive = <Type extends Primitive>(value: Type, type: Primitives): value is Type =>
-  isPrimitive<Type>(value, type);
+export const guardPrimitive: GuardPrimitive =
+  <Type extends Primitive>(value: Type, type: Primitives): value is Type => isPrimitive<Type>(value, type);
