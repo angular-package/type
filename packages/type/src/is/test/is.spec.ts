134212327b81e422b68a061c87b0cdf823bc2af7
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-27 21:58:35 +0200

    test(is): add all functions

--- packages/type/src/is/test/is.spec.ts
+++ packages/type/src/is/test/is.spec.ts
@@ -1,7 +1,29 @@
 
 import { is } from '../lib/is.object';
 
-describe('is.', () => {
-   // TRUE
-   it('is DEFINED', () => expect(is).toBeDefined());
+describe('`is`', () => {
+   describe('DEFINED', () => {
+      it('is', () => expect(is).toBeDefined());
+      it('is.array()', () => expect(is.array).toBeDefined());
+      it('is.boolean()', () => expect(is.boolean).toBeDefined());
+      it('is.booleanObject()', () => expect(is.booleanObject).toBeDefined());
+      it('is.booleanType()', () => expect(is.booleanType).toBeDefined());
+      it('is.defined()', () => expect(is.defined).toBeDefined());
+      it('is.function()', () => expect(is.function).toBeDefined());
+      it('is.instance()', () => expect(is.instance).toBeDefined());
+      it('is.key()', () => expect(is.key).toBeDefined());
+      it('is.null()', () => expect(is.null).toBeDefined());
+      it('is.number()', () => expect(is.number).toBeDefined());
+      it('is.numberObject()', () => expect(is.numberObject).toBeDefined());
+      it('is.numberType()', () => expect(is.numberType).toBeDefined());
+      it('is.object()', () => expect(is.object).toBeDefined());
+      it('is.objectKey()', () => expect(is.objectKey).toBeDefined());
+      it('is.primitive()', () => expect(is.primitive).toBeDefined());
+      it('is.string()', () => expect(is.string).toBeDefined());
+      it('is.stringObject()', () => expect(is.stringObject).toBeDefined());
+      it('is.stringType()', () => expect(is.stringType).toBeDefined());
+      it('is.symbol()', () => expect(is.symbol).toBeDefined());
+      it('is.type()', () => expect(is.type).toBeDefined());
+      it('is.undefined()', () => expect(is.undefined).toBeDefined());
+   });
 });
