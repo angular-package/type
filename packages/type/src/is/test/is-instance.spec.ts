a309e4142263bbc2add84aeffb9346fc1af53cbe
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-27 19:10:06 +0200

    test: update and add `callback`

--- packages/type/src/is/test/is-instance.spec.ts
+++ packages/type/src/is/test/is-instance.spec.ts
@@ -3,7 +3,7 @@ import { isInstance } from '../lib/is-instance.func';
 // Variables.
 import { FALSE, TRUE, FALSE_INSTANCE, TRUE_INSTANCE } from './variables/boolean.const';
 import { BIGINT, BIGINT_INSTANCE } from './variables/big-int.const';
-import { Class, CLASS } from './variables/class.const';
+import { Class, CLASS, PERSON, Person, PersonCopy, PERSON_COPY } from './variables/class.const';
 import { FUNCTION } from './variables/function.const';
 import { NULL } from './variables/null.const';
 import { NUMBER, NUMBER_INSTANCE, NUMBER_NEW_INSTANCE } from './variables/number.const';
@@ -15,51 +15,50 @@ import { notDefined } from './variables/not-defined.const';
 
 describe('isInstance', () => {
   // TRUE.
-  it('is DEFINED', () => {
-    expect(isInstance).toBeDefined();
-  });
-  it(`CLASS`, () => {
-    expect(isInstance<Class>(CLASS, Class)).toBeTruthy();
+  it('is DEFINED', () => expect(isInstance).toBeDefined());
+  it(`CLASS instance of Class`, () => expect(isInstance<Class>(CLASS, Class)).toBe(TRUE));
+  it(`PERSON instance of Person`, () => expect(isInstance<Person>(PERSON, Person)).toBe(TRUE));
+  it(`PERSON_COPY instance of PersonCopy`, () => expect(isInstance<PersonCopy>(PERSON_COPY, PersonCopy)).toBe(TRUE));
+  it(`CLASS instance of Class expect result to be true`, () => {
+    isInstance<Class>(CLASS, Class, (result: boolean) => {
+      expect(result).toBe(TRUE);
+      return result;
+    });
   });
 
   // FALSE.
+  it(`PERSON_COPY not instance of Person`, () => expect(isInstance<Person>(PERSON_COPY, Person)).toBe(FALSE));
   it(`'boolean' | Boolean`, () => {
-    expect(isInstance(FALSE, FALSE)).toBeFalse();
-    expect(isInstance(TRUE, TRUE)).toBeFalse();
-    expect(isInstance(FALSE_INSTANCE, FALSE_INSTANCE)).toBeFalse();
-    expect(isInstance(TRUE_INSTANCE, TRUE_INSTANCE)).toBeFalse();
+    expect(isInstance(FALSE, FALSE)).toBe(FALSE);
+    expect(isInstance(TRUE, TRUE)).toBe(FALSE);
+    expect(isInstance(FALSE_INSTANCE, FALSE_INSTANCE)).toBe(FALSE);
+    expect(isInstance(TRUE_INSTANCE, TRUE_INSTANCE)).toBe(FALSE);
   });
   it(`'bigint'`, () => {
-    expect(isInstance(BIGINT, BIGINT)).toBeFalse();
-    expect(isInstance(BIGINT_INSTANCE, BIGINT_INSTANCE)).toBeFalse();
+    expect(isInstance(BIGINT, BIGINT)).toBe(FALSE);
+    expect(isInstance(BIGINT_INSTANCE, BIGINT_INSTANCE)).toBe(FALSE);
   });
-  it(`CLASS`, () => {
-    expect(isInstance<Class>(Class, Class)).toBeFalse();
-  });
-  it(`'function' | Function`, () => {
-    expect(isInstance(FUNCTION, Function)).toBeFalse();
-  });
-  it(`null | NULL`, () => {
-    expect(isInstance(NULL, NULL)).toBeFalse();
-  });
+  it(`Class`, () => expect(isInstance<Class>(Class, Class)).toBe(FALSE));
+  it(`FUNCTION instance of Function`, () => expect(isInstance(FUNCTION, Function)).toBe(FALSE));
+  it(`NULL`, () => expect(isInstance(NULL, NULL)).toBe(FALSE));
   it(`'number' | Number`, () => {
-    expect(isInstance(NUMBER, NUMBER)).toBeFalse();
-    expect(isInstance(NUMBER_INSTANCE, NUMBER_INSTANCE)).toBeFalse();
-    expect(isInstance(NUMBER_NEW_INSTANCE, NUMBER_NEW_INSTANCE)).toBeFalse();
+    expect(isInstance(NUMBER, NUMBER)).toBe(FALSE);
+    expect(isInstance(NUMBER_INSTANCE, NUMBER_INSTANCE)).toBe(FALSE);
+    expect(isInstance(NUMBER_NEW_INSTANCE, NUMBER_NEW_INSTANCE)).toBe(FALSE);
   });
   // it(`'object' | Object`, () => {
-  //   expect(isInstance(OBJECT_ONE)).toBeFalse();
-  //   expect(isInstance(OBJECT_TWO, OBJECT_TWO)).toBeFalse();
+  //   expect(isInstance(OBJECT_ONE)).toBe(FALSE);
+  //   expect(isInstance(OBJECT_TWO, OBJECT_TWO)).toBe(FALSE);
   // });
   it(`'string' | String`, () => {
-    expect(isInstance(STRING, STRING)).toBeFalse();
-    expect(isInstance(STRING_INSTANCE, STRING_INSTANCE)).toBeFalse();
-    expect(isInstance(STRING_NEW_INSTANCE, STRING_NEW_INSTANCE)).toBeFalse();
+    expect(isInstance(STRING, STRING)).toBe(FALSE);
+    expect(isInstance(STRING_INSTANCE, STRING_INSTANCE)).toBe(FALSE);
+    expect(isInstance(STRING_NEW_INSTANCE, STRING_NEW_INSTANCE)).toBe(FALSE);
   });
 
   // FALSE
   it(`undefined`, () => {
-    expect(isInstance(notDefined, notDefined)).toBeFalse();
-    expect(isInstance(UNDEFINED, UNDEFINED)).toBeFalse();
+    expect(isInstance(notDefined, notDefined)).toBe(FALSE);
+    expect(isInstance(UNDEFINED, UNDEFINED)).toBe(FALSE);
   });
 });
