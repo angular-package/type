bc703fadfdf829f199c23b1c7ca9e27ff4b659ea
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-27 19:16:42 +0200

    refactor(isInstance): add `callback` and update jsdoc

--- packages/type/src/is/lib/is-instance.func.ts
+++ packages/type/src/is/lib/is-instance.func.ts
@@ -1,19 +1,28 @@
 // Function.
+import { errorCallback } from '../../lib/error-callback.func';
+import { isFunction } from './is-function.func';
 import { isObject } from './is-object.func';
-import { isString } from './is-string.func';
 // Type.
 import { Constructor } from '../../type/constructor.type';
 import { IsInstance } from '../type/is-instance.type';
-import { isFunction } from './is-function.func';
+import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any `value` is an `object` of a generic `Obj` type equal to an `instance` of `Constructor` type.
  * @param value Any `value` to compare with the `instance`.
- * @param instance The name of the generic `Obj` type to create an `instance` to compare with the `value`.
- * @returns  A `boolean` indicating whether or not the `value` is an `instance`.
+ * @param instance A generic `Obj` `Constructor` type to create an `instance` to compare with the `value`.
+ * @param callback `ResultCallback` function to handle result before returns.
+ * @callback `errorCallback`.
+ * @returns  A `boolean` indicating whether or not the `value` is an `instance` of a generic `Obj`.
  */
-export const isInstance: IsInstance = <Obj>(value: any, instance: Constructor<Obj>): value is Obj =>
-  isObject<Obj>(value) ?
-    isFunction(instance) ?
-      value instanceof instance === true && isString(instance.prototype.constructor.name)
-    : false
-  : false;
+export const isInstance: IsInstance = <Obj>(
+    value: any,
+    instance: Constructor<Obj>,
+    callback: ResultCallback = errorCallback
+  ): value is Obj =>
+    callback(
+      isObject<Obj>(value) ?
+        isFunction(instance) ?
+          value instanceof instance === true
+        : false
+      : false
+    );
