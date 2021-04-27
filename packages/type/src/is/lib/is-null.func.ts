ab572c6c35a5ab6ed9d7b396099341186320b551
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-27 20:46:22 +0200

    refactor(isNull): add `callback`

--- packages/type/src/is/lib/is-null.func.ts
+++ packages/type/src/is/lib/is-null.func.ts
@@ -1,13 +1,15 @@
 // Function.
+import { errorCallback } from '../../lib/error-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { IsNull } from '../type/is-null.type';
+import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any `value` is an `object` type and equal to `null`.
  * @param value Any `value` to check.
+ * @param callback `ResultCallback` function to handle result before returns.
+ * @callback `errorCallback`.
  * @returns A `boolean` indicating whether or not the `value` is `null`.
  */
-export const isNull: IsNull = (value: any): value is null =>
-  typeOf(value) === 'null' &&
-  typeof value === 'object' &&
-  value === null;
+export const isNull: IsNull = (value: any, callback: ResultCallback = errorCallback): value is null =>
+  callback(typeOf(value) === 'null' && typeof value === 'object' && value === null);
