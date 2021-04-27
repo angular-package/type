9981078bee0c4b94d59d74cbaf65bd80889552c5
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-27 20:57:22 +0200

    test(isNumbeR): use `toBe` method

--- packages/type/src/is/test/is-number.spec.ts
+++ packages/type/src/is/test/is-number.spec.ts
@@ -1,17 +1,15 @@
-
-import { isNumber } from '../lib/is-number.func';
-import { FALSE, TRUE, FALSE_INSTANCE, TRUE_INSTANCE } from './variables/boolean.const';
 import { BIGINT, BIGINT_INSTANCE } from './variables/big-int.const';
+import { CLASS, Class } from './variables/class.const';
+import { FALSE, TRUE, FALSE_INSTANCE, TRUE_INSTANCE } from './variables/boolean.const';
 import { FUNCTION } from './variables/function.const';
+import { NULL } from './variables/null.const';
 import { NUMBER, NUMBER_INSTANCE, NUMBER_NEW_INSTANCE } from './variables/number.const';
 import { OBJECT_ONE, OBJECT_TWO } from './variables/object.const';
 import { STRING, STRING_INSTANCE, STRING_NEW_INSTANCE } from './variables/string.const';
 import { SYMBOL_NUMBER, SYMBOL_STRING } from './variables/symbol.const';
 import { UNDEFINED } from './variables/undefined.const';
-import { CLASS, Class } from './variables/class.const';
-import { NULL } from './variables/null.const';
+import { isNumber } from '../lib/is-number.func';
 import { notDefined } from './variables/not-defined.const';
-
 /**
  * Checks
  * ✓ typeof === 'number'
@@ -19,52 +17,48 @@ import { notDefined } from './variables/not-defined.const';
  */
 describe('isNumber', () => {
   // TRUE
-  it('is DEFINED', () => {
-    expect(isNumber).toBeDefined();
-  });
+  it('is DEFINED', () => expect(isNumber).toBeDefined());
   it(`'number' | Number`, () => {
-    expect(isNumber(NUMBER)).toBeTruthy();
-    expect(isNumber(NUMBER_INSTANCE)).toBeTruthy();
-    expect(isNumber(NUMBER_NEW_INSTANCE)).toBeTruthy();
+    expect(isNumber(NUMBER)).toBe(TRUE);
+    expect(isNumber(NUMBER_INSTANCE)).toBe(TRUE);
+    expect(isNumber(NUMBER_NEW_INSTANCE)).toBe(TRUE);
   });
 
   // FALSE
   it(`'bigint'`, () => {
-    expect(isNumber(BIGINT)).toBeFalsy();
-    expect(isNumber(BIGINT_INSTANCE)).toBeFalsy();
+    expect(isNumber(BIGINT)).toBe(FALSE);
+    expect(isNumber(BIGINT_INSTANCE)).toBe(FALSE);
   });
   it(`'boolean' | Boolean`, () => {
-    expect(isNumber(FALSE)).toBeFalsy();
-    expect(isNumber(TRUE)).toBeFalsy();
-    expect(isNumber(FALSE_INSTANCE)).toBeFalsy();
-    expect(isNumber(TRUE_INSTANCE)).toBeFalsy();
+    expect(isNumber(FALSE)).toBe(FALSE);
+    expect(isNumber(TRUE)).toBe(FALSE);
+    expect(isNumber(FALSE_INSTANCE)).toBe(FALSE);
+    expect(isNumber(TRUE_INSTANCE)).toBe(FALSE);
   });
   it(`Class | CLASS`, () => {
-    expect(isNumber(Class)).toBeFalsy();
-    expect(isNumber(CLASS)).toBeFalsy();
+    expect(isNumber(Class)).toBe(FALSE);
+    expect(isNumber(CLASS)).toBe(FALSE);
   });
-  it(`'function' | Function`, () => {
-    expect(isNumber(FUNCTION)).toBeFalsy();
-  });
+  it(`'function' | Function`, () => expect(isNumber(FUNCTION)).toBe(FALSE));
   it(`null | NULL`, () => {
-    expect(isNumber(null)).toBeFalsy();
-    expect(isNumber(NULL)).toBeFalsy();
+    expect(isNumber(null)).toBe(FALSE);
+    expect(isNumber(NULL)).toBe(FALSE);
   });
   it(`'object' | Object`, () => {
-    expect(isNumber(OBJECT_ONE)).toBeFalsy();
-    expect(isNumber(OBJECT_TWO)).toBeFalsy();
+    expect(isNumber(OBJECT_ONE)).toBe(FALSE);
+    expect(isNumber(OBJECT_TWO)).toBe(FALSE);
   });
   it(`'string' | String`, () => {
-    expect(isNumber(STRING)).toBeFalsy();
-    expect(isNumber(STRING_INSTANCE)).toBeFalsy();
-    expect(isNumber(STRING_NEW_INSTANCE)).toBeFalsy();
+    expect(isNumber(STRING)).toBe(FALSE);
+    expect(isNumber(STRING_INSTANCE)).toBe(FALSE);
+    expect(isNumber(STRING_NEW_INSTANCE)).toBe(FALSE);
   });
   it(`'symbol'`, () => {
-    expect(isNumber(SYMBOL_NUMBER)).toBeFalsy();
-    expect(isNumber(SYMBOL_STRING)).toBeFalsy();
+    expect(isNumber(SYMBOL_NUMBER)).toBe(FALSE);
+    expect(isNumber(SYMBOL_STRING)).toBe(FALSE);
   });
   it(`'undefined'`, () => {
-    expect(isNumber(notDefined)).toBeFalse();
-    expect(isNumber(UNDEFINED)).toBeFalse();
+    expect(isNumber(notDefined)).toBe(FALSE);
+    expect(isNumber(UNDEFINED)).toBe(FALSE);
   });
 });
