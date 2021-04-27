dd5546eb1bbdd21a0df2b5927c92a1a9e1f6a083
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-27 17:08:23 +0200

    refactor(isDefined): add `callback`, update jsdoc

--- packages/type/src/is/lib/is-defined.func.ts
+++ packages/type/src/is/lib/is-defined.func.ts
@@ -1,13 +1,15 @@
 // Function.
+import { errorCallback } from '../../lib/error-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { IsDefined } from '../type/is-defined.type';
+import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if an unknown `value` is NOT an `undefined` type and is NOT equal to `undefined`.
- * @param value An unknown `value` to check.
- * @returns A `boolean` indicating whether or not the `value` is defined.
+ * @param value An `unknown` `value` to check.
+ * @param callback `ResultCallback` function to handle result before returns.
+ * @callback `errorCallback`.
+ * @returns A `boolean` indicating whether or not the `value` is defined, not `undefined`.
  */
-export const isDefined: IsDefined = (value: unknown): boolean =>
-  typeOf(value) !== 'undefined' &&
-  typeof value !== 'undefined' &&
-  value !== undefined;
+export const isDefined: IsDefined = (value: unknown, callback: ResultCallback = errorCallback): boolean =>
+  callback(typeOf(value) !== 'undefined' && typeof value !== 'undefined' && value !== undefined);
