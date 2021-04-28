098cab62abc3cf5fe533f187562e17038ba6b7f7
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-28 17:36:44 +0200

    feat(isArray): add `ResultCallback` feature

--- packages/type/src/is/lib/is-array.func.ts
+++ packages/type/src/is/lib/is-array.func.ts
@@ -1,14 +1,20 @@
 // Function.
+import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { IsArray } from '../type/is-array.type';
+import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any `value` is an `Array`, `Array` instance, and `object` type.
  * @param value Any `value` to check.
+ * @param callback `ResultCallback` function to handle result before returns.
+ * @callback `resultCallback`.
  * @returns A `boolean` indicating whether or not the `value` is an `Array`.
  */
-export const isArray: IsArray = <Type>(value: any): value is Array<Type> =>
-  typeOf(value) === 'array' &&
-  Array.isArray(value) === true &&
-  value instanceof Array === true &&
-  typeof value === 'object';
+export const isArray: IsArray = <Type>(value: any, callback: ResultCallback = resultCallback): value is Array<Type> =>
+  callback(
+    typeOf(value) === 'array' &&
+    Array.isArray(value) === true &&
+    value instanceof Array === true &&
+    typeof value === 'object'
+  );
