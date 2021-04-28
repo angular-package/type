b7d2121f5c540bfe79125cfa54774cce8f8e33a6
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-28 14:26:38 +0200

    refactor(isBigInt): add `callback`

--- packages/type/src/is/lib/is-big-int.func.ts
+++ packages/type/src/is/lib/is-big-int.func.ts
@@ -1,12 +1,15 @@
 // Function.
+import { errorCallback } from '../../lib/error-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { IsBigInt } from '../type/is-big-int.type';
+import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any `value` is a `bigint` type.
  * @param value Any `value` to check.
+ * @param callback `ResultCallback` function to handle result before returns.
+ * @callback `errorCallback`.
  * @returns A `boolean` indicating whether or not the `value` is a `bigint`.
  */
-export const isBigInt: IsBigInt = (value: any): value is bigint =>
-  typeOf(value) === 'bigint' &&
-  typeof value === 'bigint';
+export const isBigInt: IsBigInt = (value: any, callback: ResultCallback = errorCallback): value is bigint =>
+  callback(typeOf(value) === 'bigint' && typeof value === 'bigint');
