73d4667ed4e94e46be9be2740573b78fc62126bf
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-03 19:05:59 +0200

    feat(SymbolObject): experimental symbol

---
+++ packages/type/src/test/symbol-object.spec.ts
@@ -0,0 +1,21 @@
+import { STRING, STRING_INSTANCE, STRING_NEW_INSTANCE } from '../is/test/variables/string.const';
+import { SymbolObject } from '../lib/symbol-object.class';
+import { SYMBOL_NUMBER } from '../is/test/variables/symbol.const';
+
+describe(`SymbolObject`, () => {
+  // Defined.
+  it('is DEFINED', () => expect(SymbolObject).toBeDefined());
+
+  // Checks ...
+  describe(`checks`, () => {
+
+    // ... primitives.
+    describe(`primitive`, () => {
+      // string
+      describe(`symbol`, () => {
+        SymbolObject.set = STRING;
+        it(`Symbol(${STRING})`, () => expect(SymbolObject.get).toBeDefined());
+      });
+    });
+  });
+});
