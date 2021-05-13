12aa05f6d49fe0b30bc76cd0f2918b3ad31599c9
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-13 02:04:33 +0200

    refactor(isObject): remove `key` and add `callback`

--- packages/type/src/is/type/is-object.type.ts
+++ packages/type/src/is/type/is-object.type.ts
@@ -1,2 +1,2 @@
-import { Key } from '../../type/key.type';
-export type IsObject = <Obj = object>(value: any, key?: Key) => value is Obj;
+import { ResultCallback } from '../../type/result-callback.type';
+export type IsObject = <Obj = object>(value: any, callback?: ResultCallback) => value is Obj;
