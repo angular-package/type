1bb14ba4734d59d23cd4fb3bfe9a81b0a9e3cc6e
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-15 22:37:24 +0200

    docs(areString): update

--- packages/type/src/are/lib/are-string.func.ts
+++ packages/type/src/are/lib/are-string.func.ts
@@ -1,7 +1,7 @@
 import { check } from './check-args.func';
 /**
  * Checks if any of all the values are a `string`.
- * @param value Any arguments to check if they're all a `'string`.
- * @returns A `boolean` indicating whether or not all the `values` are an `Array`.
+ * @param value Any values to check.
+ * @returns A `boolean` indicating whether or not all the values are an `Array`.
  */
 export const areString = (...value: any): boolean => check('string', ...value);
