c98b0e17f02b3190d6085e3b3487092a08d4f9ef
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-16 01:51:56 +0200

    refactor(guardString): add guard for primitive `String` object

--- packages/type/src/guard/type/guard-string.type.ts
+++ packages/type/src/guard/type/guard-string.type.ts
@@ -1,2 +1,3 @@
+import { AnyString } from '../../type/any-string.type';
 import { ResultCallback } from '../../type/result-callback.type';
-export type GuardString = (value: string, callback?: ResultCallback) => value is string;
+export type GuardString = <S extends AnyString>(value: S, callback?: ResultCallback) => value is S;
