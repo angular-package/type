ea620c415241aecf5e251db353ef4244c4e4893e
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-16 01:50:48 +0200

    refactor(guardBoolean): add guard for primitive `Boolean` object

--- packages/type/src/guard/lib/guard-boolean.func.ts
+++ packages/type/src/guard/lib/guard-boolean.func.ts
@@ -1,13 +1,14 @@
 // Function.
 import { isBoolean } from '../../is/lib/is-boolean.func';
 // Type.
+import { AnyBoolean } from '../../type/any-boolean.type';
 import { GuardBoolean } from '../type/guard-boolean.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Guard the `value` to be a `boolean`.
+ * Guard the `value` to be any type of a boolean.
  * @param value A `boolean` type `value` to guard.
- * @param callback Optional `ResultCallback` function to handle result before returns.
- * @returns A `boolean` indicating whether or not the `value` is a `boolean`.
+ * @param callback An optional `ResultCallback` function to handle result before returns.
+ * @returns A `boolean` indicating whether or not the `value` is a `boolean` or `Boolean`.
  */
-export const guardBoolean: GuardBoolean = (value: boolean, callback?: ResultCallback): value is boolean =>
+export const guardBoolean: GuardBoolean = <B extends AnyBoolean>(value: B, callback?: ResultCallback): value is B =>
   isBoolean(value, callback);
