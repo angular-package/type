ce321ef3a41a75545e1024b50e4c4b53ec03ea4d
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-19 00:23:43 +0200

    docs(isObjectKey): update jsdoc

--- packages/type/src/is/lib/is-object-key.func.ts
+++ packages/type/src/is/lib/is-object-key.func.ts
@@ -8,7 +8,7 @@ import { IsObjectKey } from '../type/is-object-key.type';
 import { Key } from '../../type/key.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if any `value` is an `object` with its own specified `key` of the `Key` type.
+ * Checks if any `value` is an `object` with its own enumerable and non-enumerable specified `key` of the `Key` type.
  * @param value Any `value` to check if it contains a specified `key`.
  * @param key A `Key` type or an array of `Key` type to check the `value`.
  * @param callback `ResultCallback` function to handle result before returns.
