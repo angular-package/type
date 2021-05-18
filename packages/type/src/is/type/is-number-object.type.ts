07cbbe33f05df5ddee72645c42eec292ca722248
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-19 00:33:07 +0200

    chore: recover from 3.4.x

--- packages/type/src/is/type/is-number-object.type.ts
+++ packages/type/src/is/type/is-number-object.type.ts
@@ -1,2 +1,2 @@
 import { ResultCallback } from '../../type/result-callback.type';
-export type IsNumberObject = (value: any, callback?: ResultCallback) => value is number;
+export type IsNumberObject = (value: any, callback?: ResultCallback) => value is Number;
