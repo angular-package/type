8e44ce0bfc04f8ff17f6bb691652d615d99ab7f8
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-19 00:27:17 +0200

    test: get from 3.4.x

---
+++ packages/type/src/guard/test/guard-string.spec.ts
@@ -0,0 +1,28 @@
+// Function.
+import { guardString } from '../lib/guard-string.func';
+// Variables.
+import { STRING, STRING_NEW_INSTANCE } from './variables/string.const';
+import { TRUE } from './variables/boolean.const';
+
+describe(guardString.name, () => {
+  // Defined.
+  it('is DEFINED', () => expect(guardString).toBeDefined());
+
+  // Checks ...
+  describe(`checks`, () => {
+    it('callback', () => {
+      guardString(STRING, (result: boolean, value: string) => {
+        expect(result).toBe(TRUE);
+        expect(value).toEqual(STRING);
+        return result;
+      });
+    });
+    // ... primitives.
+    describe(`primitive`, () => {
+      describe(`string`, () => it(`${STRING}`, () => expect(guardString(STRING)).toBe(TRUE)));
+      describe(`object`, () => {
+        describe(`String`, () => it(`new String(${STRING})`, () => expect(guardString(STRING_NEW_INSTANCE)).toBe(TRUE)));
+      })
+    });
+  });
+});
