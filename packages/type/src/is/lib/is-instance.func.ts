d7ae650b14a8b1fae9c847495f82b0133865ecde
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-18 23:14:35 +0200

    refactor(isInstance): use `isClass` instead of `isFunction` to check the instance

--- packages/type/src/is/lib/is-instance.func.ts
+++ packages/type/src/is/lib/is-instance.func.ts
@@ -6,23 +6,24 @@ import { resultCallback } from '../../lib/result-callback.func';
 import { Constructor } from '../../type/constructor.type';
 import { IsInstance } from '../type/is-instance.type';
 import { ResultCallback } from '../../type/result-callback.type';
+import { isClass } from './is-class.func';
 /**
  * Checks if any `value` is an `object` of a generic `Obj` type equal to an `instance` of `Constructor` type.
  * @param value Any `value` to compare with the `instance`.
- * @param instance A generic `Obj` `Constructor` type to create an `instance` to compare with the `value`.
+ * @param className A generic `Obj` `Constructor` type to create an `instance` to compare with the `value`.
  * @param callback `ResultCallback` function to handle result before returns.
  * @callback `resultCallback`.
  * @returns  A `boolean` indicating whether or not the `value` is an `instance` of a generic `Obj`.
  */
 export const isInstance: IsInstance = <Obj>(
     value: any,
-    instance: Constructor<Obj>,
+    className: Constructor<Obj>,
     callback: ResultCallback = resultCallback
   ): value is Obj =>
     callback(
       isObject<Obj>(value) ?
-        isFunction(instance) ?
-          value instanceof instance === true
+        isClass(className) ?
+          value instanceof className === true
         : false
       : false,
       value
