1379f65c980ce53e6fe32a3d31b0a56909ab8d94
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-20 03:10:27 +0200

    docs(guardDefined): update jsdoc

--- packages/type/src/guard/lib/guard-defined.type.ts
+++ packages/type/src/guard/lib/guard-defined.type.ts
@@ -5,8 +5,8 @@ import { Defined } from '../../type/defined.type';
 import { GuardDefined } from '../type/guard-defined.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if a generic type `value` is NOT an `undefined` type and is NOT equal to `undefined`.
- * @param value A generic type `value` to check.
+ * Guard the `value` to be defined.
+ * @param value A generic type `value` to guard.
  * @param callback An optional `ResultCallback` function to handle result before returns.
  * @returns A `boolean` indicating whether or not the `value` is defined, if `undefined` then returns `never`.
  */
