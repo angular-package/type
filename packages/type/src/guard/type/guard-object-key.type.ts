f44f0002423d817f8867b02452e6269712366ffa
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-20 02:00:14 +0200

    refactor(GuardObjectKey): use `Key` type instead keyof

--- packages/type/src/guard/type/guard-object-key.type.ts
+++ packages/type/src/guard/type/guard-object-key.type.ts
@@ -1,1 +1,3 @@
-export type GuardObjectKey = <Obj extends object, Key extends keyof Obj>(value: Obj, key: Key) => value is Obj;
+import { Key } from '../../type/key.type';
+import { ResultCallback } from '../../type/result-callback.type';
+export type GuardObjectKey = <Obj extends object>(value: Obj, key: Key | Key[], callback?: ResultCallback) => value is Obj;
