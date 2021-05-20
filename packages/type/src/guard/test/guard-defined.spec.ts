c732110a80d00c4f76ea327b322eb2a2d13e29bf
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-20 03:10:55 +0200

    test: add `guardDefined` and update `isDefined`

---
+++ packages/type/src/guard/test/guard-defined.spec.ts
@@ -0,0 +1,23 @@
+// Function.
+import { guardDefined } from '../lib/guard-defined.type';
+// Variables.
+import { FALSE } from './variables/boolean.const';
+import { UNDEFINED } from './variables/undefined.const';
+
+describe(guardDefined.name, () => {
+  // Defined.
+  it('is DEFINED', () => expect(guardDefined).toBeDefined());
+
+  // Checks ...
+  describe(`checks`, () => {
+    it('callback', () => {
+      guardDefined(UNDEFINED, (result: boolean, value: undefined) => {
+        expect(result).toBe(FALSE);
+        expect(value).toBeUndefined();
+        return result;
+      });
+    });
+    // ... primitives.
+    describe(`primitive`, () => it(`${UNDEFINED}`, () => expect(guardDefined(UNDEFINED)).toEqual(FALSE)));
+  });
+});
