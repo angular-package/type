b752d2a444078e534157b4302bd8d7f0a0faed79
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 14:22:21 +0200

    refactor(IsDefined): back to 4.0.1

--- packages/type/src/is/type/is-defined.type.ts
+++ packages/type/src/is/type/is-defined.type.ts
@@ -1,3 +1,2 @@
-import { Defined } from '../../type/defined.type';
 import { ResultCallback } from '../../type/result-callback.type';
-export type IsDefined = <Type>(value: unknown, callback?: ResultCallback) => value is Defined<Type>;
+export type IsDefined = (value: unknown, callback?: ResultCallback) => boolean;
