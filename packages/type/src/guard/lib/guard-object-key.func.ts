ca917c25a3741002e53736d2570cf1cfcf5fe521
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-23 00:32:52 +0200

    docs(guardObjectKey): update jsdoc

--- packages/type/src/guard/lib/guard-object-key.func.ts
+++ packages/type/src/guard/lib/guard-object-key.func.ts
@@ -6,7 +6,7 @@ import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guard the `value` to be an `object` of a generic `Obj` type that contains the `key`.
  * @param value A generic `Obj` type `value` that contains the `key` to guard.
- * @param key A key of `Obj` or an array key of `Obj` type as the name of the property that the `value` contains.
+ * @param key A key of `Obj` or an array of keys of `Obj` type as the name of the property that the `value` contains.
  * @param callback An optional `ResultCallback` function to handle result before returns.
  * @returns A `boolean` indicating whether or not the `value` is an `object` of a generic `Obj` containing the `key`.
  */
