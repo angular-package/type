c36d261edbec3e73f2cd96be5555acfe61ed0516
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-23 00:29:29 +0200

    refactor(IsClass): Type  variable `Class`  default value is set to `Function`.

--- packages/type/src/is/type/is-class.type.ts
+++ packages/type/src/is/type/is-class.type.ts
@@ -1,2 +1,2 @@
 import { ResultCallback } from '../../type/result-callback.type';
-export type IsClass = <Class>(value: any, callback?: ResultCallback) => value is Class;
+export type IsClass = <Class = Function>(value: any, callback?: ResultCallback) => value is Class;
