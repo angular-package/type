050236922d739cdbf0448f6ce23a2f9ad444552b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 14:17:32 +0200

    refactor: add generic `Type` and  `Defined<Type>`

--- packages/type/src/guard/lib/guard-defined.func.ts
+++ packages/type/src/guard/lib/guard-defined.func.ts
@@ -11,4 +11,4 @@ import { ResultCallback } from '../../type/result-callback.type';
  * @returns A `boolean` indicating whether or not the `value` is defined, if `undefined` then returns `never`.
  */
 export const guardDefined: GuardDefined = <Type>(value: Type, callback?: ResultCallback): value is Defined<Type> =>
-  isDefined(value, callback);
+  isDefined<Type>(value, callback);
