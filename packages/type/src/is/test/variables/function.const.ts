c71d9adb5766559c27272e28569cca9fb3ed0416
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-25 14:11:21 +0200

    test(is): move variables to testing

--- packages/type/src/is/test/variables/function.const.ts
+++
@@ -1,7 +0,0 @@
-import { Func } from '../../../type/func.type';
-/**
- * typeof === 'function'
- * instanceof Function === true
- * instanceof Object === true
- */
-export const FUNCTION: Func = (x: number, y: string): any => x + y;
