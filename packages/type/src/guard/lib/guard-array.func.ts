379661f8c887585c027dd798c4681d3c353bad76
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-26 04:47:53 +0200

    docs: update jsdoc

--- packages/type/src/guard/lib/guard-array.func.ts
+++ packages/type/src/guard/lib/guard-array.func.ts
@@ -6,7 +6,7 @@ import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guard the `value` to be an `Array` of a generic `Type`.
  * @param value A generic `Type` `Array` `value` to guard.
- * @param callback Optional `ResultCallback` function to handle result before returns.
+ * @param callback An optional `ResultCallback` function to handle result before returns.
  * @returns A `boolean` indicating whether or not the `value` is an `Array` of a generic `Type`.
  */
 export const guardArray: GuardArray = <Type>(value: Array<Type>, callback?: ResultCallback): value is Array<Type> =>
--- packages/type/src/guard/lib/guard-defined.func.ts
+++ packages/type/src/guard/lib/guard-defined.func.ts
@@ -5,7 +5,7 @@ import { Defined } from '../../type/defined.type';
 import { GuardDefined } from '../type/guard-defined.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Guard the `value` to be defined.
+ * Guard the `value` to be defined, not `undefined`.
  * @param value A generic `Type` type from the `value` to guard.
  * @param callback An optional `ResultCallback` function to handle result before returns.
  * @returns A `boolean` indicating whether or not the `value` is defined, if `undefined` then returns `never`.
