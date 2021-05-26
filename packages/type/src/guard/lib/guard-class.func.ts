824a7f963c0285cc85d77e13e4fa076f78ab4846
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-26 03:53:50 +0200

    docs: update `value` param

--- packages/type/src/guard/lib/guard-class.func.ts
+++ packages/type/src/guard/lib/guard-class.func.ts
@@ -5,7 +5,7 @@ import { GuardClass } from '../type/guard-class.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guard the `value` to be a generic `Class` type of `class`.
- * @param value A generic `Class` type `value` to guard.
+ * @param value A generic `Class` type from the `value` to guard.
  * @param callback An Optional `ResultCallback` function to handle result before returns.
  * @returns A `boolean` indicating whether or not the `value` is a generic `class`.
  */
