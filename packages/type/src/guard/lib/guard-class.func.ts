654e6564e02bf6f05bab9a73861f6f2932b41ae4
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-26 04:46:38 +0200

    fix(guardClass): guard the `value` with `Function`

--- packages/type/src/guard/lib/guard-class.func.ts
+++ packages/type/src/guard/lib/guard-class.func.ts
@@ -9,5 +9,5 @@ import { ResultCallback } from '../../type/result-callback.type';
  * @param callback An Optional `ResultCallback` function to handle result before returns.
  * @returns A `boolean` indicating whether or not the `value` is a generic `class`.
  */
-export const guardClass: GuardClass = <Class>(value: Class, callback?: ResultCallback): value is Class =>
+export const guardClass: GuardClass = <Class extends Function>(value: Class, callback?: ResultCallback): value is Class =>
   isClass<Class>(value, callback);
