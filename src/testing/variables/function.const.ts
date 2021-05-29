7c318655a8524ecd9a3e36cde3401e74edfb0c75
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-29 15:05:44 +0200

    test(function): treat as any

--- src/testing/variables/function.const.ts
+++ src/testing/variables/function.const.ts
@@ -4,4 +4,11 @@ import { Func } from '../../type/func.type';
  * instanceof Function === true
  * instanceof Object === true
  */
-export const FUNCTION: Func = (x: number, y: string): any => x + y;
+export const FUNCTION: any = (x: number, y: string): any => x + y;
+
+export function functionConstructor(this: any, ...args: any[]): any {
+  if (args) {
+    args.forEach((arg, index: number) => this[index] = arg[index]);
+  }
+  return this;
+}
