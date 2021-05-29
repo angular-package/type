2bd456d7060ed9f3bda77f416c0449682b9613d6
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-29 22:24:31 +0200

    chore: disable tslint ban-types

--- src/type/any-boolean.type.ts
+++ src/type/any-boolean.type.ts
@@ -1,1 +1,2 @@
+// tslint:disable-next-line: ban-types
 export type AnyBoolean = Exclude<boolean | Boolean, true | false>;
--- src/type/any-string.type.ts
+++ src/type/any-string.type.ts
@@ -1,1 +1,2 @@
+// tslint:disable-next-line: ban-types
 export type AnyString = string | String;
