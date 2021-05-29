d4ddc540c6374bf56ecb9de9ffb0997f0ee806f8
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-29 15:02:40 +0200

    refactor(guardInstance): change parameter name and update jsdoc

--- src/guard/lib/guard-instance.func.ts
+++ src/guard/lib/guard-instance.func.ts
@@ -5,11 +5,12 @@ import { Constructor } from '../../type/constructor.type';
 import { GuardInstance } from '../type/guard-instance.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Guard the `value` to guard the `value` to be an `object` of a generic `Obj` type equal to an `instance`.
- * @param value An `Obj` type `value` to compare with the `instance`.
- * @param instance A generic `Obj` `Constructor` type to create an `instance` to compare with the `value`.
- * @param callback Optional `ResultCallback` function to handle result before returns.
- * @returns A `boolean` indicating whether or not the `value` is a `symbol`.
+ * Guard the `value` to be an `object` of a generic `Obj` type and an instance of `Constructor` type.
+ * @param value A generic `Obj` type `value` to be an instance of the `constructor`.
+ * @param constructor A class or function that specifies the type of the `constructor`.
+ * @param callback An optional `ResultCallback` function to handle the result before returns.
+ * @returns A `boolean` indicating whether or not the `value` is an `instance` of a generic `Obj`.
  */
-export const guardInstance: GuardInstance = <Obj extends object>(value: Obj, instance: Constructor<Obj>, callback?: ResultCallback): value is Obj =>
-  isInstance<Obj>(value, instance, callback);
+export const guardInstance: GuardInstance =
+  <Obj extends object>(value: Obj, constructor: Constructor<Obj>, callback?: ResultCallback): value is Obj =>
+    isInstance<Obj>(value, constructor, callback);
