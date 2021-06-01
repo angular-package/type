120a0967f677787906a9f010d80bb7cc99a8cb28
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-01 21:39:14 +0200

    docs(isDefined): update jsdoc and format code

--- src/is/lib/is-defined.func.ts
+++ src/is/lib/is-defined.func.ts
@@ -6,10 +6,18 @@ import { Defined } from '../../type/defined.type';
 import { IsDefined } from '../type/is-defined.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if a generic type `value` is not an `undefined` type and is not equal to `undefined`.
- * @param value A generic type `value` to check.
- * @param callback A `ResultCallback` function to handle result before returns.
+ * Checks if a generic `Type` `value` is not an `undefined` type and is not equal to `undefined`.
+ * @param value A generic `Type` `value`, by default of type detected from the `value`, to check.
+ * @param callback A `ResultCallback` function to handle the result before returns.
  * @returns A `boolean` indicating whether or not the `value` is defined, not `undefined`.
  */
-export const isDefined: IsDefined = <Type>(value: Type, callback: ResultCallback = resultCallback): value is Defined<Type> =>
-  callback(typeOf(value) !== 'undefined' && typeof value !== 'undefined' && value !== undefined, value);
+export const isDefined: IsDefined = <Type>(
+  value: Type,
+  callback: ResultCallback = resultCallback
+): value is Defined<Type> =>
+  callback(
+    typeOf(value) !== 'undefined' &&
+    typeof value !== 'undefined' &&
+    value !== undefined,
+    value
+  );
