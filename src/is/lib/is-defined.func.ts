9154c07c248195ff80ac66f41dc218b6562b5bce
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-01 11:49:41 +0200

    refactor(isDefined): change value parameter type from `any` to `Type` cause it's possible to use statement `value is Defined<Type>`, update jsdoc

--- src/is/lib/is-defined.func.ts
+++ src/is/lib/is-defined.func.ts
@@ -2,13 +2,14 @@
 import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
+import { Defined } from '../../type/defined.type';
 import { IsDefined } from '../type/is-defined.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if an unknown `value` is NOT an `undefined` type and is NOT equal to `undefined`.
- * @param value An `unknown` `value` to check.
- * @param callback `ResultCallback` function to handle result before returns.
+ * Checks if a generic type `value` is not an `undefined` type and is not equal to `undefined`.
+ * @param value A generic type `value` to check.
+ * @param callback A `ResultCallback` function to handle result before returns.
  * @returns A `boolean` indicating whether or not the `value` is defined, not `undefined`.
  */
-export const isDefined: IsDefined = (value: unknown, callback: ResultCallback = resultCallback): boolean =>
+export const isDefined: IsDefined = <Type>(value: Type, callback: ResultCallback = resultCallback): value is Defined<Type> =>
   callback(typeOf(value) !== 'undefined' && typeof value !== 'undefined' && value !== undefined, value);
--- src/is/type/is-defined.type.ts
+++ src/is/type/is-defined.type.ts
@@ -1,2 +1,3 @@
+import { Defined } from '../../type/defined.type';
 import { ResultCallback } from '../../type/result-callback.type';
-export type IsDefined = (value: unknown, callback?: ResultCallback) => boolean;
+export type IsDefined = <Type>(value: Type, callback?: ResultCallback) => value is Defined<Type>;
