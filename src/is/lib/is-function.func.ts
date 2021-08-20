c45b9bf8542b51ebfcd4019af4582401a8ee2263
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-08-20 17:55:53 +0200

    refactor(isFunction()): improve the way of returning the type of function.

--- src/is/lib/is-function.func.ts
+++ src/is/lib/is-function.func.ts
@@ -7,26 +7,26 @@ import { CallbackPayload } from '../../interface/callback-payload.interface';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any `value` is a `function` type, an instance of `Function` and `Object`.
- * @param value The `value` of any type to check.
+ * @param value The value of the captured type of `function` from the provided `value` or else any type to check.
  * @param callback A callback `function` of `ResultCallback` type with `payload` parameter of the default `CallbackPayload` shape to handle
  * the `result` and `payload` of the check before the `result` return. By default it uses `resultCallback()` function.
  * @param payload An optional `object` of a generic type variable `Payload` that is assigned to the `payload` of the provided `callback`.
  * @returns The return value is a `boolean` indicating whether the provided `value` is a `Function`.
  * @angularpackage
  */
-export const isFunction = <Payload extends object>(
-  value: any,
+export const isFunction = <Value extends any, Payload extends object = object>(
+  value: Value extends Function ? Value : any,
   callback: ResultCallback<CallbackPayload & Payload> = resultCallback,
   payload?: Payload
-): value is Function =>
+): value is Value extends Function ? Value : Function =>
   callback(
     typeOf(value) === 'function' &&
-    typeof value === 'function' &&
-    value instanceof Function === true &&
-    value instanceof Object === true
-    ? /class/.test(Function.prototype.toString.call(value).slice(0, 5)) ===
-        false
-    : false,
+      typeof value === 'function' &&
+      value as any instanceof Function === true &&
+      value as any instanceof Object === true
+      ? /class/.test(Function.prototype.toString.call(value).slice(0, 5)) ===
+      false
+      : false,
     {
       ...{
         name: isFunction.name,
