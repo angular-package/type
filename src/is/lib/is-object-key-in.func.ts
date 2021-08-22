c38e26e3193b21e6e43063319b2124f432aa89e1
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-08-22 23:23:21 +0200

    refactor(isObjectKeyIn()): change the name of generic type variable `Type` to `Obj` and update jsdoc.

--- src/is/lib/is-object-key-in.func.ts
+++ src/is/lib/is-object-key-in.func.ts
@@ -8,24 +8,27 @@ import { CallbackPayload } from '../../interface/callback-payload.interface';
 // Type.
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if any `value` is an `Object` with the `key` of the `PropertyKey` type by using the `in` operator.
- * @param value The `value` of any type to check against an `object` that contains the given `key`.
- * @param key A property key or an array of property keys to check if the given `value` contains them.
+ * Checks if any `value` is an `object` with a key or group of keys of the `PropertyKey` in it or its prototype chain by using
+ * the `in` operator.
+ * @param value The `value` of any type to check against the `object` that contains(or its prototype chain) key or group of keys from
+ * the given `key`.
+ * @param key A property key or an array of property keys to check if the given `value` contains(or its prototype chain) all of them.
  * @param callback A callback `function` of `ResultCallback` type with `payload` parameter of the default `CallbackPayload` shape and the
  * provided `key` to handle the `result` and `payload` of the check before the `result` return. By default it uses `resultCallback()`
  * function.
  * @param payload An optional `object` of a generic type variable `Payload` that is assigned to the `payload` of the provided `callback`.
- * @returns The return value is a `boolean` indicating whether the provided `value` is an `object` that contains provided keys.
+ * @returns The return value is a `boolean` indicating whether the provided `value` is an `object` that contains(or its prototype chain)
+ * provided keys.
  * @angularpackage
  */
-export const isObjectKeyIn = <Type = object, Payload extends object = object>(
+export const isObjectKeyIn = <Obj = object, Payload extends object = object>(
   value: any,
   key: PropertyKey | PropertyKey[],
   callback: ResultCallback<
     CallbackPayload & { key: typeof key } & Payload
   > = resultCallback,
   payload?: Payload
-): value is Type =>
+): value is Obj =>
   callback(
     isObject(value)
       ? isArray(key)
