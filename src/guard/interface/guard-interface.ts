736fa5eb8bbac26aa0d5a3c2a1780e0a28570e62
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-21 01:52:11 +0200

    refactor(Guard): remove `is`.

--- src/guard/interface/guard-interface.ts
+++ src/guard/interface/guard-interface.ts
@@ -3,9 +3,4 @@ import { GuardIs } from './guard-is.interface';
 /**
  * The shape of an object with guard functions.
  */
-export interface Guard extends GuardIs {
-  /**
-   * The object consists prefixed with `is` functions.
-   */
-  is: GuardIs;
-}
+export interface Guard extends GuardIs { }
