de6aa127ec3f0821487448d784d390edc35fb289
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-20 16:39:55 +0200

    refactor(isObject()): change the way of check, change the `payload` parameter type of `isObject()` and the `callback`.

--- src/is/lib/is-object.func.ts
+++ src/is/lib/is-object.func.ts
@@ -1,31 +1,28 @@
 // Function.
 import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
-// Interface.
-import { CallbackPayload } from '../../type/callback-payload.type';
 // Type.
+import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if any `value` is an `object` type, an instance of `Object` and the obtained type from its `Object.prototype` is equal to
- * `'object'`.
- * @param value The `value` of any type to check.
+ * Checks if any value is an `object` type or the type obtained from its `Object.prototype` equal to `'object'`, and an instance of
+ * `Object`.
+ * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
  * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of a generic type variable `Payload` that is assigned to the `payload` of the provided `callback`
- * function.
+ * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is an `object`.
  * @angularpackage
  */
 export const isObject = <Obj = object, Payload extends object = object>(
   value: any,
-  callback: ResultCallback<any, typeof payload> = resultCallback,
+  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
   payload?: CallbackPayload<Payload>
 ): value is Obj =>
   callback(
-    typeOf(value) === 'object' &&
-    typeof value === 'object' &&
-    value instanceof Object === true,
+    (typeof value === 'object' || typeOf(value) === 'object') &&
+    value instanceof Object,
     value,
-    { name: isObject.name, ...payload } as Payload
+    payload
   );
