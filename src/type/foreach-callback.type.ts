7935b50f906a56a625282c30778ddaf1597305a5
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-21 00:13:05 +0200

    refactor(ForEachCallback): remove optional from the `index` and `array` parameters.

--- src/type/foreach-callback.type.ts
+++ src/type/foreach-callback.type.ts
@@ -10,7 +10,7 @@
  export type ForEachCallback<Value = any, Payload = object> = (
   result: boolean,
   value: Value,
-  index?: number,
-  array?: any[],
+  index: number,
+  array: any[],
   payload?: Payload
 ) => void;
