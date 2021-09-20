24621f7b421392226e939161989b28ae0898524f
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-20 16:53:25 +0200

    refactor(StringOfLength): use `MinMax` interface and add generic type variable `Type` to denote the string type.

--- src/type/string-of-length.type.ts
+++ src/type/string-of-length.type.ts
@@ -1,10 +1,13 @@
+// Interface.
+import { MinMax } from '../interface/min-max.interface';
+// Type.
 import { AnyString } from './any-string.type';
 /**
- * A `string` type or an instance of a `String` with its specified minimum and maximum length.
- * Generic variables `Min` and `Max` are constrained with the `number` type.
+ * A generic type `StringOfLength` that takes generic type variable `Min` and `Max` constrained by `number` type as length, and takes
+ * generic type variable `Type` constrained by `AnyString` as the type by default `string` or an instance of `String`.
  */
-export type StringOfLength<Min extends number, Max extends number> =
-  AnyString & {
-    min: Min;
-    max: Max;
-  };
+export type StringOfLength<
+  Min extends number,
+  Max extends number,
+  Type extends AnyString = string
+> = Type & MinMax<Min, Max>;
