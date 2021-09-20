19ded1837f5a2d69ee571ca5a53258a1a529e701
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-20 16:55:59 +0200

    refactor(typeOf()): update jsdoc.

--- src/lib/type-of.func.ts
+++ src/lib/type-of.func.ts
@@ -1,7 +1,7 @@
 /**
- * https://javascript.plainenglish.io/the-best-way-to-type-check-in-vanilla-js-55197b4f45ec
+ * Gets the specific object type of any value.
  * @param value Any value to check its Object.prototype type.
  * @author https://javascript.plainenglish.io/the-best-way-to-type-check-in-vanilla-js-55197b4f45ec
- * @returns string.
+ * @returns The return value is a `string` of the object prototype.
  */
 export const typeOf = (value: any): string => Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
