ad40a8fb0669cbddf64a662ccbb1d202422a7e5c
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-20 02:43:37 +0200

    docs(Guard): update `Guard` interface jsdoc.

--- src/guard/interface/guard-interface.ts
+++ src/guard/interface/guard-interface.ts
@@ -1,4 +1,11 @@
+// Interface.
 import { GuardIs } from './guard-is.interface';
+/**
+ * The shape of an object with guard functions.
+ */
 export interface Guard extends GuardIs {
+  /**
+   * The object consists prefixed with `is` functions.
+   */
   is: GuardIs;
 }
