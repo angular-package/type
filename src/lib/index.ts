9283faa1600bd6ce83c704863bdc8d6eb6de1d6d
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-20 17:04:33 +0200

    refactor(index):  add index to export all from the `lib`.

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+// Class.
+export { BigIntObject } from './bigint-object.class';
+export { BooleanObject } from './boolean-object.class';
+export { NumberObject } from './number-object.class';
+export { PrimitiveObject } from './primitive-object.class';
+export { StringObject } from './string-object.class';
+export { SymbolObject } from './symbol-object.class';
+// Object.
+export { type } from './type.object';
+// Function.
+export { typeOf } from './type-of.func';
