dfb3df9f82116cd2737031b255f0aad62e4c1fa8
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-20 16:57:27 +0200

    feat(type {}): add `type` object consists of `are`, `is`, `guard` prefixed functions.

---
+++ src/lib/type.object.ts
@@ -0,0 +1,23 @@
+// Object.
+import { are } from '../are/lib/are.object';
+import { guard } from '../guard/lib/guard.object';
+import { is } from '../is/lib/is.object';
+/**
+ * The `object` consists of `are`, `is` and `guard` objects.
+ */
+export const type = Object.freeze({
+  /**
+   * The object consists of `are` functions.
+   */
+  are,
+
+  /**
+   * The object consists of `is` functions.
+   */
+  is,
+
+  /**
+   * The `object` consists of `guard` functions.
+   */
+  guard
+});
