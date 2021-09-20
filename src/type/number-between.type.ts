1263a2e92f6722dd9d025f5c5eef0e1ba652fe66
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-20 16:51:48 +0200

    refactor(NumberBetween): use `MinMax` interface and add possibility to set the number type, update jsdoc.

--- src/type/number-between.type.ts
+++ src/type/number-between.type.ts
@@ -1,10 +1,13 @@
+// Type.
 import { AnyNumber } from './any-number.type';
+// Interface.
+import { MinMax } from '../interface/min-max.interface';
 /**
- * A `number` type or an instance of a `Number` with the specified range.
- * Generic variables `Min` and `Max` are constrained with the `number` type.
+ * A generic type `NumberBetween` that takes generic type variable `Min` and `Max` constrained by `number` type as range, and takes generic
+ * type variable `Type` constrained by `AnyNumber` as the type by default `number` or an instance of `Number`.
  */
-export type NumberBetween<Min extends number, Max extends number> =
-  AnyNumber & {
-    min: Min;
-    max: Max;
-  };
+export type NumberBetween<
+  Min extends number,
+  Max extends number,
+  Type extends AnyNumber = number
+> = Type & MinMax<Min, Max>;
