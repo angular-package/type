691f07e2cf31eb3a52923715ef091f86a181a15f
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-09-23 15:06:50 +0200

    feat: add `guardStringIncludes()` and `guardStringIncludesSome()` functions with tests.

---
+++ src/guard/lib/guard-string-includes.func.ts
@@ -0,0 +1,31 @@
+// Function.
+import { isStringIncludes } from '../../is/lib/is-string-includes.func';
+import { resultCallback } from '../../lib/result-callback.func';
+// Type.
+import { AnyString } from '../../type/any-string.type';
+import { CallbackPayload } from '../../type/callback-payload.type';
+import { ResultCallback } from '../../type/result-callback.type';
+/**
+ * Guards the value to be a `string` type or an instance of `String` that includes the specified words/sentences.
+ * @param value The value of a generic type variable `Type` constrained by the `AnyString`, by default of the type captured from the
+ * provided `value` to check against the `string` that contains words/sentences from a given `includes`.
+ * @param includes An `Array` of `string` as words/sentences to be case-sensitive searched for within the given `value`.
+ * @param callback An optional `ResultCallback` function to handle the result before returns.
+ * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @returns The return value is a `boolean` indicating whether the provided `value` is a `string` type or an instance of `String` that
+ * includes the specified words/sentences.
+ * @angularpackage
+ */
+export const guardStringIncludes = <
+  Type extends AnyString,
+  Payload extends object = object
+>(
+  value: Type,
+  includes: string[],
+  callback: ResultCallback<
+    Type,
+    CallbackPayload<{ includes: typeof includes } & Payload>
+  > = resultCallback,
+  payload?: CallbackPayload<Payload>
+): value is Type =>
+  isStringIncludes(value, includes, callback, payload);
