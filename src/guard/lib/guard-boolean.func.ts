7aa99dcf890c7e2702da3f70e765df6a77897d6a
angularpackage <contact@angular-package.dev>
2022-01-22 00:26:45 +0100

    refactor(guardBoolean): remove `CallbackPayload` generic type, set generic type variable `Type` to the `value` parameter and update jsdoc.

--- src/guard/lib/guard-boolean.func.ts
+++ src/guard/lib/guard-boolean.func.ts
@@ -2,13 +2,12 @@
 import { isBoolean } from '../../is/lib/is-boolean.func';
 // Type.
 import { AnyBoolean } from '../../type/any-boolean.type';
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be `boolean` of any type.
- * @param value The value of a generic type variable `Type`, by default of the type captured from the supplied `value`, to guard.
+ * @param value The value of generic type variable `Type` to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a `boolean` type or an instance of `Boolean`.
  */
 export const guardBoolean = <
@@ -16,7 +15,6 @@ export const guardBoolean = <
   Payload extends object = object
 >(
   value: Type,
-  callback?: ResultCallback<Type, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is Type =>
-  isBoolean(value, callback, payload);
+  callback?: ResultCallback<Type, Payload>,
+  payload?: Payload
+): value is Type => isBoolean(value, callback, payload);
