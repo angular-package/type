5f61fb89b3f8d3d97d7f07e11860354faa40e0f2
angularpackage <contact@angular-package.dev>
2022-01-22 00:12:39 +0100

    refactor(guardBigInt): remove `CallbackPayload` generic type, set generic type variable `BigInt` to the `value` parameter and update jsdoc.

--- src/guard/lib/guard-big-int.func.ts
+++ src/guard/lib/guard-big-int.func.ts
@@ -1,18 +1,19 @@
 // Function.
 import { isBigInt } from '../../is/lib/is-big-int.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be a `bigint`.
  * @param value A `bigint` type value to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return `value` is a `boolean` indicating whether the `value` is a `bigint`.
  */
-export const guardBigInt = <Payload extends object>(
-  value: bigint,
-  callback?: ResultCallback<bigint, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is bigint =>
-  isBigInt(value, callback, payload);
+export const guardBigInt = <
+  BigInt extends bigint,
+  Payload extends object = object
+>(
+  value: BigInt,
+  callback?: ResultCallback<bigint, Payload>,
+  payload?: Payload
+): value is BigInt => isBigInt(value, callback, payload);
