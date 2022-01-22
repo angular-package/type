d8c210e663dbc5058eb55ee1b48cdaa71ca3ae75
angularpackage <contact@angular-package.dev>
2022-01-22 18:24:28 +0100

    refactor(guardInstance): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-instance.func.ts
+++ src/guard/lib/guard-instance.func.ts
@@ -1,16 +1,14 @@
 // Function.
 import { isInstance } from '../../is/lib/is-instance.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { Constructor } from '../../type/constructor.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be an instance of the given `constructor`.
- * @param value An `object` of a generic type variable `Obj`, by default of the type captured from the provided `value` to guard and to
- * check if it's an instance of a given `constructor`.
+ * @param value An `object` of a generic type variable `Obj` to guard and be compared with an instance of a given `constructor`.
  * @param constructor A `class` or `function` that specifies the type of the `constructor`.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is an instance of a given `constructor`.
  */
 export const guardInstance = <
@@ -19,10 +17,6 @@ export const guardInstance = <
 >(
   value: Obj,
   constructor: Constructor<Obj>,
-  callback?: ResultCallback<
-    Obj,
-    CallbackPayload<{ ctor: typeof constructor } & Payload>
-  >,
-  payload?: CallbackPayload<Payload>
-): value is Obj =>
-  isInstance(value, constructor, callback, payload);
+  callback?: ResultCallback<Obj, { ctor: typeof constructor } & Payload>,
+  payload?: Payload
+): value is Obj => isInstance(value, constructor, callback, payload);
