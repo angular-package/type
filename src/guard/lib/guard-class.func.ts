e425a2e537d88cc091f661e00b373d48097b1cd4
angularpackage <contact@angular-package.dev>
2022-01-22 16:05:01 +0100

    refactor(guardClass): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-class.func.ts
+++ src/guard/lib/guard-class.func.ts
@@ -1,14 +1,12 @@
 // Function.
 import { isClass } from '../../is/lib/is-class.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be a `class` of generic type variable `Class`.
- * @param value The `class` of a generic type variable `Class`, by default of the type captured from the provided `value` parameter, to
- * guard.
+ * @param value The `class` of a generic type variable `Class` to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is a `class` of a generic type variable `Class`.
  */
 export const guardClass = <
@@ -16,7 +14,6 @@ export const guardClass = <
   Payload extends object = object
 >(
   value: Class,
-  callback?: ResultCallback<Class, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is Class =>
-  isClass(value, callback, payload);
+  callback?: ResultCallback<Class, Payload>,
+  payload?: Payload
+): value is Class => isClass(value, callback, payload);
