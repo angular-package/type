90eacf1b1c7049cd36de36651db4fcf2b426bbd9
angularpackage <contact@angular-package.dev>
2022-01-23 00:15:35 +0100

    refactor(guardObject): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-object.func.ts
+++ src/guard/lib/guard-object.func.ts
@@ -1,13 +1,12 @@
 // Function.
 import { isObject } from '../../is/lib/is-object.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be an `object` of a generic type variable `Obj`.
  * @param value An `object` of a generic type variable `Obj`, by default of the type captured from the provided `value` to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is an `object` of a generic type variable `Obj`.
  */
 export const guardObject = <
@@ -15,7 +14,6 @@ export const guardObject = <
   Payload extends object = object
 >(
   value: Obj,
-  callback?: ResultCallback<Obj, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is Obj =>
-  isObject(value, callback, payload);
+  callback?: ResultCallback<Obj, Payload>,
+  payload?: Payload
+): value is Obj => isObject(value, callback, payload);
