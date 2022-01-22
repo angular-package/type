580658d2634ef37fc3c0a9a6a73a67e7cecdd439
angularpackage <contact@angular-package.dev>
2022-01-22 19:20:35 +0100

    refactor(guardKey): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-key.func.ts
+++ src/guard/lib/guard-key.func.ts
@@ -1,18 +1,16 @@
 // Function.
 import { isKey } from '../../is/lib/is-key.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be one of `string`, `number`, or `symbol` type.
- * @param value The value of `PropertyKey` type to guard.
+ * @param value The value of generic type variable `Key` to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a `string`, `number`, or `symbol`.
  */
-export const guardKey = <Payload extends object>(
-  value: PropertyKey,
-  callback?: ResultCallback<PropertyKey, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is PropertyKey =>
-  isKey(value, callback, payload);
+export const guardKey = <Key extends PropertyKey, Payload extends object>(
+  value: Key,
+  callback?: ResultCallback<Key, Payload>,
+  payload?: Payload
+): value is Key => isKey(value, callback, payload);
