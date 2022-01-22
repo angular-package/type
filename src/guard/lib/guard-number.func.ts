e9eb46027d0ee448edf2a480db42ec1fa6059f3e
angularpackage <contact@angular-package.dev>
2022-01-22 19:33:23 +0100

    refactor(guardNumber): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-number.func.ts
+++ src/guard/lib/guard-number.func.ts
@@ -2,14 +2,12 @@
 import { isNumber } from '../../is/lib/is-number.func';
 // Type.
 import { AnyNumber } from '../../type/any-number.type';
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be a `number` of any type.
- * @param value The value of a generic type variable `Type` constrained by `AnyNumber`, by default of the type captured from the
- * provided `value`, to guard.
+ * @param value The value of generic type variable `Type` to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a `number` type or an instance of `Number`.
  */
 export const guardNumber = <
@@ -17,7 +15,6 @@ export const guardNumber = <
   Payload extends object = object
 >(
   value: Type,
-  callback?: ResultCallback<Type, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is Type =>
-  isNumber(value, callback, payload);
+  callback?: ResultCallback<Type, Payload>,
+  payload?: Payload
+): value is Type => isNumber(value, callback, payload);
