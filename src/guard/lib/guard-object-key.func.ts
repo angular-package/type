4b207c950c1bfbfc0cda21c1227575d53bd00726
angularpackage <contact@angular-package.dev>
2022-01-23 00:38:06 +0100

    refactor(guardObjectKey): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-object-key.func.ts
+++ src/guard/lib/guard-object-key.func.ts
@@ -1,15 +1,13 @@
 // Function.
 import { isObjectKey } from '../../is/lib/is-object-key.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be an `object` of generic type variable `Obj` that contains the given `key`.
- * @param value An `object` of a generic type variable `Obj`, by default of the type captured from the provided `value` that contains the
- * given `key`.
+ * @param value An `object` of a generic type variable `Obj` that contains the given `key` to guard.
  * @param key A key of `Obj` type as the name of the property that the given `value` contains.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is an `object` of generic type variable `Obj` that contains the
  * given `key`.
  */
@@ -20,10 +18,6 @@ export const guardObjectKey = <
 >(
   value: Obj,
   key: Key,
-  callback?: ResultCallback<
-    Obj,
-    CallbackPayload<{ key: typeof key } & Payload>
-  >,
-  payload?: CallbackPayload<Payload>
-): value is Obj =>
-  isObjectKey(value, key, callback, payload as any);
+  callback?: ResultCallback<Obj, { key: typeof key } & Payload>,
+  payload?: Payload
+): value is Obj => isObjectKey(value, key, callback, payload as any);
