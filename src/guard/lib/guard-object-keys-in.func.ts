67aebed9c1c18b363b6a7eb08a4fb91c89dc4fe9
angularpackage <contact@angular-package.dev>
2022-01-23 02:09:22 +0100

    refactor(guardObjectKeysIn): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-object-keys-in.func.ts
+++ src/guard/lib/guard-object-keys-in.func.ts
@@ -1,16 +1,16 @@
 // Function.
 import { isObjectKeysIn } from '../../is/lib/is-object-keys-in.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be an `object` of a generic type variable `Obj` with specified keys in it(or its prototype chain).
- * @param value An object of a generic type variable `Obj`, by default of the type captured from the `value` that contains
- * (or its prototype chain) the given `keys` to guard.
- * @param keys An `Array` of property keys to check that the `value` contains(or its prototype chain).
+ * @param value An object of a generic type variable `Obj`, by default of the type captured from itself that contains(or its prototype
+ * chain) the given `keys` to guard.
+ * @param keys An `Array` of property keys to check whether the given `value` contains(or its prototype chain).
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
- * @returns The `value` is a `boolean` indicating whether the `value` is an `object` with specified `keys` in it(or its prototype chain).
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
+ * @returns The return value is a `boolean` indicating whether the `value` is an `object` with specified `keys` in it(or its prototype
+ * chain).
  */
 export const guardObjectKeysIn = <
   Obj extends object,
@@ -19,9 +19,6 @@ export const guardObjectKeysIn = <
 >(
   value: Obj,
   keys: Key[],
-  callback?: ResultCallback<
-    Obj,
-    CallbackPayload<{ keys: typeof keys } & Payload>
-  >,
-  payload?: CallbackPayload<Payload>
+  callback?: ResultCallback<Obj, { keys: typeof keys } & Payload>,
+  payload?: Payload
 ): value is Obj => isObjectKeysIn(value, keys, callback, payload as any);
