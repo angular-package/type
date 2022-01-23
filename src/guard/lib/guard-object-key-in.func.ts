3808927a298ed954756a2ac9337d73f7acc52d35
angularpackage <contact@angular-package.dev>
2022-01-23 01:36:10 +0100

    refactor(guardObjectKeyIn): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-object-key-in.func.ts
+++ src/guard/lib/guard-object-key-in.func.ts
@@ -1,15 +1,14 @@
 // Function.
 import { isObjectKeyIn } from '../../is/lib/is-object-key-in.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the `value` to be an `object` of a generic type variable `Obj` that contains(or its prototype chain) the given `key`.
- * @param value An `object` of a generic type variable `Obj`, by default of the type captured from the provided `value` that contains
- * (or its prototype chain) the given `key`.
+ * @param value An `object` of a generic type variable `Obj`, by default of the type captured from itself that contains(or its prototype
+ * chain) the given `key`.
  * @param key A key of `Obj` type as the name of the property that the given `value` contains(or its prototype chain).
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is an `object` that contains(or its prototype chain) the given
  * `key`.
  */
@@ -20,10 +19,6 @@ export const guardObjectKeyIn = <
 >(
   value: Obj,
   key: Key,
-  callback?: ResultCallback<
-    Obj,
-    CallbackPayload<{ key: typeof key } & Payload>
-  >,
-  payload?: CallbackPayload<Payload>
-): value is Obj =>
-  isObjectKeyIn(value, key, callback, payload as any);
+  callback?: ResultCallback<Obj, { key: typeof key } & Payload>,
+  payload?: Payload
+): value is Obj => isObjectKeyIn(value, key, callback, payload as any);
