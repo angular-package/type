a6c573e321a8b76b14955252135fd61a574fa765
angularpackage <contact@angular-package.dev>
2022-01-23 20:55:07 +0100

    style(typeOf()): use prettiezer.

--- src/lib/type-of.func.ts
+++ src/lib/type-of.func.ts
@@ -4,4 +4,5 @@
  * @author https://javascript.plainenglish.io/the-best-way-to-type-check-in-vanilla-js-55197b4f45ec
  * @returns The return value is a `string` of the object prototype.
  */
-export const typeOf = (value: any): string => Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
+export const typeOf = (value: any): string =>
+  Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
