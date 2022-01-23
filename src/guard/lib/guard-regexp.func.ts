bd2ed04623d7eead347c4ae674d9161f688f11ae
angularpackage <contact@angular-package.dev>
2022-01-23 20:53:53 +0100

    refactor(guardRegExp): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-regexp.func.ts
+++ src/guard/lib/guard-regexp.func.ts
@@ -1,18 +1,16 @@
 // Function.
 import { isRegExp } from '../../is/lib/is-regexp.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be a `RegExp`.
- * @param value A `RegExp` type value to guard.
+ * @param value Regular expression of generic type variable `Type` constrained by `RegExp` to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return `value` is a `boolean` indicating whether the `value` is a `RegExp`.
  */
-export const guardRegExp = <Payload extends object>(
-  value: RegExp,
-  callback?: ResultCallback<RegExp, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is RegExp =>
-  isRegExp(value, callback, payload);
+export const guardRegExp = <Type extends RegExp, Payload extends object>(
+  value: Type,
+  callback?: ResultCallback<Type, Payload>,
+  payload?: Payload
+): value is Type => isRegExp(value, callback, payload);
