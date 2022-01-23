419c1357cfa2f58d5eee905acc3019640ff7d31a
angularpackage <contact@angular-package.dev>
2022-01-24 00:10:11 +0100

    refactor(guardPrimitive()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-primitive.func.ts
+++ src/guard/lib/guard-primitive.func.ts
@@ -1,17 +1,16 @@
 // Function.
 import { isPrimitive } from '../../is/lib/is-primitive.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { Primitive } from '../../type/primitive.type';
 import { Primitives } from '../../type/primitives.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be the `Primitive` type or the given `type` of the `Primitives`.
- * @param value The value of a generic type variable `Type` constrained by the `Primitive`, by default of the type captured from the
- * provided `value` to guard.
+ * @param value The value of a generic type variable `Type` constrained by the `Primitive`, by default of the type captured from itself to
+ * guard.
  * @param type An optional specific type of `Primitives` to check the given value.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is the `Primitive` type or the given `type`.
  */
 export const guardPrimitive = <
@@ -20,7 +19,6 @@ export const guardPrimitive = <
 >(
   value: Type,
   type?: Primitives,
-  callback?: ResultCallback<Type, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is Type =>
-  isPrimitive(value, type, callback, payload);
+  callback?: ResultCallback<Type, Payload>,
+  payload?: Payload
+): value is Type => isPrimitive(value, type, callback, payload);
