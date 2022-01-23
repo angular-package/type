ff7004bbeb75018e690ff47da3581ecdae6c1f5a
angularpackage <contact@angular-package.dev>
2022-01-23 01:43:42 +0100

    refactor(guardObjectKeys): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-object-keys.func.ts
+++ src/guard/lib/guard-object-keys.func.ts
@@ -1,16 +1,15 @@
 // Function.
 import { isObjectKeys } from '../../is/lib/is-object-keys.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be an `object` of a generic type variable `Obj` with its specified `keys`.
- * @param value An object of a generic type variable `Obj`, by default of the type captured from the `value` that contains the given `keys`
- * to guard.
- * @param keys An `Array` of property keys to check that the `value` contains.
+ * @param value An object of a generic type variable `Obj`, by default of the type captured from itself that contains the given `keys` to
+ * guard.
+ * @param keys An `Array` of property keys to check whether the given `value` contains.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
- * @returns The `value` is a `boolean` indicating whether the `value` is an `object` with its specified `keys`.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
+ * @returns The return value is a `boolean` indicating whether the `value` is an `object` with its specified `keys`.
  */
 export const guardObjectKeys = <
   Obj extends object,
@@ -19,9 +18,6 @@ export const guardObjectKeys = <
 >(
   value: Obj,
   keys: Key[],
-  callback?: ResultCallback<
-    Obj,
-    CallbackPayload<{ keys: typeof keys } & Payload>
-  >,
-  payload?: CallbackPayload<Payload>
+  callback?: ResultCallback<Obj, { keys: typeof keys } & Payload>,
+  payload?: Payload
 ): value is Obj => isObjectKeys(value, keys, callback, payload as any);
