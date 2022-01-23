65090023b368fa84a0917ddb3b980c0989805766
angularpackage <contact@angular-package.dev>
2022-01-23 18:56:09 +0100

    refactor(guardStringLength): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-string-length.func.ts
+++ src/guard/lib/guard-string-length.func.ts
@@ -1,7 +1,6 @@
 // Function.
 import { isStringLength } from '../../is';
 // Interface.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { MinMax } from '../../interface/min-max.interface';
 // Type.
 import { AnyString } from '../../type/any-string.type';
@@ -9,11 +8,11 @@ import { ResultCallback } from '../../type/result-callback.type';
 import { StringOfLength } from '../../type/string-of-length.type';
 /**
  * Guards the value to be a `string` of a length between the specified range.
- * @param value The value of a generic type variable `Type` constrained by `AnyString`, by default of the type captured from the
- * provided `value` to guard.
+ * @param value The value of a generic type variable `Type` constrained by `AnyString`, by default of the type captured from itself to
+ * guard.
  * @param length An `object` of optional minimum and a maximum `length` of the given `value`.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a `string` type or an instance of `String` of a length between
  * the specified range.
  */
@@ -24,8 +23,8 @@ export const guardStringLength = <
   Payload extends object = object
 >(
   value: Type,
-  length: MinMax<Min, Max>,
-  callback?: ResultCallback<Type, CallbackPayload<MinMax<Min, Max> & Payload>>,
-  payload?: CallbackPayload<Payload>
+  length: MinMax<Min, Max> | Min | Max,
+  callback?: ResultCallback<Type, MinMax<Min, Max> & Payload>,
+  payload?: Payload
 ): value is StringOfLength<Min, Max, Type> =>
   isStringLength(value, length, callback, payload);
