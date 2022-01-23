bfc3523b46150fc1f58fe960c824e130521764ae
angularpackage <contact@angular-package.dev>
2022-01-23 18:13:02 +0100

    refactor(guardSymbol): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-symbol.func.ts
+++ src/guard/lib/guard-symbol.func.ts
@@ -1,18 +1,16 @@
 // Function.
 import { isSymbol } from '../../is/lib/is-symbol.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be a `symbol`.
  * @param value A `symbol` type value to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a `symbol`.
  */
 export const guardSymbol = <Payload extends object>(
   value: symbol,
-  callback?: ResultCallback<symbol, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is symbol =>
-  isSymbol(value, callback, payload);
+  callback?: ResultCallback<symbol, Payload>,
+  payload?: Payload
+): value is symbol => isSymbol(value, callback, payload);
