271d6a73c3f0ac9024f4aa42371e3bbe45579bc9
angularpackage <contact@angular-package.dev>
2022-01-23 18:03:09 +0100

    refactor(guardType): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-type.func.ts
+++ src/guard/lib/guard-type.func.ts
@@ -1,23 +1,20 @@
 // Function.
 import { isType } from '../../is/lib/is-type.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 import { Type } from '../../type/type.type';
 import { Types } from '../../type/types.type';
 /**
- * Guards the value to be a type from a given `type`.
- * @param value The value of a generic type variable `T` constrained by the `Type`, by default of the type captured from the provided
- * `value` to guard.
+ * Guards the value to be a type of given `type`.
+ * @param value The value of a generic type variable `T` constrained by the `Type`, by default of the type captured from itself, to guard.
  * @param type The value of `string` or `Constructor` type of the `Types` indicates against which type a given `value` is checked.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a type from a given `type`.
  */
 export const guardType = <T extends Type, Payload extends object = object>(
   value: T,
   type: Types<T>,
-  callback?: ResultCallback<T, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is T =>
-  isType(value, type, callback, payload);
+  callback?: ResultCallback<T, Payload>,
+  payload?: Payload
+): value is T => isType(value, type, callback, payload);
