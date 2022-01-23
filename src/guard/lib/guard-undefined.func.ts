604f0fa5aa8da58ff606d46d9d68e753debca9a0
angularpackage <contact@angular-package.dev>
2022-01-23 02:52:39 +0100

    refactor(guardUndefined): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-undefined.func.ts
+++ src/guard/lib/guard-undefined.func.ts
@@ -1,18 +1,16 @@
 // Function.
 import { isUndefined } from '../../is/lib/is-undefined.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be `undefined`.
  * @param value The value of an `undefined` type to guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is `undefined`.
  */
 export const guardUndefined = <Payload extends object>(
   value: undefined,
-  callback?: ResultCallback<undefined, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is undefined =>
-  isUndefined(value, callback, payload);
+  callback?: ResultCallback<undefined, Payload>,
+  payload?: Payload
+): value is undefined => isUndefined(value, callback, payload);
