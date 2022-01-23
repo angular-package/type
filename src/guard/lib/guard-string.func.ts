d05400527f5c951b5593686017d7a91fb8d56160
angularpackage <contact@angular-package.dev>
2022-01-23 21:33:12 +0100

    refactor(guardString()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/guard/lib/guard-string.func.ts
+++ src/guard/lib/guard-string.func.ts
@@ -2,14 +2,13 @@
 import { isString } from '../../is/lib/is-string.func';
 // Type.
 import { AnyString } from '../../type/any-string.type';
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Guards the value to be `string` of any type.
- * @param value The value of a generic type variable `Type` constrained by the `AnyString`, by default of the type captured from the
- * provided `value` to guard.
+ * @param value The value of a generic type variable `Type` constrained by the `AnyString`, by default of the type captured from itself to
+ * guard.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
- * @param payload An optional `object` of `CallbackPayload<Payload>` that is assigned to the `payload` of the provided `callback` function.
+ * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a `string` type or an instance of `String`.
  */
 export const guardString = <
@@ -17,7 +16,6 @@ export const guardString = <
   Payload extends object = object
 >(
   value: Type,
-  callback?: ResultCallback<Type, CallbackPayload<Payload>>,
-  payload?: CallbackPayload<Payload>
-): value is Type =>
-  isString(value, callback, payload);
+  callback?: ResultCallback<Type, Payload>,
+  payload?: Payload
+): value is Type => isString(value, callback, payload);
