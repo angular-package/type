59c034b51bbea366c74e65fdb988648b52eb56b7
angularpackage <contact@angular-package.dev>
2022-01-24 14:59:04 +0100

    docs(Defined): update.

--- src/type/defined.type.ts
+++ src/type/defined.type.ts
@@ -1,6 +1,5 @@
-import { Never } from './never.type';
 /**
- * A generic type `Defined` that takes generic type variable `Type` constrained by `undefined` by using `Never` which constraint causes its
- * change to `never`.
+ * A generic type `Defined` indicates the generic type variable `Type` is never `undefined`.
+ * It takes generic type variable `Type` constrained by `undefined` by using `Never` which constraint causes its change to `never`.
  */
 export type Defined<Type> = Type extends undefined ? never : Type;
