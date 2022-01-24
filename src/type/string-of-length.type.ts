ed703cfec73fff756402d2f2f500381ff8f3aafb
angularpackage <contact@angular-package.dev>
2022-01-24 14:31:06 +0100

    docs(StringOfLength): update.

--- src/type/string-of-length.type.ts
+++ src/type/string-of-length.type.ts
@@ -3,9 +3,8 @@ import { MinMax } from '../interface/min-max.interface';
 // Type.
 import { AnyString } from './any-string.type';
 /**
- * A generic type `StringOfLength` represents `string` type or an instance of `String` of length between a specified range.
- * It takes generic type variable `Min` and `Max` constrained by `number` type as length, and takes generic type variable `Type` constrained
- * by generic type `AnyString` as the type.
+ * A `string` type or an instance of `String` of length between a specified range takes generic type variable `Min` and `Max` constrained by
+ * `number` type as length, and takes generic type variable `Type` constrained by generic type `AnyString` as the type.
  */
 export type StringOfLength<
   Min extends number,
