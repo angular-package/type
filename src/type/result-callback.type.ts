e022e89f4474d7ae2ee5337af44f3c4631cf9d82
angularpackage <contact@angular-package.dev>
2022-01-24 01:08:39 +0100

    docs(ResultCallback): update.

--- src/type/result-callback.type.ts
+++ src/type/result-callback.type.ts
@@ -1,12 +1,12 @@
 /**
  * Represents a callback function with parameters, the `value` that has been checked, the `result` of this check, and an optional
  * `payload`.
- * @var Value A generic type variable `Value` by default equal to `any` determines the type of the `value` parameter.
- * @var Payload The shape of the optional `payload` parameter, by default equal to the `object`.
- * @param result The result of the check of a `boolean` type.
- * @param value The value that has been checked of a generic type variable `Value`.
+ * @var Value A generic type variable `Value` by default `any` determines `value` parameter type.
+ * @var Payload Generic type variable `Payload` indicates `payload` parameter type, which by default is `object`.
+ * @param result The check result of a `boolean` type of the given `value`.
+ * @param value The value of the generic type variable Value to check and which the given check `result` applies.
  * @param payload An optional `object` of a generic type variable `Payload` to provide more data.
- * @returns The return value is a `boolean`.
+ * @returns The return value is a `boolean` indicating the check result of the given `value`.
  */
  export type ResultCallback<Value = any, Payload = object> = (
   result: boolean,
