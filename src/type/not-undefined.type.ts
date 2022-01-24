85c3770b27fa83a1bddc53b86241b1be244f25bb
angularpackage <contact@angular-package.dev>
2022-01-24 14:47:51 +0100

    refactor(NotUndefined): remove null, it's not undefined.

--- src/type/not-undefined.type.ts
+++ src/type/not-undefined.type.ts
@@ -1,5 +1,5 @@
 /**
- * A generic type `NotUndefined` that takes generic type variable `Type` constrained by `undefined` and `null` which constraint causes its
- * change to `never`.
+ * A generic type `NotUndefined` represents any type instead of `undefined`. It takes generic type variable `Type` constrained by
+ * `undefined` which constraint causes its change to `never`.
  */
-export type NotUndefined<Type> = Type extends undefined | null ? never : Type;
+export type NotUndefined<Type> = Type extends undefined ? never : Type;
