45ddedbe3d2c4481115bc1e4ac3f82d52f0cd6fb
angularpackage <contact@angular-package.dev>
2022-01-24 14:35:21 +0100

    docs(NumberBetween): update.

--- src/type/number-between.type.ts
+++ src/type/number-between.type.ts
@@ -3,11 +3,13 @@ import { AnyNumber } from './any-number.type';
 // Interface.
 import { MinMax } from '../interface/min-max.interface';
 /**
- * A generic type `NumberBetween` that takes generic type variable `Min` and `Max` constrained by `number` type as range, and takes generic
- * type variable `Type` constrained by `AnyNumber` as the type by default `number` or an instance of `Number`.
+ * A `number` type or an instance of `Number` between a specified range takes generic type variable `Min` and `Max` constrained by `number`
+ * type as range, and takes generic type variable `Type` constrained by generic type `AnyNumber` as the type.
  */
 export type NumberBetween<
   Min extends number,
   Max extends number,
   Type extends AnyNumber = number
 > = Type & MinMax<Min, Max>;
+
+
