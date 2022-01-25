1b7d28a19a35d7185dfb75fb928fc7bc6d6d97b1
angularpackage <contact@angular-package.dev>
2022-01-25 15:15:04 +0100

    refactor(isArray()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-array.func.ts
+++ src/is/lib/is-array.func.ts
@@ -2,27 +2,30 @@
 import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if any value is the type obtained from its `Object.prototype` equal to `'array'` or an `object` type. The value is also checked
- * by the `isArray()` method of `Array`.
+ * Checks if any value is the type obtained from its object class equal to `'array'` or an `object` type. The value is also checked by the
+ * `isArray()` method of `Array`.
+ * @var Type The `Type` generic type variable indicates the `array` element type, which by default is `any` of the given `value` via the
+ * return type.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isArray()` function from which it captures its value.
  * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is an `Array`.
  * @angularpackage
  */
 export const isArray = <Type = any, Payload extends object = object>(
   value: any,
-  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, Payload> = resultCallback,
+  payload?: Payload
 ): value is Array<Type> =>
   callback(
     (typeOf(value) === 'array' || typeof value === 'object') &&
-    Array.isArray(value),
+      Array.isArray(value),
     value,
     payload
   );
