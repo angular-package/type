057b24d466948204e0235c2f66dbbae1836649ca
angularpackage <contact@angular-package.dev>
2022-01-25 20:32:17 +0100

    refactor(isStringLength()): remove `CallbackPayload` in favor of generic type variable `Payload`, change parameter `length` to `min` and `max`, and update jsdoc.

--- src/is/lib/is-string-length.func.ts
+++ src/is/lib/is-string-length.func.ts
@@ -1,23 +1,24 @@
 // Function.
 import { isNumberType } from './is-number-type.func';
-import { isObject } from './is-object.func';
 import { isString } from './is-string.func';
 import { resultCallback } from '../../lib/result-callback.func';
 // Interface.
 import { MinMax } from '../../interface/min-max.interface';
 // Type.
 import { AnyString } from '../../type/any-string.type';
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 import { StringOfLength } from '../../type/string-of-length.type';
 /**
- * Checks if any value is a `string` type or an instance of `String` by using `isString()` of `length` within the specified range.
+ * Checks if any value is a `string` type or an instance of `String` by using `isString()` of length within the specified range.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isStringLength()` function from which it captures its value.
  * @param value The value of any type to check.
- * @param length An `object` of optional minimum and a maximum `length` of the given `value`.
+ * @param min The optional **minimum** length of generic type variable `Min` for a given `value`.
+ * @param max The optional **maximum** length of generic type variable `Max` for a given `value`.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape, with the minimum and maximum `length` and optional properties from the
+ * check, and `payload` of generic type variable `Payload`, with the minimum and maximum `length` and optional properties from the
  * provided `payload`, to handle them before the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is a `string` type or an instance of `String` of length
  * within the specified range.
  * @angularpackage
@@ -29,26 +30,16 @@ export const isStringLength = <
   Payload extends object = object
 >(
   value: any,
-  length: MinMax<Min, Max>,
-  callback: ResultCallback<
-    any,
-    CallbackPayload<MinMax<Min, Max> & Payload>
-  > = resultCallback,
-  payload?: CallbackPayload<Payload>
+  min?: Min,
+  max?: Max,
+  callback: ResultCallback<any, MinMax<Min, Max> & Payload> = resultCallback,
+  payload?: Payload
 ): value is StringOfLength<Min, Max, Type> =>
   callback(
-    isString(value) && isObject(length)
-    ? (isNumberType(length.min) && length.min >= 0
-        ? value.valueOf().length >= length.min
-        : true) &&
-        (isNumberType(length.max) && length.max >= 0
-          ? value.valueOf().length <= length.max
-          : true)
+    isString(value)
+    ? (isNumberType(min) && min >= 0 ? value.valueOf().length >= min : true) &&
+      (isNumberType(max) && max >= 0 ? value.valueOf().length <= max : true)
     : false,
     value,
-    {
-      ...payload,
-      min: length?.min,
-      max: length?.max,
-    } as Payload
+    { ...payload, min, max } as Payload
   );
