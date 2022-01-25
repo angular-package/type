ca5b2a941606275a156e63ba8b253598d8d0ddce
angularpackage <contact@angular-package.dev>
2022-01-25 15:53:38 +0100

    refactor(isBooleanObject()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-boolean-object.func.ts
+++ src/is/lib/is-boolean-object.func.ts
@@ -2,23 +2,22 @@
 import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if any value is of the type obtained from its `Object.prototype` equal to `'boolean'` or an `object` type, and an instance of
- * `Boolean` that is equal to `true` or `false`.
+ * Checks if any value is of the type obtained from its object class equal to `'boolean'` or an `object` type, and an instance of `Boolean`
+ * and equal `true` or `false`.
  * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is an instance of `Boolean`.
  * @angularpackage
  */
 export const isBooleanObject = <Payload extends object>(
   value: any,
-  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, Payload> = resultCallback,
+  payload?: Payload
 ): value is Boolean =>
   callback(
     (typeOf(value) === 'boolean' || typeof value === 'object') &&
