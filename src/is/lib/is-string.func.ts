431341155c8b1743844696f531fb23a9ed64f05b
angularpackage <contact@angular-package.dev>
2022-01-25 23:31:37 +0100

    refactor(isString()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-string.func.ts
+++ src/is/lib/is-string.func.ts
@@ -4,16 +4,19 @@ import { isStringObject } from './is-string-object.func';
 import { isStringType } from './is-string-type.func';
 // Type.
 import { AnyString } from '../../type/any-string.type';
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any value is a `string` type by using the `isStringType()` function or an instance of `String` by using the
  * `isStringObject()` function.
+ * @var Type A generic type variable `Type` constrained by `AnyString` indicates the captured `string` type of the given `value` via the
+ * return type and the `value` parameter of the provided `callback` function of `ResultCallback` type.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isString()` function from which it captures its value.
  * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is a `string` type or an instance of `String`.
  * @angularpackage
  */
@@ -22,7 +25,7 @@ export const isString = <
   Payload extends object = object
 >(
   value: any,
-  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, Payload> = resultCallback,
+  payload?: Payload
 ): value is Type =>
   callback(isStringType(value) || isStringObject(value), value, payload);
