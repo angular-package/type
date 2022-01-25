7a42d07bb072519238390a9cec7cb76ae0c11e8a
angularpackage <contact@angular-package.dev>
2022-01-25 18:16:56 +0100

    refactor(isStringObject()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-string-object.func.ts
+++ src/is/lib/is-string-object.func.ts
@@ -2,23 +2,24 @@
 import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any value is of the type obtained from its `Object.prototype` equal to `'string'` or an `object` type, and an instance of
  * `String`.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isStringObject()` function from which it captures its value.
  * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is an instance of `String`.
  * @angularpackage
  */
 export const isStringObject = <Payload extends object>(
   value: any,
-  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, Payload> = resultCallback,
+  payload?: Payload
 ): value is String =>
   callback(
     (typeOf(value) === 'string' || typeof value === 'object') &&
