7bdfba7a4c71294d34d8d96e2f2dc709dd1a5e68
angularpackage <contact@angular-package.dev>
2022-01-26 00:23:38 +0100

    refactor(isClass()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-class.func.ts
+++ src/is/lib/is-class.func.ts
@@ -2,23 +2,26 @@
 import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if any value is a `function` type or of the type obtained from its `Object.prototype` equal to `'function'` and an instance of
- * `Function`. It also confirms it's a `class` by using `regexp` on the obtained string from its `Function.prototype`.
+ * Checks if any value is a `function` type or the type obtained from its object class equal to `'function'` and an instance of
+ * `Function`. It also confirms it's a `class` by checking whether the function converted with `Function.prototype.toString()` to string
+ * contains the word `class` at the beginning.
+ * @var Class The `Class` generic type variable indicates the class type of the given `value` via the return type, by default `Function`.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isBoolean()` function from which it captures its value.
  * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is a `class`.
  * @angularpackage
  */
 export const isClass = <Class = Function, Payload extends object = object>(
   value: any,
-  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, Payload> = resultCallback,
+  payload?: Payload
 ): value is Class =>
   callback(
     typeof value === 'function' ||
