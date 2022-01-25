ed60301aad330bc53a2b2a972c5dec08c09bdb32
angularpackage <contact@angular-package.dev>
2022-01-25 15:39:07 +0100

    refactor(isBoolean()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-boolean.func.ts
+++ src/is/lib/is-boolean.func.ts
@@ -2,17 +2,18 @@
 import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { AnyBoolean } from '../../type/any-boolean.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Checks if any value is a `boolean` type, or the obtained type from its `Object.prototype` equal to `'boolean'`, or an `object` type
- * and an instance of `Boolean` that is equal to `true` or `false`.
+ * Checks if any value is a `boolean` type, or the obtained type from its object class equal to `'boolean'`, or an `object` type and an
+ * instance of `Boolean` that is equal to `true` or `false`.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isBigInt()` function from which it captures its value.
  * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is a `boolean` type or an instance of `Boolean`.
  * @angularpackage
  */
@@ -21,8 +22,8 @@ export const isBoolean = <
   Payload extends object = object
 >(
   value: any,
-  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, Payload> = resultCallback,
+  payload?: Payload
 ): value is Type =>
   callback(
     (typeof value === 'boolean' ||
