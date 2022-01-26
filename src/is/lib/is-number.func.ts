b115e5b87f4219b6a2e2288f3c1b9b1184808201
angularpackage <contact@angular-package.dev>
2022-01-26 15:14:35 +0100

    refactor(isNumber()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-number.func.ts
+++ src/is/lib/is-number.func.ts
@@ -3,17 +3,20 @@ import { resultCallback } from '../../lib/result-callback.func';
 import { typeOf } from '../../lib/type-of.func';
 // Type.
 import { AnyNumber } from '../../type/any-number.type';
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any value is a `number` type, or the type obtained from its `Object.prototype` equal to `'number'` or an `object` type
  * and an instance of `Number`. The value is also checked by the `Number.isFinite()` method to determine whether it's finite and is
  * validated by the `Number.isNaN()` method.
+ * @var Type A generic type variable `Type` constrained by `AnyNumber` indicates the captured `number` type of the given `value` via the
+ * return type and the `value` parameter of the provided `callback` function of `ResultCallback` type.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isDate()` function from which it captures its value.
  * @param value The value of any type to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is a finite number of a `number` type or an instance of
  * `Number`.
  * @angularpackage
@@ -23,8 +26,8 @@ export const isNumber = <
   Payload extends object = object
 >(
   value: any,
-  callback: ResultCallback<any, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, Payload> = resultCallback,
+  payload?: Payload
 ): value is Type =>
   callback(
     (typeof value === 'number' ||
