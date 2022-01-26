62f8ab39db2af10cc5dcaff9e1474afb30495d63
angularpackage <contact@angular-package.dev>
2022-01-26 19:28:25 +0100

    refactor(isObjectKey()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-object-key.func.ts
+++ src/is/lib/is-object-key.func.ts
@@ -2,27 +2,29 @@
 import { isObject } from './is-object.func';
 import { resultCallback } from '../../lib/result-callback.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any value is an `object` by using the `isObject()` function with its key of the `PropertyKey` type.
+ * @var Obj A generic type variable `Obj` indicates the type of `value` parameter by default `object` via the return type `value is Obj`.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isObjectKey()` function from which it captures its value.
  * @param value The value of any type to check against an `object` that contains a key from a given `key`.
  * @param key A property key to check if a given `value` contains.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape, with the provided `key` and optional properties from the provided
+ * check, and `payload` of generic type variable `Payload`, with the provided `key` and optional properties from the provided
  * `payload`, to handle them before the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is an `object` with its `key`.
  * @angularpackage
  */
-export const isObjectKey = <Obj extends object, Payload extends object = object>(
+export const isObjectKey = <
+  Obj extends object,
+  Payload extends object = object
+>(
   value: any,
   key: PropertyKey,
-  callback: ResultCallback<
-    Obj,
-    CallbackPayload<{ key: typeof key } & Payload>
-  > = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, { key: typeof key } & Payload> = resultCallback,
+  payload?: Payload
 ): value is Obj =>
   callback(
     isObject(value) ? {}.hasOwnProperty.call(value, key) : false,
