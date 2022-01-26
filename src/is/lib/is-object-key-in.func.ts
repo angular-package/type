8d92e344b259ee0c270f9c2635138cb6d95bb1e9
angularpackage <contact@angular-package.dev>
2022-01-26 19:45:30 +0100

    refactor(isObjectKeyIn()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/lib/is-object-key-in.func.ts
+++ src/is/lib/is-object-key-in.func.ts
@@ -2,17 +2,19 @@
 import { isObject } from './is-object.func';
 import { resultCallback } from '../../lib/result-callback.func';
 // Type.
-import { CallbackPayload } from '../../type/callback-payload.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
  * Checks if any value is an `object` by using the `isObject()` function with a key of the `PropertyKey` in it(or its prototype chain) by
  * using the `in` operator.
+ * @var Obj A generic type variable `Obj` indicates the type of `value` parameter by default `object` via the return type `value is Obj`.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isObjectKeyIn()` function from which it captures its value.
  * @param value The value of any type to check against an `object` that contains(or its prototype chain) key from a given `key`.
  * @param key A property key to check if a given `value` contains(or its prototype chain).
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape, with the provided `key` and optional properties from the provided `payload`,
+ * check, and `payload` of generic type variable `Payload`, with the provided `key` and optional properties from the provided `payload`,
  * to handle them before the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is an `object` that contains(or its prototype chain) a
  * given `key`.
  * @angularpackage
@@ -20,11 +22,8 @@ import { ResultCallback } from '../../type/result-callback.type';
 export const isObjectKeyIn = <Obj = object, Payload extends object = object>(
   value: any,
   key: PropertyKey,
-  callback: ResultCallback<
-    any,
-    CallbackPayload<{ key: typeof key } & Payload>
-  > = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<any, { key: typeof key } & Payload> = resultCallback,
+  payload?: Payload
 ): value is Obj =>
   callback(isObject(value) ? key in value : false, value, {
     ...payload,
