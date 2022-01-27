4d8f70a965d64d676d97ae5ed6c05295c918a554
angularpackage <contact@angular-package.dev>
2022-01-27 15:12:10 +0100

    refactor(isNotUndefined()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/not/lib/is-not-undefined.func.ts
+++ src/is/not/lib/is-not-undefined.func.ts
@@ -2,23 +2,22 @@
 import { resultCallback } from '../../../lib/result-callback.func';
 import { typeOf } from '../../../lib/type-of.func';
 // Type.
-import { CallbackPayload } from '../../../type/callback-payload.type';
 import { Defined } from '../../../type/defined.type';
 import { ResultCallback } from '../../../type/result-callback.type';
 /**
- * Checks if the `value` is not the type obtained from its `Object.prototype` equal to `'undefined'`, not an `undefined` type and not
- * equal to `undefined`.
- * @param value The `value` of a generic type variable `Type`, by default of the type captured from the provided `value` to check.
+ * Checks if the `value` is not the type obtained from its object class equal to `'undefined'`, not an `undefined` type and not equal to
+ * `undefined`.
+ * @param value The `value` of a generic type variable `Type`, by default of the type captured from itself to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of the generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is not `undefined`.
  */
 export const isNotUndefined = <Type, Payload extends object = object>(
   value: Type,
-  callback: ResultCallback<Type, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<Type, Payload> = resultCallback,
+  payload?: Payload
 ): value is Defined<Type> =>
   callback(
     typeOf(value) !== 'undefined' &&
