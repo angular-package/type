e88e987434725bf4df86bf4f78c286d091990184
angularpackage <contact@angular-package.dev>
2022-01-27 16:59:51 +0100

    chore(index): remove experimental from index.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,5 @@
-// Class.
-export { BigIntObject } from './bigint-object.class';
-export { BooleanObject } from './boolean-object.class';
-export { NumberObject } from './number-object.class';
-export { PrimitiveObject } from './primitive-object.class';
-export { StringObject } from './string-object.class';
-export { SymbolObject } from './symbol-object.class';
 // Object.
 export { type } from './type.object';
 // Function.
+export { resultCallback } from './result-callback.func';
 export { typeOf } from './type-of.func';
