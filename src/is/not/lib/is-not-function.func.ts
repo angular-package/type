ac84e88bd8b6ad358ab7d2f8ebe51fd51e7ef71a
angularpackage <contact@angular-package.dev>
2022-01-27 14:50:39 +0100

    docs: update `callback` parameter.

--- src/is/not/lib/is-not-function.func.ts
+++ src/is/not/lib/is-not-function.func.ts
@@ -13,7 +13,7 @@ import { ResultCallback } from '../../../type/result-callback.type';
  * supplied `callback` function and `payload` optional parameter of the `isNotFunction()` function from which it captures its value.
  * @param value The `value` of a generic type variable `Type`, by default of the type captured from itself to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of the generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
  * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is not a `function`.
