d96e7509210f2699fcbe1ae70632346d34f531c6
angularpackage <contact@angular-package.dev>
2022-01-27 02:32:48 +0100

    style: angularpackage.

--- src/guard/lib/guard-type.func.ts
+++ src/guard/lib/guard-type.func.ts
@@ -11,6 +11,7 @@ import { Types } from '../../type/types.type';
  * @param callback An optional `ResultCallback` function to handle the result before returns.
  * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a type from a given `type`.
+ * @angularpackage
  */
 export const guardType = <T extends Type, Payload extends object = object>(
   value: T,
--- src/guard/lib/guard-undefined.func.ts
+++ src/guard/lib/guard-undefined.func.ts
@@ -8,6 +8,7 @@ import { ResultCallback } from '../../type/result-callback.type';
  * @param callback An optional `ResultCallback` function to handle the result before returns.
  * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is `undefined`.
+ * @angularpackage
  */
 export const guardUndefined = <Payload extends object>(
   value: undefined,
