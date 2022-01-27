f101c54c2bac5fc0158743b36cb0d5b8f2cc565c
angularpackage <contact@angular-package.dev>
2022-01-27 13:34:43 +0100

    refactor(isNotBoolean()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/not/lib/is-not-boolean.func.ts
+++ src/is/not/lib/is-not-boolean.func.ts
@@ -3,13 +3,16 @@ import { resultCallback } from '../../../lib/result-callback.func';
 import { typeOf } from '../../../lib/type-of.func';
 // Type.
 import { AnyBoolean } from '../../../type/any-boolean.type';
-import { CallbackPayload } from '../../../type/callback-payload.type';
 import { Never } from '../../../type/never.type';
 import { ResultCallback } from '../../../type/result-callback.type';
 /**
- * Checks if the `value` is not the type obtained from its `Object.prototype` equal to `'boolean'`, not a `boolean` type and not an
- * instance of `Boolean`.
- * @param value The `value` of generic type variable `Type`, by default of the type captured from the provided `value` to check.
+ * Checks if the `value` is not the type obtained from its object class equal to `'boolean'`, not a `boolean` type and not an instance of
+ * `Boolean`.
+ * @var Type A generic type variable `Type` indicates the captured type of the given `value` via the return type
+ * `value is Never<AnyBoolean, Type>` and the `value` parameter of the provided `callback` function `ResultCallback` type.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isNotBoolean()` function from which it captures its value.
+ * @param value The `value` of generic type variable `Type`, by default of the type captured from itself to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
  * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
@@ -19,8 +22,8 @@ import { ResultCallback } from '../../../type/result-callback.type';
  */
 export const isNotBoolean = <Type, Payload extends object = object>(
   value: Type,
-  callback: ResultCallback<Type, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<Type, Payload> = resultCallback,
+  payload?: Payload
 ): value is Never<AnyBoolean, Type> =>
   callback(
     typeOf(value) !== 'boolean' &&
