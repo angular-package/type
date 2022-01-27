1914eda2a03a137f49b728a950a8f250a4456e07
angularpackage <contact@angular-package.dev>
2022-01-27 15:06:47 +0100

    refactor(isNotNumber()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/not/lib/is-not-number.func.ts
+++ src/is/not/lib/is-not-number.func.ts
@@ -3,24 +3,27 @@ import { resultCallback } from '../../../lib/result-callback.func';
 import { typeOf } from '../../../lib/type-of.func';
 // Type.
 import { AnyNumber } from '../../../type/any-number.type';
-import { CallbackPayload } from '../../../type/callback-payload.type';
 import { Never } from '../../../type/never.type';
 import { ResultCallback } from '../../../type/result-callback.type';
 /**
- * Checks if the `value` is not the type obtained from its `Object.prototype` equal to `'number'`, not a `number` type and not an instance
- * of `Number`.
- * @param value The `value` of a generic type variable `Type`, by default of the type captured from the provided `value` to check.
+ * Checks if the `value` is not the type obtained from its object class equal to `'number'`, not a `number` type and not an instance of
+ * `Number`.
+ * @var Type A generic type variable `Type` indicates the captured type of the given `value` via the return type
+ * `value is Never<AnyNumber, Type>` and the `value` parameter of the provided `callback` function `ResultCallback` type.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isNotNumber()` function from which it captures its value.
+ * @param value The `value` of a generic type variable `Type`, by default of the type captured from itself to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
- * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
+ * check, and `payload` of the generic type variable `Payload` with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is not a `number` type and not an instance of  `Number`.
  * @angularpackage
  */
 export const isNotNumber = <Type, Payload extends object = object>(
   value: Type,
-  callback: ResultCallback<Type, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<Type, Payload> = resultCallback,
+  payload?: Payload
 ): value is Never<AnyNumber, Type> =>
   callback(
     typeOf(value) !== 'number' &&
