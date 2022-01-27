5063ff8c9205e4c8ccf21291a5c465fc835ed37c
angularpackage <contact@angular-package.dev>
2022-01-27 14:10:48 +0100

    docs(isNotBoolean()): update payload parameter.

--- src/is/not/lib/is-not-boolean.func.ts
+++ src/is/not/lib/is-not-boolean.func.ts
@@ -16,7 +16,7 @@ import { ResultCallback } from '../../../type/result-callback.type';
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
  * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
  * the `result` return. By default, it uses `resultCallback()` function.
- * @param payload An optional `object` of `CallbackPayload` that is assigned to the `payload` of the supplied `callback` function.
+ * @param payload An optional `object` of the generic type variable `Payload` is assigned to the `payload` of the given `callback` function.
  * @returns The return value is a `boolean` indicating whether the provided `value` is not `boolean` type and not an instance of `Boolean`.
  * @angularpackage
  */
