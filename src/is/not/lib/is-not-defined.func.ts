15f6ff4ce84a9107ba6ddc82c0dcfa8bea4bb8ae
angularpackage <contact@angular-package.dev>
2022-01-27 13:44:46 +0100

    refactor(isNotDefined()): remove `CallbackPayload` in favor of generic type variable `Payload` and update jsdoc.

--- src/is/not/lib/is-not-defined.func.ts
+++ src/is/not/lib/is-not-defined.func.ts
@@ -2,12 +2,15 @@
 import { resultCallback } from '../../../lib/result-callback.func';
 import { typeOf } from '../../../lib/type-of.func';
 // Type.
-import { CallbackPayload } from '../../../type/callback-payload.type';
 import { ResultCallback } from '../../../type/result-callback.type';
 import { Undefined } from '../../../type/undefined.type';
 /**
- * Checks if the `value` is not the type obtained from its `Object.prototype` equal to `'undefined'`, not an `undefined` type and is not
- * equal to `undefined`.
+ * Checks if the `value` is not the type obtained from its object class equal to `'undefined'`, not an `undefined` type and is not equal
+ * to `undefined`.
+ * @var Type A generic type variable `Type` indicates the captured type of the given `value` via the return type
+ * `value is Undefined<Type>` and the `value` parameter of the provided `callback` function `ResultCallback` type.
+ * @var Payload The `Payload` generic type variable constrained by `object` indicates the type of optional parameter `payload` of the
+ * supplied `callback` function and `payload` optional parameter of the `isNotDefined()` function from which it captures its value.
  * @param value The `value` of a generic type variable `Type`, by default of the type captured from the provided `value` to check.
  * @param callback A callback `function` of `ResultCallback` type with parameters, the `value` that has been checked, the `result` of this
  * check, and `payload` of the default `CallbackPayload` shape with optional properties from the provided `payload`, to handle them before
@@ -18,8 +21,8 @@ import { Undefined } from '../../../type/undefined.type';
  */
 export const isNotDefined = <Type, Payload extends object = object>(
   value: Type,
-  callback: ResultCallback<Type, CallbackPayload<Payload>> = resultCallback,
-  payload?: CallbackPayload<Payload>
+  callback: ResultCallback<Type, Payload> = resultCallback,
+  payload?: Payload
 ): value is Undefined<Type> =>
   callback(
     typeOf(value) === 'undefined' &&
