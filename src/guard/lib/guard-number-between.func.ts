0a7f44942c9b04bc577af04e317f4ecb58db5409
angularpackage <contact@angular-package.dev>
2022-01-28 15:58:50 +0100

    refactor(guardNumberBetween()): set `min` and `max` parameters as required, remove the `MinMax` generic type in favor of simple object, update jsdoc.

--- src/guard/lib/guard-number-between.func.ts
+++ src/guard/lib/guard-number-between.func.ts
@@ -1,16 +1,14 @@
 // Function.
 import { isNumberBetween } from '../../is/lib/is-number-between.func';
-// Interface.
-import { MinMax } from '../../interface/min-max.interface';
 // Type.
 import { AnyNumber } from '../../type/any-number.type';
 import { NumberBetween } from '../../type/number-between.type';
 import { ResultCallback } from '../../type/result-callback.type';
 /**
- * Guards the value to be `number` between the specified range.
+ * Guards the value to be `number` type or instance of `Number` between the specified range.
  * @param value The value of a generic type variable `Type` to guard.
- * @param min The optional **minimum** range of generic type variable `Min` for a given `value`.
- * @param max The optional **maximum** range of generic type variable `Max` for a given `value`.
+ * @param min The **minimum** range of generic type variable `Min` for a given `value`.
+ * @param max The **maximum** range of generic type variable `Max` for a given `value`.
  * @param callback An optional `ResultCallback` function to handle the result before returns.
  * @param payload Optional `object` of generic type variable `Payload` is assigned to the `payload` of the provided `callback` function.
  * @returns The return value is a `boolean` indicating whether the `value` is a `number` type or an instance of `Number` between the
@@ -23,9 +21,9 @@ export const guardNumberBetween = <
   Payload extends object = object
 >(
   value: Type,
-  min?: Min,
-  max?: Max,
-  callback?: ResultCallback<Type, MinMax<Min, Max> & Payload>,
+  min: Min,
+  max: Max,
+  callback?: ResultCallback<Type, { min: Min, max: Max } & Payload>,
   payload?: Payload
 ): value is NumberBetween<Min, Max, Type> =>
   isNumberBetween(value, min, max, callback, payload);
